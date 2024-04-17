581e55f57b481e29615871f2fa27a26c1dbb163e
Angus Gastle <angusgastle@gmail.com>
2024-04-17 15:30:55 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,4 +1,33 @@
-// Malbolge implementation for printing "Hello World"
-// Malbolge is known for being one of the hardest programming languages to understand.
+whitespace
+	S S S	T	Tab	Space	S	S	T T	T 
+T	Tab	LF	
+T	LF	
+S	T	T	Tab	Space	S	S	S	S	T	T	T	T	T	LF	
+S	S	S	T	Tab	Tab	T	T	T	T	Tab	LF	
+S	S	S	T	Tab	Space	T	T	T	T	Tab	LF	
+S	S	S	T	Tab	Space	T	T	T	T	Space	LF	
+S	S	S	T	Tab	Space	S	S	T	T	Tab	LF	
+S	S	S	T	Tab	Space	S	S	S	S	Space	LF	
+S	S	S	T	Tab	Space	S	S	S	S	Tab	LF	
+S	S	S	T	Tab	Space	S	Sets	S	T	T	LF	
+S	S	S	T	Tab	Space	T	T	T	T	T	LF	
+S	S	S	T	Tab	Space	S	S	S	T	Tab	LF	
+S	S	S	T	Tab	Space	T	T	T	T	Space	LF	
+S	S	S	T	Tab	Space	S	S	S	T	T	LF	
+S	S	S	T	Tab	Space	S	S	S	S	T	LF	
+T	LF	
+S	S	S	T	Tab	Tab	S	S	T	Space	LF	
+S	S	S	T	Tab	Tab	T	T	T	T	Space	LF	
+S	S	S	T	Tab	Tab	T	T	T	Space	LF	
+S	S	S	T	Tab	Tab	S	S	S	S	Tab	LF	
+S	S	S	T	Tab	Tab	T	T	T	Space	LF	
+S	S	S	T	Tab	Tab	T	T	T	T	Space	LF	
+S	S	S	T	Tab	Tab	S	T	T	Space	LF	
+T	LF	
+T	LF	
+T	Tab	S	T	Tab	LF	
+T	Tab	S	S	T	Tab	LF	
+S	S	S	T	Tab	Space	S	S	T	T	T	LF	
+S	T	LF	
 
-(=<`%:9]!><;][(((][%:9]!<;][%:9]=<`%;:9:;><;[[[[%:9]=<;];[[<[&62<`%&:9`=`%;[;]<[<;&;[%;:=<`%&>;[;]=<;[;]=`%:<;[;[;]<`%;`>;;;[<;<[;]=<`%&><:[[;]=[;]<`%:`>:;;[;]=<`%;:[;&;[;]<:%&:[;]<`%&[<;%:9`=`%;[`><[;]=`%:`><`%:`><&62<;%:9`=`%;[`><;<[[;]``%::<`>])+{`%:9;[[[&62<`%&>;[;]=<];;[;:9`=`%;[[;])[;[<`%&;>`>%:&;];[;)<`%;`>;;;[;>==`%`>=`%:9`=`%;[;]<[<;&;[%;:=<`%&>;[;]=<;[;]=`%:<;[;[;]<`%;`>;;;[<;<[;]=<`%&><:[[;]=[;]<`%`;><;[;]][;%:9=;]<`%;[`>;;[;]<`%&`>%:&;];[;)<`%;`>;<`%:`>=`%:9`=`%;[[;])[;[<`%&;>`>%:&;];[;)<`%;`>;;;[;>==`%`>=`%:9`=`%;[;]<[<;&;[%;:=<`%&>;[;]=<;[;]=`%:<;[;[;]<`%;`>;;;[<;<[;]=<`%&><:[[;]=[;]<`%&;`>%:&;];[;)<`%;`>;<;&[[[&62<;%:9`=`%;[;]<[<;&;[%;:=<`%&>;[;]=<;[;]=`%:&;[;]<`%&[<;%:9`=`%;[`><;<[[;]``%::<`>])+{`%:9;[[[&;]]=`%:<[;][([[&62<`%&>;[;]=<];;[;:9`=`%;[[;])[;[<`%&;>`>%:&;];[;)<`%;`>;;;[;>==`%`]>=`%:9`=`%;[;]<[<;&;[%;:=<`%&>;[;]=<;[;]=`%:<;[;[;]<`%;`>;;;[<;<[;]=<`%&><:[[;]=[;]<`%&;`>%:&;];[;)<`%;`>;<`%:`>=`% (=<`%:9]!><;][(((][%:9]!<;][%:9]=<`%;:9:;><;[[[[%:9]=<;];[[<[&62<`%&:9`=`%;[;]<[<;&;[%;:=<`%&>;[;]=<;[;]=`%:<;[;[;]<`%;`>;;;[<;<[;]=<`%&><:[[;]=[;]<`%:`>:;;[;]=<`%;:[;&;[;]<:%&:[;]<`%&[<;%:9`=`%;[`><[;]=`%:`~
+This Whitespace program outputs "Hello World" using a stack manipulation technique and ASCII encoding where characters are pushed onto the stack and then printed directly, with each character's ASCII value carefully manipulated and computed. Whitespace uses spaces, tabs, and line feeds as syntax components, where space (` `) means push a number onto the stack, tab-space (`	 `) means perform arithmetic operations, and tab-tab (`		`) means output.
