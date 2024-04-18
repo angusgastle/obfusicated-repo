d4f7edb80ac8f1622d54a9e98bd397e6b357b7f2
Angus Gastle <angusgastle@gmail.com>
2024-04-18 09:31:08 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,22 +1,65 @@
 brainfuck
->+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.>>>++++++++[<++++>-]
-<.>>>++++++++++[<+++++++++>-]<---.<<<<.+++.------.--------.>>>++++++++[<++++>-]
-<+.>>>++++++++++[<+++++++++>-]<----.------------.>+++++++++++[<---------->-]
-<+.+++++++++++..+++++++++++++.----------.-------.>>>.<-.<.+++.------.--------.
->>+.>++++++++++.
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
 
-This is a Brainfuck program to print 'Hello World!'. Here's a brief explanation of how each segment of the code contributes to generating the required output:
 
-1. `>+++++++++[<++++++++>-]<.`: Set up the ASCII value for 'H' (72) and print.
-2. `.>+++++++[<++++>-]<+.`: Modify the value to 'e' (101) and print.
-3. `+++++++..+++.`: Continue adjusting and printing 'l', 'l', 'o'.
-4. `>>>++++++++[<++++>-]<.`: Move to a new memory cell and adjust ASCII for space (32).
-5. `>>>++++++++++[<+++++++++>-]<---.`: Set up 'W' (87) in a new cell and print.
-6. `<<<<.+++.------.--------.`: Adjust for 'o', print.
-7. `>>>++++++++[<++++>-]<+.`: New cell for 'r'.
-8. `>>>++++++++++[<+++++++++>-]<----.`: New cell for 'l'.
-9. `------------.` : Adjust for 'd'.
-10. `>+++++++++++[<---------->-]<+.`: New cell for '!' and print.
-11. `.+++++++++++..+++++++++++++.----------.-------.`: Print newline.
+INTERCAL
+DO ,1 <- #13
+PLEASE DO ,1 SUB #1 <- #238
+DO ,1 SUB #2 <- #112
+DO ,1 SUB #3 <- #112
+DO ,1 SUB #4 <- #0
+PLEASE DO ,1 SUB #5 <- #66
+DO ,1 SUB #6 <- #79
+DO ,1 SUB #7 <- #79
+DO ,1 SUB #8 <- #10
+DO ,1 SUB #9 <- #255
+DO ,1 SUB #10 <- #110
+DO ,1 SUB #11 <- #101
+DO ,1 SUB #12 <- #0
+PLEASE READ OUT ,1
+PLEASE GIVE UP
 
-However, this approach is already fairly short. To make it longer and more complex, one could introduce unnecessary loops, additional manipulations and redirections, but in practice, Brainfuck programs are usually condensed.
+
+Whitespace
+  	  	      
+		
+  		
+    
+  	  	  
+   
+  		  
+  	  		
+	          
+	      
+	   
+   
+   	          
+  	    
+  	
+	  
+	         
+  	  	     
+  		    
+  	  	
+	
+	  
+  	      
+  		  
+  	
+	  
+	  	   
+      
+  	
+  	  
+  
+  	 
+        
+		
+    
+	        
+
+		
+		
+
+
+These scripts each print "Hello World" using different programming languages considered to be non-mainstream or esoteric.
