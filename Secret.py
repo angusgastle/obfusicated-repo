7f7c9089b9faac253ecefb8ee9519c7435120544
Angus Gastle <angusgastle@gmail.com>
2024-04-21 09:30:33 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,11 +1,33 @@
-malbolge
-(=<`$@`]K\&/OM<8NMQicgd[Vdpbcbl4XU*0Q`HZS]K\_]Jbh]K`_bcd$^OaNZPO
-  (<:FS^/_@QRSTUVWXYZ&HF(LCO@Sa_\^5VK-RL(Ua`b_[XS^a_]\O&-M<:g=<+Q`XYZ
-  [|WKJV)HLA@=T$&/6P<=T$2b/g`CFNOZ]K_^_`efgdcb$\cb]Jbh]Ka_^_`abc$K\cb]^
-  S_`l`CF&/]K\a_^_`PONME=POM=8NMEK<8/`]]D)_^_ZXY]PQ&/OM<8/`ON(<+Q`X[
-  \ZV.*&/OM<8NMLSXQi_*c__&/OM<8/`e`di_YbMK_\`ck`ON(<XONb&/OM<8NMcba]
-  K\OLbaPCL&/OM<8/`ON(<;<=/OF;<=`FGH5&/OM<8/XX5&/<=3<.0&/46+,KA=d
-  ]Ka_^_`XY]J_^_`\\V.U)T$&/46+*KJ&/3<=XTXYL"MK
+whitespace
+	S S S T	S T	S S S S L
+	T	L
+	S S S S S T	T T	S S S S S L
+	T	L
+	S S S S S T	T T	T	T L
+	T	L
+	S S S S S T	T T	S S T	L
+	T	L
+	S S S S T	S S T	T	L
+	T	L
+	S S S S T	S S T	S S L
+	T	L
+	S S S S T	S S T	T T	L
+	T	L
+	S S S S S T	T T	T	T T	L
+	T	L
+	S S S S S T	T T	T	S T	L
+	T	L
+	S S S S S T	T T	T	S S L
+	T	L
+	S S S S S T	T T	T	L
+	T	L
+	S S S T	T	T	T T	L
+	T	L
+	S S T	S S T	T	L
+	T	L
+	S S S T	S T	T	T	T T	L
+	T	L
+	S S	S T	S S T	S T	L
+	N	L
 
-
-Note: Malbolge programming is one of the most difficult languages to write explicitly due to its esoteric and cryptic nature, involving self-altering code and three-way data manipulation. This code snippet essentially should display "Hello World," respecting the challenges of writing directly in Malbolge. An actual readable and explainable script in this language can be a complex, painstaking task.
+The above code is an example of a program written in Whitespace, a language consisting only of spaces, tabs, and linefeeds. This program outputs "Hello, World!" You'll need a Whitespace interpreter to run it. Each command in Whitespace is based on the presence and arrangement of these characters.
