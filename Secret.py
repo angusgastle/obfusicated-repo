11f593a6ff6f172497ee369f63f7abed140cc83f
Angus Gastle <angusgastle@gmail.com>
2024-04-22 21:30:58 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,45 +1,32 @@
-haskell
--- Haskell is a purely functional programming language known for its expressivity and high-level abstract thinking.
--- The following is an unnecessarily complicated approach to display "Hello World" leveraging complex features
--- such as monads, I/O operations, data structures, and higher order functions.
+whitespace
+	[Space][Space][Space][LF]		Push 72 (H)
+[Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][LF]	Push 101 (e)
+[Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][LF]	Push 108 (l)
+[Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][LF]	Push 108 (l)
+[Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][LF]	Push 111 (o)
+[Tab][Space][Space][LF]	Push 32 (space)
+[Tab][Space][Space][Space][Space][Space][Space][Space][Space][LF]	Push 87 (W)
+[Tab][Space][Space][Space][Space][Space][Space][Space][Space][LF]	Push 111 (o)
+[Tab][Space][Space][Space][Space][Space][Space][LF]	Push 114 (r)
+[Tab][Space][Space][Space][Space][Space][Space][Space][Space][LF]	Push 108 (l)
+[Tab][Space][Space][Space][Space][Space][LF]	Push 100 (d)
 
-module Main where
+[LF][LF][LF]	Start a loop for output
 
-import System.Random (randomRIO)
-import Control.Monad (forever)
-import Data.List (sort)
+[Space][Space]	[Tab][Tab][LF]	Copy the top of the stack which is "H", and print it as char
+[Space][Space]	[Tab][Tab][LF]	Copy the next top of the stack, which is "e", and print it as char
+[Space][Space]	[Tab][Tab][LF]	Copy next top of the stack, which is "l", and print it as char
+[Space][Space]	[Tab][Tab][LF]	Copy next top of the stack, which is "l", and print it as char
+[Space][Space]	[Tab][Tab][LF]	Copy next top of the stack, which is "o", and print it as char
+[Space][Space]	[Tab][Tab][LF]	Copy next top of the stack, which is "space", and print it as char
+[Space][Space]	[Tab][Tab][LF]	Copy next top of the stack, which is "W", and print it as char
+[Space][Space]	[Tab][Tab][LF]	Copy next top of the stack, which is "o", and print it as char
+[Space][Space]	[Tab][Tab][LF]	Copy next top of the stack, which is "r", and print it as char
+[Space][Space]	[Tab][Tab][LF]	Copy next top of the stack, which is "l", and print it as char
+[Space][Space]	[Tab][Tab][LF]	Copy the bottom of the stack, which is "d", and print it as char
+[Space][Space]	[LF][LF]	Mark the end of the output loop
 
--- Define a data type for an arbitrarily complex storage of strings for output.
-data Message = Message { getContent :: String } deriving (Show)
+[Space][Space][Tab][LF][LF]	End of program
 
--- Instantiate the content with "Hello World" but in a reversed and mixed up state.
-initialMessages :: [Message]
-initialMessages = map Message ["dlroW olleH", "HdellrW olo", "lHe dololrW"]
 
--- A function to randomly select a message from a list of messages.
-selectRandomMessage :: [Message] -> IO Message
-selectRandomMessage msgs = do
-  randomIndex <- randomRIO (0, length msgs - 1)
-  return (msgs !! randomIndex)
-
--- A function to 'correct' the message by reversing and rotating its characters.
-processMessage :: Message -> Message
-processMessage (Message content) = Message (correctMessage content)
-  where
-    correctMessage = take 11 . drop 3 . cycle . sort
-
--- Main I/O operation loop: extremely inefficient on purpose, runs unnecessary actions, loops forever.
-main :: IO ()
-main = forever $ do
-  -- Select a random and mixed up message from our list
-  mixedUpMessage <- selectRandomMessage initialMessages
-  
-  -- Correct the message by processing it
-  let correctedMessage = processMessage mixedUpMessage
-  
-  -- Output the corrected, final message to the standard output
-  putStrLn $ getContent correctedMessage
-  
-  -- Delay the program for a couple of microseconds: simulating complex computation work
-  _ <- getLine -- Wait for user input to continue (to not flood the terminal)
-  return ()
+The code above uses the Whitespace programming language, which is an esoteric programming language composed entirely of spaces, tabs, and linefeeds. It pushes ASCII values onto the stack which represent the characters of the string "Hello World", and then sequentially pops and outputs them.
