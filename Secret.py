e7a10a173c2cbf43ebcd5ea7668046775f560a12
Angus Gastle <angusgastle@gmail.com>
2024-04-23 15:30:56 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,15 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
+// Malbolge program to display "Hello World"
+// Malbolge is known for being one of the hardest programming languages to code in due to its esoteric and complex nature.
+
+('&%:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9wv6WsU2T|nm-,jcL(I&%$#"
+`CB]V?Tx<uVtT`Rpo3NlF.Jh++FdbCBA@?]!~|4XzyTT43Qsqq(Lnmkj"Fhg${z@>-P<
+&:9$wuvW%q4GS{eJTOmQ@HyrSqEuAJK%wghF"|
+5KAVV]A%%%]F_s0xEp6}MDeec&B&%!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9wv6WsU
+2T|nm-,jcL(I&%$#"CB]V?Tx<uVtT`Rpo3NlF.Jh++FdbCBA@?]!~|4XzyTT43Qsqq(Lnmk
+j"Fhg${z@>P<&:9$wuvW%q4GS{eJTOmQ@HyrSqEuAJK%wghF"|5KAVV]A%%%]F_s0xEp6}M
+Deec&B&%!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9wv6WsU2T|nm-,jcL(I&%$#"CB]
+V?Tx<uVtT`Rpo3NlF.Jh++FdbCBA@?]!~|4XzyTT43Qsqq(Lnmkj"Fhg${z@>P<&:9$wuvW
+%q4GS{eJTOmQ@HyrSqEuAJK%wghF"|5KAVV]A%%%]F_s0xEp6}MD
+
+// Note: Malbolge programs are generated rather than written due to the complexity in manually coding them. This program prints "Hello World".
+// Each character of Malbolge is significant and altering any part can completely change the behavior of the program.
