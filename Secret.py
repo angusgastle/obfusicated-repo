95522c3a75699f88f391b13c9fe64c2abcab61b3
Angus Gastle <angusgastle@gmail.com>
2024-04-24 09:31:05 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,20 +1,2 @@
-esoteric
-;The following Malbolge code displays the message "Hello World" when run.
-;Malbolge is known for being a complicated and esoteric programming language where programming is made difficult by design.
-
-(=<`$`=87326<`)
-
-; The above line initializes the data pointer and sets up the environment for the subsequent program
-; In Malbolge, data and code share the same space and "crazy" operation alters code/data on the fly making predictions harder.
-
-$/=({(<{({([(<${(<(<{}+<<<<[+<****>>++])=+$$$$=$=+;=<++++]+[)[+<****>>++])=<++)+]>>)++]+<>)++)=++;==+==)=$>)+++]+<>)</=/+==)++++;=<++++])>>)+++]+<>)++)=++++;==++)++)++++++])<)>))++];
-; This line performs various operations which manipulate data, perform loops and implement conditional logic.
-; The main purpose here is to set up memory with appropriate values that are to be used for outputting "Hello World".
-
-(++<[=+++[[=++]]=<++{<+++++[<++++****>>++]){<++++]{=++=)=<++++])>+++++]))++<++[+++]+{</<[=++;={==++++]<<[/<++++])<++++]}=;=++)=$+++];<++++])$/,++]);<++++])++=++++;=+<++]);
-; This sequence further manipulates program state, preparing the output characters for the string "Hello World".
-
-++++$;+++];;++;</;[=++;={==++++]<<[/<++++])<++++]}=;=++)=$+++];<++++])$/,++]);;+++];==+$=++<++[+++]+<++++])<);
-; This line ultimately outputs each character of "Hello World" by moving the data pointer to the correct memory locations where the characters are stored after being set up in previous lines.
-
-; The succeeding lines and characters (which are logical NOOP in Malbolge) are often used for padding to adjust the memory and data pointer positions correctly due to Malbolge's trinary (ternary) execution and encoding.
+brainfuck
+++++++++++[>++++++++++>+>+>+>+>++++<<<<<-]>++++>++++++>+++++>++++>++>>>+++++[-<++++<++++<++++<++++<++++>>>]<<<<<<[>>+>+>+>+>++>+++++<<<<<<-]>>>>>++++++++++[<<<<+>>>>-]<<<<[>++[<++++++>-]<.>++++++++++[<++++++>-]<+.>+++++[<+++++>-]<.>++[<++++++>-]<+.>++++++++++[<+++++>-]<.]
