f44efabbe69f482e2b878c20ba2439be046bc002
Angus Gastle <angusgastle@gmail.com>
2024-04-23 21:30:29 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,15 +1,32 @@
-// Malbolge program to display "Hello World"
-// Malbolge is known for being one of the hardest programming languages to code in due to its esoteric and complex nature.
+prolog
+% Prolog isn't typically used for simple I/O tasks, making this example inherently "obscure" in usage.
+% This Prolog program will display "Hello World" once you consult the file and run the hello_world predicate.
 
-('&%:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9wv6WsU2T|nm-,jcL(I&%$#"
-`CB]V?Tx<uVtT`Rpo3NlF.Jh++FdbCBA@?]!~|4XzyTT43Qsqq(Lnmkj"Fhg${z@>-P<
-&:9$wuvW%q4GS{eJTOmQ@HyrSqEuAJK%wghF"|
-5KAVV]A%%%]F_s0xEp6}MDeec&B&%!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9wv6WsU
-2T|nm-,jcL(I&%$#"CB]V?Tx<uVtT`Rpo3NlF.Jh++FdbCBA@?]!~|4XzyTT43Qsqq(Lnmk
-j"Fhg${z@>P<&:9$wuvW%q4GS{eJTOmQ@HyrSqEuAJK%wghF"|5KAVV]A%%%]F_s0xEp6}M
-Deec&B&%!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9wv6WsU2T|nm-,jcL(I&%$#"CB]
-V?Tx<uVtT`Rpo3NlF.Jh++FdbCBA@?]!~|4XzyTT43Qsqq(Lnmkj"Fhg${z@>P<&:9$wuvW
-%q4GS{eJTOmQ@HyrSqEuAJK%wghF"|5KAVV]A%%%]F_s0xEp6}MD
+% Predicate to initialize the program
+start :-
+    format('Starting the program...~n', []),
+    init_hello_world.
 
-// Note: Malbolge programs are generated rather than written due to the complexity in manually coding them. This program prints "Hello World".
-// Each character of Malbolge is significant and altering any part can completely change the behavior of the program.
+% Predicate to prepare greeting message
+init_hello_world :-
+    create_message("Hello", "World", Message),
+    display_message(Message).
+
+% Predicate to combine parts of the greeting into a single string
+create_message(PartOne, PartTwo, Message) :-
+    string_concat(PartOne, " ", TempMessage),
+    string_concat(TempMessage, PartTwo, Message).
+
+% Predicate to print the final message
+display_message(Message) :-
+    format('Your message: "', []),
+    format(Message, []),
+    format('"~n', []),
+    format('Program finished.~n', []).
+
+% Entry point for the Prolog interpreter
+hello_world :-
+    start.
+
+% To run this script, consult the file in your Prolog environment and execute the predicate:
+% ?- hello_world.
