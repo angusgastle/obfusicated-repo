f22f5f82540b713e2964c0a858a91a11427fc9e6
Angus Gastle <angusgastle@gmail.com>
2024-04-24 15:31:02 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,26 @@
-brainfuck
-++++++++++[>++++++++++>+>+>+>+>++++<<<<<-]>++++>++++++>+++++>++++>++>>>+++++[-<++++<++++<++++<++++<++++>>>]<<<<<<[>>+>+>+>+>++>+++++<<<<<<-]>>>>>++++++++++[<<<<+>>>>-]<<<<[>++[<++++++>-]<.>++++++++++[<++++++>-]<+.>+++++[<+++++>-]<.>++[<++++++>-]<+.>++++++++++[<+++++>-]<.]
+esoteric
+Brainfuck:
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>>>+.>++.
+
+
+esoteric
+Whitespace:
+[Space][Tab][Space]   [Space][Tab][Space][Space][Space][Space]   
+[Space][Tab][Space][Space][Space]   [Space][Tab][Space][Space][Space][Space][Space]   
+[Space][Tab][Space][Space][Space][Space][Space]   [Space][Tab][Space][Space][Space][Space][Space][Space]   
+[Space][Tab][Space][Space][Space][Space][Space][Space][Space]   [Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space]   
+[Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space]   [Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space]   
+[Newline][Space][Space][Space][Space][Space][Space]   [Space][Space][Space][Space][Space][Space][Space][Space]   
+[NL][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space]   [Space][Space][Space][Space][Space][Space][Space][Space]   
+[Newline][Space][Space][Space][Space][Space][Space]   [Newline]
+ 
+
+esoteric
+Malbolge:
+('&%:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9wv6WsU2T|nm-,jcL(I&%$#"
+`CB]V?Tx<uVtT`Rpo3NlF.Jh++FdbCBA@?]!~|4XzyTT43Qsqq(Lnmkj"Fhg${z@>
+
+
+esoteric
+Piet:
+(Due to the graphical nature of Piet, an actual useful output here would be an image of the Piet program executing "Hello World". Piet programs are drawn with colors where each color change represents different instructions based on codel choosers and switches.)
