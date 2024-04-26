b41472389bb8a6b9ca51665ee518d0b315e7c108
Angus Gastle <angusgastle@gmail.com>
2024-04-26 09:31:01 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,36 +1,24 @@
-// ArnoldC is a programming language based on the one-liners of Arnold Schwarzenegger.
-// This example script demonstrates a "Hello World" program in ArnoldC, making it as
-// complex and documented as possible for educational purposes.
-
-IT'S SHOWTIME                      // Start of the main program block
-TALK TO THE HAND "Hello World"     // Print "Hello World" to the console
-
-// Declaring some unnecessary variables for complexity
-HEY CHRISTMAS TREE isSkyBlue
-YOU SET US UP @NO PROBLEMA
-HEY CHRISTMAS TREE hasMuscles
-YOU SET US UP @I LIED
-HEY CHRISTMAS TREE itIsNotATumor
-YOU SET US UP @NO PROBLEMA
-
-// Perform some pointless checks
-BECAUSE I'M GOING TO SAY PLEASE isSkyBlue
-    TALK TO THE HAND "The sky is blue."
-BULLSHIT
-    TALK TO THE HAND "The sky isn't blue?"
-YOU HAVE NO RESPECT FOR LOGIC
-
-BECAUSE I'M GOING TO SAY PLEASE hasMuscles
-    TALK TO THE HAND "I have muscles."
-BULLSHIT
-    TALK TO THE HAND "I have no muscles?"
-YOU HAVE NO RESPECT FOR LOGIC
-
-BECAUSE I'M GOING TO SAY PLEASE itIsNotATumor
-    TALK TO THE HAND "It's not a tumor."
-BULLSHIT
-    TALK TO THE HAND "It is a tumor?"
-YOU HAVE NO RESPECT FOR LOGIC
-
-// End of program
-YOU HAVE BEEN TERMINATED             // Terminates the program
+whitespace
+	S S S T	T  S S L
+T	L
+S S S T	T  L
+T	L
+S S S T	T  T	S S L
+T	L
+S S S T	T  T	T	S L
+T	L
+S S S T	T  T	T	T	L
+T	L
+S S S T	T  S T	S T	L
+T	L
+S S S T	T  S S S L
+T	L
+S S S T	T  S T	S L
+T	L
+S S S T	T  S T	T L
+T	L
+S S S T	T  T	S L
+T	L
+S S S T	L
+L
+L
