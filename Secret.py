c42f536ff2bdb86b3e8a4d959866351d6dac3267
Angus Gastle <angusgastle@gmail.com>
2024-04-27 09:30:31 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,49 +1,5 @@
-prolog
-% A Prolog program to display "Hello World"
-%
-% Prolog is primarily a logic programming language associated with artificial intelligence
-% and computational linguistics. Here, I write a Prolog script that creatively outputs 
-% "Hello World" by using a series of predicates to select, compose, and display the message.
-
-% Define a predicate that holds character pieces of "Hello World"
-char(h, 0).
-char(e, 1).
-char(l, 2).
-char(l, 3).
-char(o, 4).
-char(space, 5).
-char(w, 6).
-char(o, 7).
-char(r, 8).
-char(l, 9).
-char(d, 10).
-
-% Map positions to characters for creating the string
-position_char(Pos, Char) :-
-    char(Char, Pos).
-
-% A predicate to convert a list of character keys to the actual message string
-build_string([], []).
-build_string([H|T], [Char|Tail]) :-
-    position_char(H, Char),
-    build_string(T, Tail).
-
-% A helper predicate to write elements of the list to the console
-write_list([]).
-write_list([Head|Tail]) :-
-    write(Head),
-    write_list(Tail).
-
-% Predicate to initiate the action of building and displaying "Hello World"
-display_hello_world :-
-    % Define the order of character pieces to form "Hello World"
-    MessageKeys = [h, e, l, l, o, space, w, o, r, l, d],
-    build_string(MessageKeys, MessageChars),
-    write_list(MessageChars),
-    nl.
-
-% Query to run when the file is consulted or loaded to display "Hello World"
-:- initialization(display_hello_world).
-
-
-This Prolog program defines a complex way to print "Hello World" by mapping each character to a position and assembling them through logical inference. The reasoning capabilities of Prolog are shown in how it puts together the string before printing.
+malbolge
+(<`#`<{({<%<{``#'<<`<<'#'`'<`<{`#{`<'#>#}>[#]>}}#<}>>[[[{[[[{[{[{<[{[{[{[{[[[[[{[{[{[{[{[{[[<[[[[[{[[
+>>]}[>>]}[>)}[>)}[>>]}[>>)}[}[[}[}[[}[}[}[}[}[}[>[}}[[}[}[>[}}[[}[}[}[}[>)}[>>]}[[[[[[[[[[[}[
+>[}}[[}[}[}[}[}[>)}[>>[}}[[}[}[}[>[}[>[[[[[[[[[[[}[[}}[[}[}[>[[[}[}[>[}}[[[[[[[[[[[[[[[}[
+d
