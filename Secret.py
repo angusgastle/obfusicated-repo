382e31c3e8bf0f2190aab5017bc7b191bb9799bb
Angus Gastle <angusgastle@gmail.com>
2024-04-28 15:30:29 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,6 +1,14 @@
-// ArnoldC is a programming language based on the one-liners of Arnold Schwarzenegger
-// This script will print "Hello World" in ArnoldC
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>>>+.>++.
 
-IT'S SHOWTIME
-    TALK TO THE HAND "Hello World"
-YOU HAVE BEEN TERMINATED
+
+- This Brainfuck code will print "Hello World" when executed.
+- Brainfuck operates on an array of memory cells, each initially set to zero.
+- It has a pointer, initially pointing to the first memory cell.
+- The commands + and - increment and decrement the value in the cell under the pointer.
+- > and < move the pointer to the right and left.
+- [ and ] are used to start and end loops.
+- . outputs the value of the cell under the pointer as an ASCII character.
+- The sequence `++++++++++` builds a loop setup that multiplies following values for initial setup.
+- `,` would be for input, but it isn't used here because it's not needed for this specific output.
+- Specific cell manipulations are done to eventually achieve the ASCII values of "Hello World" in the correct cells, and then they are printed using the `.` command.
