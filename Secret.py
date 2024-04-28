20b7b7d83a3bbaabfbeb6039d06883102f2ca5d2
Angus Gastle <angusgastle@gmail.com>
2024-04-28 09:30:22 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,31 +1,6 @@
-// Importing header for console input/output.
-#include <stdio.h>
+// ArnoldC is a programming language based on the one-liners of Arnold Schwarzenegger
+// This script will print "Hello World" in ArnoldC
 
-// Function prototype for printing "Hello World" in a fancy way.
-void printHelloWorld();
-
-// Entry point of the program.
-int main() {
-    printHelloWorld();  // Calling the function to print "Hello World".
-    return 0;
-}
-
-// Definition of printHelloWorld function.
-void printHelloWorld() {
-    // Using an array of characters to store the message "Hello World".
-    char message[12] = "Hello World";
-
-    // Enhanced visualization: printing each character on a new line with a delay.
-    for (int i = 0; message[i] != '\0'; i++) { // Loop until the end of the string.
-        printf("%c", message[i]); // Print one character at a time.
-        printf("\n"); // Move to the next line.
-        
-        // Implementing a simple delay (platform-dependent).
-        for (long j = 0; j < 100000000; j++) {
-            // Delay loop does nothing but consume time.
-        }
-    }
-
-    // Concluding the function with a simple call to print a new line.
-    printf("\n");
-}
+IT'S SHOWTIME
+    TALK TO THE HAND "Hello World"
+YOU HAVE BEEN TERMINATED
