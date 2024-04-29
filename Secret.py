5a36ddcb74eac8a61df2661269d2a8e19e11b588
Angus Gastle <angusgastle@gmail.com>
2024-04-28 21:30:21 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,14 +1,3 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>>>+.>++.
-
-
-- This Brainfuck code will print "Hello World" when executed.
-- Brainfuck operates on an array of memory cells, each initially set to zero.
-- It has a pointer, initially pointing to the first memory cell.
-- The commands + and - increment and decrement the value in the cell under the pointer.
-- > and < move the pointer to the right and left.
-- [ and ] are used to start and end loops.
-- . outputs the value of the cell under the pointer as an ASCII character.
-- The sequence `++++++++++` builds a loop setup that multiplies following values for initial setup.
-- `,` would be for input, but it isn't used here because it's not needed for this specific output.
-- Specific cell manipulations are done to eventually achieve the ASCII values of "Hello World" in the correct cells, and then they are printed using the `.` command.
+whitespace
+	S	S	S	T	T	S	S	L		T	S	S	T	S	T	T	T	L		T	T	S	S	T	T	S	L		T	T	T	S	S	S	S	S	L		T	T	T	S	S	S	T	S	L		T	S	S	T	T	S	S	T	L		T	T	T	S	S	T	T	L		T	T	T	S	S	T	S	T	L		T	T	T	T	T	T	S	T	L		L
+	S	S	L	T	S	T	T	T	S	S	T	L		L	L
