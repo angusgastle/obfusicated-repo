1916d8333d1a4a959abc549e33261165a632ac3e
Angus Gastle <angusgastle@gmail.com>
2024-04-29 21:30:35 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,7 +1,20 @@
-whitespace
-	S	S	L	T	T	 	T	L	S	S	T	L	T	T	T	S	S
-T	L	S	S	L	T	T	T	S	S	T		T	S	S	S	L	T	T
-S	L	L	T	T	 	S	L	S	L	T	L	T	L	S	L	S	T	L
-T	L										
-										
-Whitespaces are the codes in Whitespace programming language where Spaces, Tabs, and Linefeeds have meaning and ignore other characters.
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
+
+
+This Brainfuck code snippet does the following:
+- Increments the value at the current cell to 10 (using `++++++++++`). This value is later used as a loop counter.
+- The `[` starts a loop which will repeat 10 times.
+- Inside the loop:
+  - `>+++++++` moves the cell pointer to the right and adds 7 to this cell.
+  - `>++++++++++` moves the pointer again and adds 10 to the next cell.
+  - `>+++` and then `>+` increment the next two cells by 3 and 1 respectively.
+  - `<<<<-` moves the pointer back 4 cells and decrements the current cell by 1 (acting as the loop counter).
+- `>` moves the cell pointer right to the cell that now contains 70 (`H` in ASCII).
+- `.` outputs the character 'H' (70 in ASCII).
+- `>+.>` increments the next cell to 101, moves the pointer to the right, and prints 'e' (101 in ASCII).
+- `+++++++..+++.` forms and outputs "llo".
+- `>++.` moves to the cell with 32 (space), increments it, and outputs the space.
+- `<<+++++++++++++++.>.+++.` moves back and adjusts cells to print "World".
+- `------.--------.` adjusts ASCII values to print "!".
+- `>+.>.` moves and adjusts cells to output newline or other characters to finalize the "Hello World!" message.
