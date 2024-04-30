c8d6f4c3793a6e0e67a83d83a08b20d294338dbd
Angus Gastle <angusgastle@gmail.com>
2024-04-30 03:31:20 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,20 +1,42 @@
 brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>>>+.>++.
 
 
-This Brainfuck code snippet does the following:
-- Increments the value at the current cell to 10 (using `++++++++++`). This value is later used as a loop counter.
-- The `[` starts a loop which will repeat 10 times.
-- Inside the loop:
-  - `>+++++++` moves the cell pointer to the right and adds 7 to this cell.
-  - `>++++++++++` moves the pointer again and adds 10 to the next cell.
-  - `>+++` and then `>+` increment the next two cells by 3 and 1 respectively.
-  - `<<<<-` moves the pointer back 4 cells and decrements the current cell by 1 (acting as the loop counter).
-- `>` moves the cell pointer right to the cell that now contains 70 (`H` in ASCII).
-- `.` outputs the character 'H' (70 in ASCII).
-- `>+.>` increments the next cell to 101, moves the pointer to the right, and prints 'e' (101 in ASCII).
-- `+++++++..+++.` forms and outputs "llo".
-- `>++.` moves to the cell with 32 (space), increments it, and outputs the space.
-- `<<+++++++++++++++.>.+++.` moves back and adjusts cells to print "World".
-- `------.--------.` adjusts ASCII values to print "!".
-- `>+.>.` moves and adjusts cells to output newline or other characters to finalize the "Hello World!" message.
+This is "Hello World" written in Brainfuck, one of the most baffling programming languages because of its extremely minimalistic design. Here's a breakdown:
+
+1. `++++++++++` Set the value at the current cell to 10 (Cell #0: 10).
+2. `[` Begin loop.
+  - `>+++++++` Move to the next cell (Cell #1) and add 7 to it.
+  - `>++++++++++` Move to the next cell (Cell #2) and add 10 to it.
+  - `>+++` Move to the next cell (Cell #3) and add 3 to it.
+  - `>+` Move to the next cell (Cell #4) and add 1 to it.
+  - `<<<<-` Move back to Cell #0 and subtract 1.
+  - The loop will run until the value in Cell #0 is zero, effectively initializing cells #1 to #4 with 70, 100, 30, and 10, respectively.
+3. `>` Move to Cell #1.
+4. `++.` Add 2, resulting in ASCII value of 72, which is 'H'. Print it.
+5. `>+.++++++++++` Move to Cell #2, add 1 (resulting in 101, ASCII for 'e'), print it, then add 10.
+6. `++++.` Add 4 to Cell #2 (115, ASCII 's') and print.
+7. `++.+++++++..` Add 2 to get 117 ('o'), print, add 7 to get 108 ('l'), print twice.
+8. `+++.` Add 3 to get 111 ('o') again, print.
+9. `>++.` Move to Cell #3, add 2 (to make 32, ASCII for space), print.
+10. `<<+++++` Move back to Cell #1, add 5 to make 87 ('W').
+11. `+++++++.` Add 7, resulting in ASCII 108 ('l'), print.
+12. `>.` Move to Cell #2, print ('d').
+13. `+++.` Add 3 to make ASCII 114 ('r'), print.
+14. `------.` Subtract 6 to make 108 ('l'), print.
+15. `--------.` Subtract 8 to make 100 ('d').
+16. `>+.>` Move to Cell #3, add 1 (ASCII 33, '!') and print.
+17. `>++.` Move to Cell #4, add 2, ready for next operation or terminate the script.
+
+
+Brainfuck operates on an array of memory cells, each initially set to zero. It has a pointer that can move between cells and can increment or decrement the value at the current cell, and conditionally loop depending on the value of the current cell. Each symbol has the following meaning:
+- `>` Move the pointer to the right
+- `<` Move the pointer to the left
+- `+` Increment the memory cell at the pointer
+- `-` Decrement the memory cell at the pointer
+- `.` Output the character at the cell at the pointer
+- `,` Input a character and store it in the cell at the pointer
+- `[` Jump past the matching `]` if the cell at the pointer is 0
+- `]` Jump back to the matching `[` if the cell at the pointer is nonzero
+
+This code generates the phrase "Hello World!" using Brainfuck's extremely limited command set.
