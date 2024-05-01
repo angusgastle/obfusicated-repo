189f0c145f82c52223103ca61e216493b714bbf6
Angus Gastle <angusgastle@gmail.com>
2024-05-01 09:30:56 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,31 +1,26 @@
-whitespace
-	S	S	T	T	S	T	S	T	T	T	L
-	S	T	T	S	S	S	S	T	T	T	T	L
-	S	S	T	S	T	S	T	T	S	T	T	L
-	S	T	S	S	T	T	T	T	T	S	L
-	S	T	S	T	S	T	S	S	S	S	L
-	S	T	T	S	S	T	T	S	S	T	L
-	S	S	T	S	T	S	T	S	T	S	L
-	S	T	S	S	T	S	T	T	T	T	L
-	S	T	T	S	S	S	S	S	T	T	L
-	S	T	T	S	S	S	S	S	S	S	L
-	S	T	S	S	S	T	S	S	T	T	L
-	S	T	S	T	T	S	T	T	S	T	L
-	S	T	S	S	S	T	T	T	T	S	L
-	S	S	T	S	T	T	S	T	T	S	L
-	S	T	S	S	T	S	T	S	T	T	L
-	S	S	T	S	S	T	S	S	S	S	L
-	S	S	T	S	T	S	T	S	S	T	L
-	S	S	T	S	S	S	S	T	S	T	L
-	S	T	T	S	T	T	S	T	S	S	L
-	S	T	S	S	T	T	T	T	S	S	L
-	S	S	T	S	T	T	T	T	T	T	L
-	S	T	T	S	T	T	S	T	S	T	L
-	S	S	S	L
+// Brainfuck code that outputs "Hello World!"
 
-This script in Whitespace prints "Hello World". The Whitespace program uses spaces, tabs, and linefeeds to represent commands:
-- Space (`S`) represents a command prefix.
-- Tab (`T`) is often used in numbers or to modify commands.
-- Linefeed (`L`) ends a command.
+// Declare an array large enough and a pointer
++++++ +++++             // 10 cells forward
+[
+    >+++++++            // Add 7 to cell #1
+    >++++++++++         // Add 10 to cell #2
+    >+++                // Add 3 to cell #3
+    >+                  // Add 1 to cell #4
+    <<<<-               // Go back to cell #0 and decrement it
+]
 
-Most of the complexity and length are achieved by controlling the stack and heap with redundant operations, using Whitespace's limited instruction set to manipulate numbers and memory as verbosely as possible. The above script specifically manipulates the stack to achieve the ASCII values corresponding to "Hello World", outputting them to the terminal.
+// Now, set the cells to the correct ASCII values
+>++.                    // 'H'
+>+.                     // 'e'
++++++++..               // 'll'
++++.
+----- -.                // 'o'
+>++.                    // Space
+<< +++++ +++++++++ +++. // 'W'
+>>.                     // 'o'
++++ .                   // 'r'
+----- -.                // 'l'
+<<+.                    // 'd'
+<.                      // '!'
+>.                      // New Line (optional based on output view)
