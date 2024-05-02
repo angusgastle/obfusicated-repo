1f5ab3ac7ae647afd6fc419a2d9c6cef470d2e49
Angus Gastle <angusgastle@gmail.com>
2024-05-02 03:30:17 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,7 +1,8 @@
-whitespace
-	S	L	L	
-	P	P	T	Hello, World!	S	L
-	L	
-;
-
-In Whitespace, characters are represented using spaces, tabs, and line feeds. This program outputs "Hello, World!" when run. The actual logic and data are encoded in the spaces and tabs, while line feeds are used to separate instructions.
+befunge
+                     v
+>"dlroW olleH",,,,,,,@
+                     > 0123456789v
+>25*"!dlroW olleH":v
+              v:,_@
+>"dlrWo olleH"<<<<<
+              ^  _$
