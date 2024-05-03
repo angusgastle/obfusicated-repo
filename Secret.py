7d9fd1eadb245d70a635cb00d9c583185e8bf16e
Angus Gastle <angusgastle@gmail.com>
2024-05-03 09:31:35 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,34 +1,10 @@
-racket
-#lang racket
-
-; Define a function named `display-hello-world`
-; This function does not take any arguments and when invoked, 
-; it will determine a complex and unpredictable method to display "Hello World".
-(define (display-hello-world)
-  ; Create a list of characters representing "Hello World"
-  (define hello-world-list (string->list "Hello World"))
-  
-  ; Define a helper function to manipulate each character
-  ; This will be used to obfuscate and then deobfuscate the characters
-  (define (transform-character c shift)
-    ; Convert character to ASCII, shift it by `shift` positions, convert back to character
-    (integer->char (+ (char->integer c) shift)))
-
-  ; Obfuscate characters by shifting each by 5 positions
-  (define obfuscated-list (map (lambda (c) (transform-character c 5)) hello-world-list))
-  
-  ; Deobfuscate characters by shifting each back by -5 positions
-  (define original-list (map (lambda (c) (transform-character c -5)) obfuscated-list))
-  
-  ; Convert the list of characters back to a string
-  (define original-string (list->string original-list))
-  
-  ; Print the deobfuscated string to the console
-  (display original-string)
-  (newline))
-
-; Call the function to display "Hello World"
-(display-hello-world)
-
-
-This Racket program uses a custom character transformation to obfuscate and then deobfuscate a "Hello World" string, making it more complex than a simple print statement. It showcases various functional programming techniques available in Racket.
+whitespace
+	S S S	T	T	T	LF	T	LF	LF
+	T	LF	S S S	LF	LF	S	LF	T	LF	T	T	LF
+	S S S	LF	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	LF	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	T	S	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	T	S	S	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	LF	S	LF	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF
+	T	LF	S S S	T	T	T	LF	S	LF	T	LF	T	T	LF	S S S	LF	LF	S	LF	T	LF	T	T	LF	S S S	LF	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	LF	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	T	S	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	T	S	S	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	LF	S	LF	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF
+	T	LF	S S S	T	T	T	LF	S	LF	T	LF	T	T	LF	S S S	LF	LF	S	LF	T	LF	T	T	LF	S S S	LF	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	LF	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	T	S	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	T	S	S	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	LF	S	LF	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF
+	T	LF	S S S	T	T	T	LF	S	LF	T	LF	T	T	LF	S S S	LF	LF	S	LF	T	LF	T	T	LF	S S S	LF	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	LF	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	T	S	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	T	S	S	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	LF	S	LF	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF
+	T	LF	S S S	T	T	T	LF	S	LF	T	LF	T	T	LF	S S S	LF	LF	S	LF	T	LF	T	T	LF	S S S	LF	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	LF	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	T	S	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	T	S	S	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	LF	S	LF	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	T	LF	LF	LF
+	T	LF	S S S	T	T	T	LF	S	LF	T	LF	T	T	LF	S S S	LF	LF	S	LF	T	LF	T	T	LF	S S S	LF	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	LF	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	T	S	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	T	S	S	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	LF	LF	S	T	LF	S	LF	S	T	LF	LF	T	LF	S	S	S	T	T	LF	LF	T
+	T	LF	S S S	T	T	T	LF	T	LF	LF	LF	LF
