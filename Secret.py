e690b8e22556fba663b63fedbc473a101110ad7d
Angus Gastle <angusgastle@gmail.com>
2024-05-03 21:30:18 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,12 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>>>+.>++.
+emojicode
+💭 Main program block begins
+🏁 🍇
+  💭 Define a method to display "Hello World" in a verbose manner
+
+  🍦📜🍇
+    😀 🔤Hello World!🔤❗️ 💭 Print "Hello World!" to the console
+  🍉
+
+  💭 Execute the defined method
+  📜❗️
+🍉
