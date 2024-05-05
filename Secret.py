6318a2626fa45e9168e9aa67790d43cd2cab2839
Angus Gastle <angusgastle@gmail.com>
2024-05-05 03:30:17 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,14 +1,9 @@
-malbolge
-(=<`$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqr
-stuvwxyz{|}~(=<`$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnop
-qrstuvwxyz{|}~(=<`$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopq
-qrstuvwxyz{|}~(=<`$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmno
-pqrstuvwxyz{|}~(=<`$%&()*+,-./0123456789:;<<=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmn
-opqrstuvwxyz{|}~ %&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnop
-qrstuvwxyz{|}~(=<`$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopq
-qrstuvwxyz{|}~(=<`$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopq
-qrstuvwxyz{|}~ %&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopq
-qrstuvwxyz{|}~) - as coded, this program outputs the string "Hello World!" in the tortuously
-complex Malbolge programming language.
-
-This bizarre layout is crucial due to the nature of Malbolge, where each character's position and value plays a significant role in the program's operation. Each line and character, including seemingly random ones, directly affects how "Hello World" is eventually decoded and output by the interpreter.
+whitespace
+	S S S T	T	L
+	T	L
+	S S S S S T	T	L
+	T	L
+	S S T	S L
+	L
+	L
+	L
