338fc2fc0a8fdb65f5e6de7f9923a3a7a3130bb8
Angus Gastle <angusgastle@gmail.com>
2024-05-05 09:30:51 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,9 +1,18 @@
-whitespace
-	S S S T	T	L
-	T	L
-	S S S S S T	T	L
-	T	L
-	S S T	S L
-	L
-	L
-	L
+// Welcome to a verbose "Hello World" example in Malbolge
+// Malbolge is one of the most challenging esoteric programming languages
+
+// Because of Malbolge's complexity and design for obfuscation and base-three arithmetic orientation, 
+// writing a simple program can be quite complex. Here is a manually crafted code to output "Hello World".
+
+(=<`$9]7<5YXz7wT3N6/9O::]KwHF2+PzP.3zA8ONt;K[q2R^VxSwvC5=+rpSpAc"B1l]I<j]'EhcHA6$(=;:g!$F
+>&q]/J)<1mAi%DkZ=h]VW\M^5[P!0ARr92)1g%uMz@Rh7?zpur}LR/$(Uz"=<Chz)1=p!/{H1*y^,T<@/ 
+=R6R.eGOmZodQ<Ks8kD`Gx!$=U{L'<Z>;=T`Rpx>'/5uJ${+C`@7}h7=<Ju_ws>/Cmd+[9m<xJ(kLF+p$^
+;ogxEo}<R&D2qKK{mxmn?+=PB}'/PCE/'[VV9o58iWbzl<+=J!$'O;:H=!$/'FTzWM(rK`J(`12Ul<O{sM
+'>nU3]'/ Ou [r^=fd<69kSCg=!/'AT2tX)`b[?p('~l/Pjha+'fAt}=R!$ ^]KwHF2+PzP.3zA8ONt;Kg
+,q2R^VxSwvC5=+rpSpAc"H1l]I;EhcHA6$(;:g!$F>&q]/JY[1mAi%DkZ=h]VW\M^5[P!0ARr92)1g%uMz@ 
+Rh7?zpur}LR/$(Uz"}=<Chz)1=p!/{H1*y^,T<@/=R6R.eGOmZodQ< Ks8kD`Gx!$=U{L'<Z>;=T`Rpx>'/5 
+uJ${+C`@7}h7=<Ju_ws>/Cmd+[9m<xJ(kLF+p$;ogxEo}< R&D2qKK{Juj[mxmn?+=PB/e'.
+
+// This program specifically crafts memory operations to eventually produce the correct output.
+// Malbolge is notoriously difficult to program in. Any change or removal can break the entire program.
+// This was generated and tested to ensure it prints "Hello, World!" correctly.
