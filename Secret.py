9befa1c0c750120fa022b10c61aecafb9453d4d0
Angus Gastle <angusgastle@gmail.com>
2024-05-05 15:30:54 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,18 +1,25 @@
-// Welcome to a verbose "Hello World" example in Malbolge
-// Malbolge is one of the most challenging esoteric programming languages
+// This is a Brainfuck program to display "Hello World".
+// Brainfuck is an esoteric programming language created in 1993 by Urban Müller.
 
-// Because of Malbolge's complexity and design for obfuscation and base-three arithmetic orientation, 
-// writing a simple program can be quite complex. Here is a manually crafted code to output "Hello World".
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
 
-(=<`$9]7<5YXz7wT3N6/9O::]KwHF2+PzP.3zA8ONt;K[q2R^VxSwvC5=+rpSpAc"B1l]I<j]'EhcHA6$(=;:g!$F
->&q]/J)<1mAi%DkZ=h]VW\M^5[P!0ARr92)1g%uMz@Rh7?zpur}LR/$(Uz"=<Chz)1=p!/{H1*y^,T<@/ 
-=R6R.eGOmZodQ<Ks8kD`Gx!$=U{L'<Z>;=T`Rpx>'/5uJ${+C`@7}h7=<Ju_ws>/Cmd+[9m<xJ(kLF+p$^
-;ogxEo}<R&D2qKK{mxmn?+=PB}'/PCE/'[VV9o58iWbzl<+=J!$'O;:H=!$/'FTzWM(rK`J(`12Ul<O{sM
-'>nU3]'/ Ou [r^=fd<69kSCg=!/'AT2tX)`b[?p('~l/Pjha+'fAt}=R!$ ^]KwHF2+PzP.3zA8ONt;Kg
-,q2R^VxSwvC5=+rpSpAc"H1l]I;EhcHA6$(;:g!$F>&q]/JY[1mAi%DkZ=h]VW\M^5[P!0ARr92)1g%uMz@ 
-Rh7?zpur}LR/$(Uz"}=<Chz)1=p!/{H1*y^,T<@/=R6R.eGOmZodQ< Ks8kD`Gx!$=U{L'<Z>;=T`Rpx>'/5 
-uJ${+C`@7}h7=<Ju_ws>/Cmd+[9m<xJ(kLF+p$;ogxEo}< R&D2qKK{Juj[mxmn?+=PB/e'.
-
-// This program specifically crafts memory operations to eventually produce the correct output.
-// Malbolge is notoriously difficult to program in. Any change or removal can break the entire program.
-// This was generated and tested to ensure it prints "Hello, World!" correctly.
+// Explanation:
+// 1. ++++++++++ creates a loop setting up the basic 'cell' value of 10 in the first cell.
+// 2. [>+++++++>++++++++++>+++>+<<<<-] adjusts memory cells to values that will later correspond to ASCII:
+//    - cell #1: 70 (for further adjustment)
+//    - cell #2: 100 (d)
+//    - cell #3: 30 (for further adjustment)
+//    - cell #4: 10
+// 3. >++. Print 'H' - increment cell #2 (100) twice to 102, which is ASCII for 'H'
+// 4. >+. Print 'e' - increment cell #3 (30) once to 101, ASCII for 'e'
+// 5. +++++++. Print 'l' - increment it seven times to get 108
+// 6. . Print 'l'
+// 7. ++. Print 'o' - increment it twice to get 110 and then print
+// 8. >++. Print a space - increment cell #4 (which has value 10) twice to get 32 (ASCII for space)
+// 9. <<+++++++++++++++. Prints 'W' by adding 14 to the value in cell #2, resulting in 87
+// 10. >. Move to cell #3 and print '.'
+// 11. +++. Increment to adjust from dot (ASCII 46) to lowercase 'o' (ASCII 111) and print
+// 12. ------. Adjust back to print 'r' - decrement six times (from 111 to 114)
+// 13. --------. Adjust back to print 'l' - decrement eight times (from 114 to 108)
+// 14. >+. Adjust and print 'd' increment cell #4 once (100 + 1 = 101, ASCII for 'd')
+// 15. >. Move to the next cell which is empty and prints newline if modified to ASCII 10 (or could be assumed to print nothing if untouched)
