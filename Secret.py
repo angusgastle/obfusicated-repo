34083f8f6ecc636dcad024028e501fd79159ccff
Angus Gastle <angusgastle@gmail.com>
2024-05-07 03:30:46 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,193 +1,37 @@
-whitespace
-     	
-  	
-		 
-  
-  		
-    	
-		  
-    
-  	
-		  
-  	  
- 	
-  		   
-		  
-  		 
-   
-  	       
-  	
-   
+// Intercal script to print "Hello World" with unnecessary complexity
 
-   
-     
- 		  
- 		   
-        
-    	
-		  
-		    
-  	
-  		  
-		  
-			  	  
-		
-     
-  		  
-		  
-  			  
-    
-  		  
-		  
-			    
-	  
-  		 
-		  
-  	     
-		 
-	
-    
-  	
-		  
-  		  
- 	
-  		 
-		  
-  		    
-		 
-  
-  		  
-		  
-  		  	  
-		 
-  
-   	  
-		  
-  		    
- 		  
-		  
-  	     
-		  
-    
-  	
-		  
-  		  
-		 
-    
-  		   
-		  
-         
-	 
-  		  
-		  
-  			  
+DO ,1 <- #13             // Assign constant to the variable 
+PLEASE DO ,2 <- #238      // Another variable assignment with some bigger number
+DO ,3 <- #112             // More variable usage for diversification
+DO ,4 <- #0               // A variable set to zero for operations
 
-  		 
-		  
-  			  
-	 
-  		 
-		  
-  			   
+PLEASE DO ,5 <- ',1       // Copy the value from ,1 to ,5
+DO ,6 <- ',2              // Copy the value from ,2 to ,6
+PLEASE DO ,7 <- ',3       // Copy the value from ,3 to ,7
+DO ,8 <- ',4              // Copy the value from ,4 to ,8
 
-  		 
-		  
-  			    
-		 
-  
-  		  
-		  
-  			  
+(1) PLEASE DO ,1 SUB #1 <- #72  // H
+DO ,2 SUB #1 <- #69             // E
+DO ,3 SUB #1 <- #76             // L
+DO ,4 SUB #1 <- #76             // L
+DO ,5 SUB #1 <- #79             // O
+DO ,6 SUB #1 <- #32             // space
+DO ,7 SUB #1 <- #87             // W
+DO ,8 SUB #1 <- #79             // O
+DO ,9 SUB #1 <- #82             // R
+DO ,10 SUB #1 <- #76            // L
+DO ,11 SUB #1 <- #68            // D
 
-   		   
+(2) PLEASE DO ,12 <- #10
+DO ,13 <- ',12
+PLEASE DO ,14 <- #13           // Put the new line carriage return
 
- 		   
-        
-		  
-  		    
-   		  
-    
-  		
-		  
-  		  
-    
-  		
-		  
-  		     
-
- 		 
-		  
-  	       
-
-  		 
-		  
-  		      
-
- 		   
-		  
-  		      
-    
-  		    
-		  
-  			  
-
- 		  
-		  
-  			  
-
- 		      
-		  
-  	     
-  	
-  		  
-		  
-  			   	  
-	  
-    
-  		  
-		  
-  	        
-	  
-  		  
-		  
-  		      
-
-	  
-  		 
-		  
-  		     
-	  
-  		  
-		  
-  		        
-	  
-    
-  		  
-		  
-  	     
-		 
-  		  
-		  
-  			       
-
-  		    
-		  
-  			  	  
-		
-  		 
-		  
-  	     
- 		  
-  		  
-		  
-  	       
-		 
-  	
-		  
-  		   
-
-
-This Whitespace program will output "Hello World" when run. Whitespace is an esoteric programming language that uses only whitespace characters (spaces, tabs, and newlines).
-
-- The program consists of multiple lines, and the specific instructions are encoded in the presence or absence of these whitespace characters.
-- This makes the code non-visible and difficult to read unless presented in an editor that can highlight these characters.
+// Print each character on a newline
+DO ,20 <- #1
+DO COME FROM (3)
+PLEASE WRITE IN ,20
+DO ,20 SUB #1 <- ',20 SUB #1
+PLEASE DO ,20 <- ',20 + #1
+DO (3) NEXT
+PLEASE DO (2) NEXT WHEN ',20 SUB #1 ~< ',14     // Loop until all characters are printed
+DO GIVE UP
