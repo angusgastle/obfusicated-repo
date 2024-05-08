c7f066f5d0243260c25f9ca753bd40ede98e6681
Angus Gastle <angusgastle@gmail.com>
2024-05-07 21:30:37 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,17 +1,48 @@
-esoteric
-Brainfuck program to output "Hello World":
-
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]
->++.          // Prints 'H'
->+.           // Prints 'e'
-+++++++.      // Prints 'l'
-.             // Prints 'l'
-+++.          // Prints 'o'
->++.          // Prints space
-<<+++++++++++++++.
->.            // Prints 'W'
-+++.          // Prints 'o'
-------.       // Prints 'r'
---------.     // Prints 'l'
->+.           // Prints 'd'
->.            // Prints newline
+whitespace
+	S S S T	T S S S S L		push "H" ASCII 72
+	T	L
+	S S S T S S S T T L			push "e" ASCII 101
+	T	L
+	S S S T S T L				push "l" ASCII 108
+	T	L
+	S S S T S T L				push "l" again
+	T	L
+	S S S T T S S L				push "o" ASCII 111
+	T	L
+	S S S T T S S S T L			push " " ASCII 32 (space)
+	T	L
+	S S S T S S S T S S T L		push "W" ASCII 87
+	T	L
+	S S S T S S S S L			push "o" again
+	T	L
+	S S S T T S S L				push "r" ASCII 114
+	T	L
+	S S S T S T L				push "l" again
+	T	L
+	S S S S T S S S T L			push "d" ASCII 100
+	T	L
+	S L						clear the stack (ready to print)
+	N
+	T S T L					print top of the stack
+	T	L
+	T S T L					print top of the stack
+	T	L
+	T S T L					print top of the stack
+	T	L
+	T S T L					print top of the stack
+	T	L
+	T S T L					print top of the stack
+	T	L
+	T S T L					print top of the stack
+	T	L
+	T S T L					print top of the stack
+	T	L
+	T S T L					print top of the stack
+	T	L
+	T S T L					print top of the stack
+	T	L
+	T S T L					print top of the stack
+	T	L
+	T S T L					print top of the stack
+	T	L
+	L
