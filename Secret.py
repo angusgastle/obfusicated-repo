b8ae35fb8d5fc922962c40c4685f517531b3fd9b
Angus Gastle <angusgastle@gmail.com>
2024-05-08 21:30:32 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,82 +1,12 @@
-whitespace
-	S S S T	T S S S L
-	T	L
-	S S S T	T T	T S S L
-	T	L
-	S S S T	T T	T T	L
-	S S S T	T T	T T	T L
-	T	L
-	S S S T	T T	T S S S L
-	T	L
-	S S L
-	L
-	L
-	T	S S S T	S L
-	S S S T	T T	T T	L
-	S S S T	T S S S L
-	T	L
-	S S S T	T T	T T	T L
-	T	L
-	S S S T	T T	T S S S L
-	T	L
-	L
-	L
-	T	S S S T	S L
-	S S S T	T T	S S L
-	T	L
-	S S S T	T T	T S T L
-	T	L
-	S L
-	L
-	L
-	T	S S S T	S L
-	S S S T	T S S S L
-	T	L
-	S S S T	T T	S S L
-	T	L
-	S L
-	L
-	L
-	T	S S S T	S L
-	S S S T	S L
-	T	L
-	S L
-	L
-	L
-	T	S S S T	S L
-	S S S T	T S S T L
-	T	L
-	S S S T	T T	T S S L
-	T	L
-	S S L
-	L
-	L
-	T	S S S T	S L
-	S S S T	T T	S L
-	T	L
-	S S S T	T T	T T	L
-	S S S T	T T	T T	L
-	S S L
-	L
-	L
-	T	S S S T	S L
-	S S S T	T S S L
-	T	L
-	S S T
-	L
-	L
-	T	S S S T	S L
-	S S S T	T T	T T	T L
-	T	L
-	S S S T	T T	T S S S L
-	T	L
-	L
-	L
-	T	S S S T	S L
-	S S S T	T S S S L
-	T	L
-	S S S T	T T	T S T L
-	T	L
-	S L
-	L
-	L
+// Befunge Program to Display "Hello World"
+// Befunge is an esoteric programming language which is designed to be as difficult to compile as possible.
+// The commands have a direct impact on the control flow of the program, including moving the instruction pointer.
+
+"dlroW olleH">              // Push "Hello World" in reverse order to the stack
+v                            // Instruct the pointer to start moving downwards
+              _@#           // Control constructs: _ horizontal if, @ end, # bridge(skip one cell)
+             >              // Instruct the pointer to move to the right
+,:,           _^#           // :, prints the top of stack as character, move pointer upwards if top is 0
+"!!!!!!!!!!!!"              // Push multiple '!' to make the stack non-zero for as long as needed
+64*-!                       // Loop control: calculate, if result non-zero will be false, moving upward
+                            // This will allow the entire "Hello World" to be printed character by character
