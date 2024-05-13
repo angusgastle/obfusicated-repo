afd28964928b72a82a8966a6e805144b5d321b31
Angus Gastle <angusgastle@gmail.com>
2024-05-12 21:30:50 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,58 +1,48 @@
-// INTERCAL program to output "Hello World"
-// INTERCAL stands for Compiler Language With No Pronounceable Acronym
+ada
+-- Ada is a structured, statically typed, imperative, and object-oriented high-level computer programming language,
+-- which is extended from Pascal and other languages. Below is an elaborately verbose version of a "Hello, World!" program.
+with Ada.Text_IO; -- This Ada package allows for basic input and output operations.
 
-INCLUDE "libc.i"          // Pull in the standard INTERCAL library
+procedure Hello_World is
+   -- Declaring a procedure named Hello_World, that will contain our main program.
 
-DO 1 <- #13               // Initial setup: taking a constant from trinary (13)
-DO 2 <- #21845            // Likewise, another constant (21845 decimal in trinary)
-DO 3 <- #2718             // Another constant for later use
+   -- Function to return the greeting string
+   function Get_Greeting return String is
+      Greeting : constant String := "Hello, World!";
+      -- 'Greeting' is a constant string that holds the words we want to print.
+   begin
+      return Greeting;
+      -- The Get_Greeting function returns the string stored in Greeting.
+   end Get_Greeting;
+   
+   -- A procedure to display a greeting message
+   procedure Display_Greeting(Message : String) is
+   begin
+      Ada.Text_IO.Put_Line(Message);
+      -- This line outputs the string passed to the procedure to the console.
+   end Display_Greeting;
 
-// We will use the following variables:
-// DO 4 to DO 12 for storing character values
+   -- A procedure to perform a countdown before displaying the greeting
+   procedure Countdown(From : Positive) is
+      -- This procedure takes a positive integer and counts down to 1
+      Temp : Positive := From;
+      -- Temp is used to hold the current number in the countdown.
+   begin
+      while Temp > 0 loop
+         Ada.Text_IO.Put_Line(Integer'Image(Temp) & "...");
+         -- Print the current number in the countdown followed by ellipsis.
+         Temp := Temp - 1;
+         -- Decrement Temp by 1.
+      end loop;
+      Ada.Text_IO.New_Line;
+      -- Print a newline for clean separation before displaying the greeting.
+   end Countdown;
 
-// Initialize the DO 1722 variable (tape location 1722) for storing
-// output string 13 characters long
-PLEASE DO 1722 <- #13
-
-// The following lines calculate the ASCII values for "Hello World!" one by one
-DO 4 <- 'H'
-DO 5 <- 'e'
-DO 6 <- 'l'
-DO 7 <- 'l'
-DO 8 <- 'o'
-DO 9 <- ','
-DO 10 <- ' '
-DO 11 <- 'W'
-DO 12 <- 'o'
-DO 13 <- 'r'
-DO 14 <- 'l'
-DO 15 <- 'd'
-DO 16 <- '!'
-
-// The .2 specifies we are referring the 2nd index, which in this case starts with 0, 1, 2...
-PLEASE STASH .2              // Store the cursor position with the STASH operation
-
-// Get ready for output by pointing at the beginning of the array
-PLEASE RETRIEVE .1           // Retrieve into the current position
-
-// Setting up looping through each character using TAIL and RABBIT
-DO 1730 <- #1                // Start of loop marker with value 1
-PLEASE DO 1731 <- #1         // End of loop marker
-
-DO 1732 <- #1                // Initialize loop counter
-PLEASE DO 1733 <- #12        // Set end limit for loop
-
-DO 1722 <- .4                // Point at the first letter to print
-PLEASE COMPUTE #39204        // Dummy computation for legal INTERCAL structure
-
-(1722) NEXT:                 // Begin loop for printing characters
-	PLEASE DO 1723 <- .7822     // Move to the next character
-	PLEASE WRITE IN ,1         // Output the character at DO 1722
-	PLEASE DO 1722 SUB #1 <- #1 // Increase address for next character
-	PLEASE .2 ~ DO 1732          // Increment loop counter
-	PLEASE RESUME .2            // Check loop count
-
-PLEASE FORGET #1              // Clean up by forgetting literal 1
-
-// Finally, output the result
-PLEASE GIVE UP               // Standard termination of an INTERCAL program
+begin
+   -- Main code block of the Hello_World procedure.
+   Countdown(5);
+   -- Call Countdown procedure starting from 5.
+   Display_Greeting(Get_Greeting);
+   -- Call Display_Greeting procedure and pass the result of Get_Greeting function.
+end Hello_World;
+-- End of the Hello_World procedure.
