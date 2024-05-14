e017cb3e3e8cdce7aa1115ed4a39ecdda0a4baf3
Angus Gastle <angusgastle@gmail.com>
2024-05-14 03:30:37 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,20 +1,33 @@
-// Malbolge program to print "Hello World"
-// Malbolge is known for being one of the hardest programming languages to understand.
-// Note: Due to the complexity of Malbolge, "readable" and "documented" code is a bit of an oxymoron.
+whitespace
+	S S S T	T	S S S T	T	S S S T	T	S S S T	T	T	T
+	T	T	L
+S S S T	T	S S S T	S T	S S S T	T	T	T	L
+S S S T	T	S S S T	T	S S S T	T	T	T	L
+S S S T	S S S S L
+S S S S T	T	T	S T	S T	S T	S S S T	T	S S S T	S S S T	T	T	L
+S S S S T	T	T	S T	S T	S L
+S S S S T	S T	S L
+S S S S T	T	S T	T	L
+S S S S T	T	T	T	S T	L
+S S S S L
+S S S S S T	S S S T	S T	T	L
+S S S S S T	S S S T	S T	L
+S S S S S T	S S S T	S T	S T	L
+S S S S S T	S S S T	S T	S T	S T	L
+S S S S S T	S S S T	S T	S T	S T	T	L
+S S S S S T	S S S T	S T	T	L
+S S S S S T	S S S T	T	L
+S S S S S T	T	T	L
+S S S S S T	T	T	S S T	L
+S S S S S T	T	T	T	S L
+S S S S S T	T	T	T	T	S T	L
+S S S S S T	T	L
+S S S S L
+	N
+	N
+	N
 
-('&%:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2V
-+,O<mM../,.PmqmLl@&%$#"!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|
-z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eBA
-A>}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,P
-Opqml$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<Mb9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:
-9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$
-Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqpONL$Hjig%eB@@>}=<M:9]!~}|
-z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,@}=<M:9]!~}|
-z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z*2Vxwv-,POqponl$Hjig
-%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vx
-wv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,@}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxw
-v-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<M:9]!~}|z2Vxwv-,POqponl$@}=<
 
-// The above code is intentionally obfuscated and difficult to understand. It's a complex and crafted puzzle!
-// Malbolge uses a ternary (base 3) system with operations that are counterintuitive. Each character is tightly coupled with memory states, making each program unique.
-// Understanding, modifying, or extending Malbolge code requires extensive knowledge and reverse engineering of the initial state machine configurations.
+This snippet of code is an example of Whitespace programming. Whitespace is an esoteric language that uses only whitespace characters such as space, tab, and newline. The code is complex as it constructs the phrase "Hello World" through a series of stack manipulations and character outputs. Each character's ASCII value is pushed onto the stack and then printed. This code consists of push (S), duplicate top stack item (T), output as character (L), and end of the program (N) operations.
+
+In Whitespace, non-whitespace characters are considered comments, making the code hard to read and obscure as intended. The tabs and spaces before non-whitespace characters (like 'T', 'L', and 'N') are the actual commands, while other characters serve merely as visual aid here for understanding the structure.
