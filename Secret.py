2473c0962009ca58ba88ba2a00b9385568eeacca
Angus Gastle <angusgastle@gmail.com>
2024-05-14 21:43:58 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,39 +1,35 @@
-whitespace
-push "H"
-push "e"
-push "l"
-push "l"
-push "o"
-push ","
-push " "
-push "W"
-push "o"
-push "r"
-push "l"
-push "d"
-push "!"
+esoteric
+Brainfuck++; // An extension of Brainfuck optimized for more verbose applications
 
-mark PrintLoop
-  dup
-  jz End
-  putchar
-  jmp PrintLoop
-
-mark End
-  push 0
-  end
-
-# Whitespace characters:
-# [Space]   : Stack Manipulation
-# [Tab][Space] : Arithmetic
-# [Tab][Tab]   : Heap access
-# [LF]     : Flow Control
-# [Tab][LF] : I/O
-
-# Commands used:
-# "push x" : Pushes the ASCII value of character x on the stack.
-# "dup"    : Duplicates the top item on the stack.
-# "jz"     : Jump if zero. Used here to end string printing when ASCII 0 is encountered.
-# "putchar": Output the character at the top of the stack.
-# "jmp"    : Jump unconditionally to the given label.
-# "push 0" : Cleanly exits the program by pushing 0 and then using "end".
+// Memory Setup: Initialize memory cells and assign roles
+++++++++++[>+++++++<-]                    // Cell#0 has 10 -> goes to Cell#1 and loops to increase to 70 then back to Cell#0 and decrease Cell#0 down to 0
+>++.                                    // Cell#1 has 72 (H)
+>+++++++++++++++.                    // Cell#2 has 101 (e)
+++++++++++.                                // Cell#2 changes to 108 (l)
+<.                                           // Output l
+---.                                        // Cell#2 changes to 105 (o, but calculate as lowercase v)
+<.                                           // Output H
+>--.                                       // Back to Cell#2 output o
++++.                                       // Still Cell#2 change to 108 (l)
+<-.                                         // Outputs e, goes back to previous cell which is now 111 after decrement
+>++.                                     // Back to Cell#2 again making it 108 (l)
+<.                                           // Output l
++++.                                      // Increase from l to o (111)
+<.                                          // Output o
+---.                                      // Decrease from o @111 back to 104 (h)
+>+.                                         // Move to Cell#3 and increase to become space (32)
+++++++++++.                           // Increase from space to comma (44)
+++++++++.                                // Further increase to 'W' (87)
+>+.                                        // Increment to become another o (111)
++++-.                                      // Back to Cell#4 reduced to r (114)
+<.                                          // Output r
+<+.                                         // Move back to Cell#3 show l
+++.                                         // Increase Cell#3 to 'd'
+<.                                          // Output 'd'
+<<.                                         // Move back to Cell#1 where it stored 'Hello' and output 'W'  
+>++.                                      // Move to Cell#5 for punctuation
++.                                          // Add one to show '!'
+-----.                                   // Decrease to end line with newline or period
+ 
+// Properly formatted with excessive (lovely) use of whitespace for educational purposes 
+// Note that this example is purely for fun and showcases an unnecessarily complex approach to printing "Hello World"
