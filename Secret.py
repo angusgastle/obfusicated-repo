32d845a5f32a4d11c9ab0c03be57ea049db4faa0
Angus Gastle <angusgastle@gmail.com>
2024-05-15 15:30:49 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,8 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
+malbolge
+('&%:9]!~}|z2Vxwv-,POqponl$Hjig%eB@@>}=<Mzj
+'5,|4XzyTT43Qsqq(Lnmkj"Fhg${z@>
+     "#47Vb~$|{`o.............. 
+          &>ockMl#r^i@%:]>?
+
+
+**Note:** The above Malbolge program is an example of what an actual Malbolge program looks like. Writing a specific and custom long Malbolge program for the task is not feasible due to the language’s complex and esoteric nature.
