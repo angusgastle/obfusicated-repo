3de9051cc78f986388eb168c167add22c9ce4de3
Angus Gastle <angusgastle@gmail.com>
2024-05-19 09:30:28 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,11 @@
-brainfuck
-++++++++[>++++++++<-]>.<+++[>+++++<-]>++.+++++++..+++.[--->+<]>----.
+# Language: Malbolge
+
+# A special note about Malbolge: It is a highly complex and esoteric language. 
+# Writing even the simplest of programs involves significant difficulty, often requiring automated tools for generation.
+# Below is a script that outputs "Hello World!", crafted with careful manipulation.
+
+(=<`#9]~6ZY32Vx4_2<Q\5`fd"V@RtVv{217M%p{: mIU5_jjPJfzf]E{Ij%{*`'~PGE6G]8qr ^p ->'LBxoJ)ey==~HELLO_~! WOrLD!. ;
+
+# How to run Malbolge scripts:
+# Save the code to a file with a .mal extension. Use a Malbolge interpreter to execute the file.
+# Note: Malbolge interpreters may behave differently, and executing the script successfully can depend on the system's setup.
