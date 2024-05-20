404c71dfa8572a8ba16d8f18e1d54fd754078084
Angus Gastle <angusgastle@gmail.com>
2024-05-20 03:30:33 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,44 +1,53 @@
-# T-SQL (Transact-SQL) Script to display "Hello World"
+// This program displays "Hello World" in INTERCAL
+// INTERCAL is an esoteric programming language designed to be as different from conventional programming languages as possible.
 
--- Creating a temporary table to hold the message
-CREATE TABLE #HelloWorldTable (
-    Message NVARCHAR(50)
-);
+DO ,1 <- #13
+DO ,1 SUB #1 <- #238
+DO ,1 SUB #2 <- #108
+DO ,1 SUB #3 <- #112
+DO ,1 SUB #4 <- #140
+DO ,1 SUB #5 <- #44
+DO ,1 SUB #6 <- #95
+DO ,1 SUB #7 <- #124
+DO ,1 SUB #8 <- #0
+DO ,1 SUB #9 <- #64
+DO ,1 SUB #10 <- #98
+DO ,1 SUB #11 <- #90
+DO ,1 SUB #12 <- #100
+DO ,1 SUB #13 <- #120
 
--- Inserting the "Hello World" message into the temporary table
-INSERT INTO #HelloWorldTable (Message)
-VALUES ('Hello World');
+DO ,2 <- #1
+DO ,3 <- #1
+DO .1 <- #1
+PLEASE DO (5) NEXT
+MS PAUSE
+DO (5) NEXT
 
--- Declaring a cursor to read from the temporary table
-DECLARE @MessageCursor CURSOR;
-DECLARE @Message NVARCHAR(50);
+(5) PLEASE DO ,1 <- #67
+PLEASE DO ,1 SUB #1 <- #72
+PLEASE DO ,1 SUB #2 <- #101
+PLEASE DO ,1 SUB #3 <- #108
+PLEASE DO ,1 SUB #4 <- #108
+PLEASE DO ,1 SUB #5 <- #111
+PLEASE DO ,1 SUB #6 <- #32
+PLEASE DO ,1 SUB #7 <- #87
+PLEASE DO ,1 SUB #8 <- #111
+PLEASE DO ,1 SUB #9 <- #114
+PLEASE DO ,1 SUB #10 <- #108
+PLEASE DO ,1 SUB #11 <- #100
+PLEASE DO ,1 SUB #12 <- #33
+PLEASE DO ,1 SUB #13 <- #64
 
--- Initializing the cursor with the data from our temporary table
-SET @MessageCursor = CURSOR FOR
-    SELECT Message
-    FROM #HelloWorldTable;
+PLEASE READ OUT ,1
 
--- Opening the cursor
-OPEN @MessageCursor;
+DO ,1 <- #818
+DO ,1 <- #819
+PLEASE RESET #1
+PLEASE DO FORGET <.1>
+DO ,1 <- #104 DE IF .1 IS LESS THAN #2
+DO .1 <- #55
+PLEASE ABSTAIN FROM (5)
+DO (5) AGAIN
+PLEASE RESUME
 
--- Fetching the first and only row from the cursor
-FETCH NEXT FROM @MessageCursor INTO @Message;
-
--- Checking if the fetch operation got any rows
-WHILE @@FETCH_STATUS = 0
-BEGIN
-    -- Displaying the "Hello World" message
-    PRINT @Message;
-
-    -- Fetching the next row (though there won't be one)
-    FETCH NEXT FROM @MessageCursor INTO @Message;
-END;
-
--- Closing the cursor
-CLOSE @MessageCursor;
-
--- Deallocating the cursor to free up resources
-DEALLOCATE @MessageCursor;
-
--- Dropping the temporary table to clean up the database
-DROP TABLE #HelloWorldTable;
+(5) PLEASE GIVE UP
