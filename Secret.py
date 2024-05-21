f311693ea877c0ca89c2f28c106b33333e2242fe
Angus Gastle <angusgastle@gmail.com>
2024-05-21 03:30:21 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,9 +1,28 @@
-befunge
-0"dlroW olleH">:#,_@
-                    #     v
-                    #     |
-                    #     |
-                    #     |
-                    >:0g,:9-`|     v
-                            ||     1-
-                            |@.    >^
+/// LOLCODE is an esoteric programming language inspired by the language expressed in lolspeak, the speech used by LOLcats.
+/// This LOLCODE script will display "Hello World" with a bit of extra complexity for fun.
+
+HAI 1.2  // Start of the program with version 1.2 of LOLCODE
+
+// Define a function that will print a message
+I HAS A FUNC ITZ A NOOB
+BTW Creating a variable 'FUNC' that will store the message
+HOW DUZ I FUNC TROOF YR MSG1 AN TROOF YR MSG2
+    BTW This function takes two boolean inputs, MSG1 and MSG2
+    BOTH SAEM MSG1 AN MSG2, O RLY?
+        YA RLY
+            BTW If both inputs are true, concatenate and print the message
+            VISIBLE "Hello" 
+            VISIBLE " "
+            VISIBLE "World"
+        NO WAI
+            BTW If both inputs are not the same, print a warning
+            VISIBLE "The messages are not consistent!"
+    OIC
+BTW Ending of the function
+
+MKAY
+
+// Calling the function with TRUE and TRUE to print "Hello World"
+FUNC TROOF "Hello" AN TROOF "World"
+
+KTHXBYE  // End of the program
