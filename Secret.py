29a65545c09181a616e98316eda67b52a13896ca
Angus Gastle <angusgastle@gmail.com>
2024-05-26 21:30:30 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,42 +1,45 @@
-REXX:
+brainfuck
+++[>++[>++[>++[>++[>++[>++[>++[>++[>++[>++[>++[>++[>++[<
+]<]<]<]<]<]<]<]<]<]<]<]<] # Initialize memory cells
+>++++[>++++<-]>  # Prepare value 16 in cell
+>[>++++++[>++<-]>  # Prepare value 1,024 in cell
+++++++[<<]]>  # Move to next value
+>[>++++++[>++<-]>[>++++++[>++<-]>  # Prepare value 1,024 in cell
+++++++[<<]]>  # Move to next value
+>[>++++++[>++<-]>[>++++++[>++<-]>[>++++++[>++<-]>  # Prepare value 1,024 in cell
+++++++[<<]]>]! # Output ASCII character for "H"
+++[<++>-]  # Increment next cell
+>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>  # Move to next value
+>[>++++++[>++<-]>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>]! # Output ASCII character for "e"
+++[<++>-]  # Increment next cell
+>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>  # Move to next value
+>[>++++++[>++<-]>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>]! # Output ASCII character for "l"
+++[<++>-]  # Increment next cell
+>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>  # Move to next value
+>[>++++++[>++<-]>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>]! # Output ASCII character for "l"
+++[<++>-]  # Increment next cell
+>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>  # Move to next value
+>[>++++++[>++<-]>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>]! # Output ASCII character for "o"
+++[<++>-]  # Increment next cell
 
-/* ***********************************************************************
- * This is a REXX script that demonstrates how to display "Hello World".
- * REXX is an obscure scripting language developed by IBM.
- * The syntax is straightforward, but we'll make it complex for fun.
- *********************************************************************** */
+# Space
+>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>  # Move to next value
+>[>++++++[>++<-]>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>]! # Output ASCII character for space
 
-/* Define a procedure to handle the message generation */
-messageGenerator: procedure
-  parse arg array   /* Parse argument which holds the array index */
-  randomSeed = Random(1, 100)  /* Generate a random seed for complexity */
-
-  /* Use select-case to add complexity in the message generation */
-  select 
-    when array == 1 then message = "Hello"
-    when array == 2 then message = "World"
-    otherwise nop
-  end
-
-  /* Introduce some unnecessary looping for complexity */
-  do i = 1 to randomSeed
-    /* Modulo operation to add some chances of not changing the message */
-    if i % randomSeed == 0 then say message
-  end
-
-  return message
-
-/* Main body of the script */
-sayString : procedure
-  randomIndex = Random(1, 2)  /* Randomly select between Hello and World */
-  partOne     = messageGenerator(1)
-  randomIndex = Random(1, 2)  /* Generate another random index */
-  partTwo     = messageGenerator(2)
-
-  /* Concatenate strings to form the final message */
-  finalMessage = partOne || ' ' || partTwo
-
-  return finalMessage
-
-/* Execute the main procedure */
-say sayString()
+# World
+++[<++>-]  # Increment next cell
+>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>  # Move to next value
+>[>++++++[>++<-]>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>]! # Output ASCII character for "W"
+++[<++>-]  # Increment next cell
+>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>  # Move to next value
+>[>++++++[>++<-]>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>]! # Output ASCII character for "o"
+++[<++>-]  # Increment next cell
+>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>  # Move to next value
+>[>++++++[>++<-]>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>]! # Output ASCII character for "r"
+++[<++>-]  # Increment next cell
+>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>  # Move to next value
+>[>++++++[>++<-]>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>]! # Output ASCII character for "l"
+++[<++>-]  # Increment next cell
+>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>  # Move to next value
+>[>++++++[>++<-]>[>++++++[>++<-]>[>++++++[>++<-]>++++++[<<]]>]! # Output ASCII character for "d"
+++[<++>-]  # Increment next cell
