440ec35dce42bcd0e6d4be368ffadf546db9508f
Angus Gastle <angusgastle@gmail.com>
2024-05-28 03:30:44 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,93 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>>++.>+.+++++++..+++.>++.
+Here is a "Hello World" script in Intercal, a language intentionally designed to be as different from conventional programming languages as possible:
+
+intercal
+PLEASE DO .1 <- #13
+PLEASE DO .2 <- #13
+PLEASE DO .3 <- #4
+PLEASE DO .4 <- #1
+PLEASE DO .5 <- #0
+PLEASE DO .6 <- #256
+PLEASE DO .7 <- #256
+PLEASE DO .8 <- #14
+PLEASE DO .9 <- #2
+PLEASE DO .7 <- .6 ~# .1
+PLEASE DO .6 <- .6 ~# .2
+PLEASE DO .7 <- .7 ~# .1
+PLEASE DO .6 <- .6 ~# .1
+PLEASE DO .7 <- .7 ~# .3
+PLEASE DO .7 <- .6 ~# .3
+PLEASE DO .6 <- .6 ~# .3
+PLEASE DO .7 <- .7 ~# .4
+PLEASE DO .6 <- .6 ~# .4
+PLEASE DO .7 <- .7 ~# .1
+PLEASE DO .6 <- .6 ~# .1
+PLEASE DO .7 <- .7 ~# .1
+PLEASE DO .6 <- .6 ~# .1
+PLEASE DO .7 <- .7 ~# .3
+PLEASE DO .6 <- .6 ~# .3
+PLEASE DO .7 <- .7 ~# .4
+PLEASE DO .6 <- .6 ~# .4
+PLEASE DO .7 <- .7 ~# .1
+PLEASE DO .6 <- .6 ~# .1
+PLEASE DO .7 <- .7 ~# .4
+PLEASE DO .6 <- .6 ~# .4
+PLEASE DO .6 <- .6 ~# .3
+PLEASE DO .5 <- .6 ~# .3
+PLEASE DO .5 <- .5 ~# .4
+PLEASE DO .5 <- .5 ~# .1
+PLEASE DO .5 <- .5 ~# .3
+PLEASE DO .5 <- .5 ~# .6
+PLEASE DO .5 <- .5 ~# .7
+PLEASE DO .8 <- .7 ~# .4
+PLEASE DO .9 <- .9 ~# .3
+PLEASE DO .5 <- .5 ~# .8
+PLEASE DO .5 <- .5 ~# .3
+PLEASE DO .5 <- .5 ~# .4
+PLEASE DO .5 <- .5 ~# .4
+PLEASE READ OUT .5 ~# .6
+
+PLEASE STASH .6 ~# .5
+PLEASE DO .1 <- #5
+PLEASE DO .2 <- #6
+PLEASE DO .3 <- #2
+PLEASE DO .4 <- #255
+PLEASE DO .5 <- .1 ~# .2
+PLEASE DO .5 <- .5 ~# .3
+PLEASE DO .5 <- .5 ~# .4
+PLEASE DO .5 <- .1 ~# .2
+PLEASE DO .5 <- .5 ~# .5
+PLEASE DO .5 <- .1 ~# .5
+PLEASE DO .1 <- .1 ~# .5
+PLEASE DO .1 <- .1 ~# .2
+PLEASE DO .1 <- .1 ~# .3
+PLEASE DO .1 <- .1 ~# .4
+PLEASE DO .1 <- .1 ~# .5
+PLEASE DO .1 <- .1 ~# .6
+PLEASE DO .1 <- .1 ~# .7
+PLEASE DO .1 <- .1 ~# .8
+PLEASE DO .1 <- .1 ~# .9
+PLEASE DO .5 <- .5 ~# .1
+PLEASE DO .1 <- .1 ~# .8
+PLEASE DO .5 <- .1 ~# .3
+PLEASE DO .5 <- .1 ~# .4
+PLEASE DO .5 <- .5 ~# .5
+PLEASE DO .5 <- .5 ~# .3
+PLEASE DO .5 <- .4 ~# .5
+PLEASE DO .5 <- .5 ~# .5
+PLEASE DO .5 <- .1 ~# .2
+PLEASE DO .5 <- .1 ~# .6
+PLEASE DO .5 <- .1 ~# .6
+PLEASE DO .5 <- .1 ~# .1
+
+PLEASE STASH .6 ~# .1
+PLEASE DO ,1 <- #72
+PLEASE DO ,2 <- #101
+PLEASE DO ,3 <- #108
+PLEASE DO ,4 <- #111
+PLEASE DO ,5 <- #32
+PLEASE DO ,6 <- #87
+PLEASE DO ,7 <- #114
+PLEASE DO ,8 <- #100
+PLEASE DO ,9 <- #10
+
+PLEASE FORGET #1
