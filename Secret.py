1bd209cfde4a83250ffeb2c0edd72f9cfd42e3c8
Angus Gastle <angusgastle@gmail.com>
2024-06-01 09:30:27 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,41 +1,12 @@
-# Language: INTERCAL (a parody programming language known for its complexity and obfuscation)
-# The following script will display "HELLO WORLD" using INTERCAL's unique and obscure syntax.
-
-DO ,1 <- #13
-DO ,1 SUB #1 <- #72
-DO ,1 SUB #2 <- #69
-DO ,1 SUB #3 <- #76
-DO ,1 SUB #4 <- #76
-DO ,1 SUB #5 <- #79
-DO ,1 SUB #6 <- #32
-DO ,1 SUB #7 <- #87
-DO ,1 SUB #8 <- #79
-DO ,1 SUB #9 <- #82
-DO ,1 SUB #10 <- #76
-DO ,1 SUB #11 <- #68
-DO ,2 <- #1
-(NEXT)
-(1) PLEASE READ OUT ,1 SUB #1
-PLEASE DO ,2 <- ,2 ~ #1
-PLEASE DO ,2 <- ,2 / #1
-PLEASE DO ,2 <- ,2 + #1
-(2) DO ,3 <- #1
-DO ,3 SUB #1 <- #0
-DO ,4 <- #1
-(3) PLEASE DO ,4 <- ,4 ~ #1
-PLEASE DO ,4 <- ,4 / #1
-PLEASE DO ,4 <- ,4 + #1
-PLEASE WRITE IN ,4 SUB #3
-PLEASE DO ,4 <- ,4 ~ #2
-PLEASE DO ,4 <- ,4 / #2
-PLEASE DO ,4 <- ,4 + #2
-PLEASE WRITE IN ,4 SUB #4
-PLEASE DO ,4 <- ,4 ~ #3
-PLEASE DO ,4 <- ,4 / #3
-PLEASE DO ,4 <- ,4 + #3
-PLEASE WRITE IN ,4 SUB #5
-DO ,5 <- ,2
-DO ,6 <- #1
-(4) DO ,1 SUB #6 <- ,1 SUB #1
-DO ,6 <- ,6 + #1
-PLEASE RESUME (1)
+brainfuck
+>++++++++[<+++++++++>-]<.                   Output 'H'
+>++++[<+++++++>-]<+.                        Output 'e'
++++++++..+++.                               Output 'l', 'l', 'o'
+>>>++++++++[<++++>-]<.                      Prepare for space character 
+>++.                                       Output ' '
+>++++[<+++++++>-]<++.                       Output 'W'
+>++[<+++++>-]<.                             Output 'o'
+>++++++[<+++++++>-]<-.                      Output 'r'
+>+++++++++++[<+++++>-]<-.                   Output 'l'
+>+++++[<+++++>-]<+.                         Output 'd'
+>+++[<+++-]>-.++.                           Output '!' and newline
