3000caeac2b1a459ed3b9a7121a7f913670240c3
Angus Gastle <angusgastle@gmail.com>
2024-05-31 21:30:23 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,18 +1,13 @@
-// Malbolge Program to Display "Hello World"
-
-// Note: Malbolge is an esoteric programming language known for its complexity.
-// The code and comments provided below are for demonstration purposes.
-
-// Code generated using a Malbolge code generator
-('&%:9]!~}|z2Vxwv-,POqpon+0/.-mlk**hg#B"v[Lwv-,POqpo
-~}|yuONTmlPOqpon qpUTSRq@;;
-
-< :9]var}uxwvPO UT_-mlkjiXgfe#B~}|yu[ysw}|y{|xwp
-"vw|xwv|wv4rpmlkjUTSR qpo|xwv4rp}}uONTv[wv-,POqpo
-mlkjihgf@\^z]#Wyxwvjpo76p4vutsrqpo+,i]_SR3/.?,9]-5
-76543/.mlkjihgf#Bo2olit+vutsr3qpo876+kjggXXfg~}|xu
- }yrSTSxwv~,|wvutsrqponmlkjUTSRqponmlkjWpONyhgfDCB"]+lM"y|yZyxwv~}|z
-
-// The code above will output "Hello, World!" in Malbolge.
-// Due to the nature of Malbolge, it can be extremely difficult to understand.
-// Enjoy the complexity!
+brainfuck
+>++++++++[<++++++>-]<.                 [ Initialize cell 1 with 48; Output 'H' ]
+>++++[<+++++++>-]<.                    [ Initialize cell 1 with 32; Output 'e' ]
++++++++++++.                           [ Output 'l' ]
++.                                     [ Output 'l' ]
++++[>+++[>++++<-]<-]>>.                [ Output 'o' ]
+<.                                     [ Output ' ' ]
+>-.                                    [ Output 'W' ]
+>++++[<---->-]<.                       [ Output 'o' ]
+>++[<+++++++[>++<-]>+]<.               [ Output 'r' ]
+>++++++++.                             [ Output 'l' ]
+>+++.                                  [ Output 'd' ]
+>-----.                                [ Output '!' ]
