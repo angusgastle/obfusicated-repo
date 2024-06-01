cae740390cd6a1d27e7f93feb22ea15ce5b3fb1f
Angus Gastle <angusgastle@gmail.com>
2024-06-01 15:30:25 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,12 +1,19 @@
-brainfuck
->++++++++[<+++++++++>-]<.                   Output 'H'
->++++[<+++++++>-]<+.                        Output 'e'
-+++++++..+++.                               Output 'l', 'l', 'o'
->>>++++++++[<++++>-]<.                      Prepare for space character 
->++.                                       Output ' '
->++++[<+++++++>-]<++.                       Output 'W'
->++[<+++++>-]<.                             Output 'o'
->++++++[<+++++++>-]<-.                      Output 'r'
->+++++++++++[<+++++>-]<-.                   Output 'l'
->+++++[<+++++>-]<+.                         Output 'd'
->+++[<+++-]>-.++.                           Output '!' and newline
+; Programmer's Note: The following code is written in Intercal, a purposely esoteric and obscure programming language. This script will print "HELLO WORLD" to the console.
+
+DO ,1 <- #13
+DO ,1 SUB #1 <- #72
+DO ,1 SUB #2 <- #69
+DO ,1 SUB #3 <- #76
+DO ,1 SUB #4 <- #76
+DO ,1 SUB #5 <- #79
+DO ,1 SUB #6 <- #32
+DO ,1 SUB #7 <- #87
+DO ,1 SUB #8 <- #79
+DO ,1 SUB #9 <- #82
+DO ,1 SUB #10 <- #76
+DO ,1 SUB #11 <- #68
+DO ,1 SUB #12 <- #33
+DO ,1 SUB #13 <- #0
+
+READ OUT ,1
+GIVE UP
