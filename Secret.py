00cdacf82d3291b4c20a797c39d2f43c81f0cd6e
Angus Gastle <angusgastle@gmail.com>
2024-06-03 15:30:50 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,42 +1,32 @@
-// Language: INTERCAL (Complicated Hello World Example)
 
-/* 
-   This INTERCAL program prints "Hello World". 
-   INTERCAL is a programming language designed to be as different from 
-   conventional programming languages as possible.
-*/
+/*
+ * Slightly esoteric "Hello World!" program in INTERCAL.
+ * It was designed to be different from all other programming languages.
+ */
+DO ,1 <- #13
+DO ,1 SUB #1 <- #'H'
+DO ,1 SUB #2 <- #'e'
+DO ,1 SUB #3 <- #'l'
+DO ,1 SUB #4 <- #'l'
+DO ,1 SUB #5 <- #'o'
+DO ,1 SUB #6 <- #32    ; ASCII space
+DO ,1 SUB #7 <- #'W'
+DO ,1 SUB #8 <- #'o'
+DO ,1 SUB #9 <- #'r'
+DO ,1 SUB #10 <- #'l'
+DO ,1 SUB #11 <- #'d'
+DO ,1 SUB #12 <- #'!'
+DO ,1 SUB #13 <- #0    ; Null terminator for output
 
-// Load library for I/O functions
-DO ,1 <- #72
-DO ,1 <- #101
-DO ,1 <- #108
-DO ,1 <- #108
-DO ,1 <- #111
-DO ,1 <- #32
-DO ,1 <- #87
-DO ,1 <- #111
-DO ,1 <- #114
-DO ,1 <- #108
-DO ,1 <- #100
-DO ,1 <- #33
-DO READ OUT ,1
+PLEASE DO ,1 <- #1     ; Just a formality in INTERCAL, moving content into main
 
-// Define storage arrays to hold the character values
-PLEASE DO   :1  <-  #1
-PLEASE DO   :2  <-  #1
+; Loop over and display characters until null terminator is reached
+(1) NEXT:
+    DO FORGET #2       ; Prepare for next character
+    DO READ OUT ,1 SUB #1
+    DO .1 <- "ON"~#1  ; Modify control variable to point to next character in string
+    DO (NEXT) NEXT!0(#1)     ; Loop condition check
 
-// Loop through the array to print each character
-PLEASE DO AGAIN(1) NEXT
-PLEASE DO   :1  <-  :1 + #1
-PLEASE DO   (11) SUB #1
-PLEASE DO   WRITE IN ,1
-PLEASE DO   FORGET #1
-PLEASE DO   (1) SUB #1
-
-// Ending the loop
-PLEASE DO   :2  <-  :1 ~ #38
-PLEASE DO RESUME(1) NEXT
-PLEASE DO STOP
-
-// Execute the program to display "Hello World"
-PLEASE DO   EXECUTE
+DO .1 <- "FF"
+DO RESUME NEXT
+DO GIVE UP
