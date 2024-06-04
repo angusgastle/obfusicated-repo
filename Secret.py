2f2272d0c6327c8efbd22944cf3d60d9d4db79f4
Angus Gastle <angusgastle@gmail.com>
2024-06-04 15:30:42 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,36 +1,23 @@
-(Obscure language: Malbolge)
+APL
 
-(Please note that Malbolge is intentionally difficult to understand and isn't generally used for practical programming. The following code extensively uses the language's syntax and constructs to achieve the "Hello World" output.)
+⍝ Define a function to display "Hello World"
+DisplayHelloWorld ← {
+    ⍝ Create a character vector with each character of "Hello World"
+    message ← 'Hello World'
+    
+    ⍝ Display each character with a delay to simulate complexity
+    {
+        ⍺ ← 0.1 ⍝ Set a short pause interval
+        ⎕DL ⍺   ⍝ Wait for the interval
+        ⍵       ⍝ Return the character
+    }¨ message  ⍝ Map the delay function to each character of the message
+    
+    ⍝ Join the characters back to a single string using the 'Reduce' operator /
+    finalMessage ← ∊ message
+    
+    ⍝ Display the final message
+    ⍞←finalMessage
+}
 
-(Decoded and crafted specifically to be complex)
-
-( Init Malbolge program with junk characters )
-( This will eventually print "Hello World" )
-('&%:9]!~}|zcvwaONMLIJKLMNOPmnopqrponmlkj
-ihgfedcbSksjihgONMLIJKL}{wvu8<#"!ZYX~~~~|
-( Diamond pattern to intrigue inner processing)
-*=$T-]
-
-( Complex NOP instructions )
->,'.%a9":+/,cba&[cba*918e&[<`%!%]*$%#S:
-NOXEXQFEWON;)
-|222O!EDB?+}|9!|pv|8}|9u|9r8=!#
-^~})!b7+~&<-}{^+['@&*(:-#L!]+[}^;678#~`!
-NOBCD}}<+++|}EDo|abcd
-
-( Memory manipulation to reach central display routine )
- '�~!#{}:<42u#:<"pnovNDC_:>zRdcvponog
-
-( Begin encoded commands holding the values)
- AYK30<0=T=+++}234S9J6aJH=ADJY'Hi-R0G! UTpocnLG1Ts@&qp5w3U'
-ol#nmp2%vfR/<.'>R%eHj+^C]-rk^kfpK@qvtSJ509
-Og.Td29IM32#7V. {78FD#8ka0}'.dF:+XhQ&Xk)Kb#9P!^3
-
-( Finish and cleanup )
-|1110988@~]B^)#!@&()!}3
-
-( Mirroring section to satisfy interpreter )
-_dc\c%"`j=!}>%;">!^c*#&"!^+=1`43c#pg!g'~|!
-3333445523@!@~;/[/
-
-( The processing ends here )
+⍝ Execute the function
+DisplayHelloWorld ⍬
