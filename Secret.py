141330640c48825ea5cde10ad51d876e3adbe095
Angus Gastle <angusgastle@gmail.com>
2024-06-05 15:30:46 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,26 +1,20 @@
-Befunge-98:
+Burlesque (a concatenative programming language)
 
-befunge
->>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>
- v vv v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v
- 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 
- # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
- " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " 
- v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v 
- v > " e l l o " < > 25g:@# 
- v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v 
- ^" " " " " " " " 
-
-# Initialize the stack with values corresponding to "Hello World!"
-99 , 60 , 80 , 63 , 75 , 50 , 97 , 100 , 20 , 40 , 111 , !
-
-# Push each character of "Hello World!" onto the stack in reverse order
-"!dlroW olleH"
-
-# Redirect execution to print out the stack content
- > : 96,00g96.*, # ~ # <
- ~ v ! "3- 2 Valhnaver".v:.,@
-
-# End of the code
-$ . :
- 
+; Define the string "Hello, World!"
+"Hello, World!" 
+; Assign the string to a variable
+dup
+; Reverse the string (not necessary for display, but adds complexity)
+rev
+; Convert the reversed string to a list of characters
+list
+; Sort the list of characters to add more complexity
+sort
+; Convert the sorted list back to a string
+fromlist
+; Push the original string back onto the stack (as the result of the sort-fromlist operations)
+pop
+; Append a new line character to the string (for display purposes)
+"\n" app
+; Display the final string
+print
