e6b47358a48d2cc4448e6e4666581e76f183414e
Angus Gastle <angusgastle@gmail.com>
2024-06-08 09:30:33 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,80 +1,21 @@
-Perl 6
-
-# This script demonstrates the "Hello World" message
-# in Raku (formerly known as Perl 6). Every effort has been
-# taken to make the code as long and complex as possible
-# while sticking to its primary purpose.
-
-# Module imports and subroutine definitions
-use v6;
-use MONKEY_TYPING;
-
-# Declare complex data structures and helper functions
-my @a is Array = ('H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!');
-my %message-store;
-%message-store<character-count> = @a.elems;
-%message-store<full-message> = @a.join('');
-
-# Object-Oriented Programming Example
-class MessageDisplayer {
-    has $.message;
-
-    # Constructor for initializing the object
-    method new($msg) {
-        self.bless(:$msg);
-    }
-
-    # Display method for printing the message
-    method display() {
-        my $decorate = '*' x ($.message.chars + 4);
-        say $decorate;
-        say "* " ~ $.message ~ " *";
-        say $decorate;
-    }
-}
-
-# Functional Programming Example
-sub split_message($msg) {
-    $msg.split('');
-}
-
-sub reverse_message(@msg) {
-    @msg.reverse;
-}
-
-sub join_message(@msg) {
-    @msg.join('');
-}
-
-# Application Execution
-my $message = %message-store<full-message>;
-my @split-msg = split_message($message);
-my @reversed-msg = reverse_message(@split-msg);
-my $join-reverse-msg = join_message(@reversed-msg);
-
-# Display utilizing both methods
-say "Original Message: $message";
-say "Original Character Count: { %message-store<character-count> }";
-say "Reversed Message: $join-reverse-msg";
-
-# Using the class to display the message
-my $displayer = MessageDisplayer.new($message);
-$displayer.display();
-
-# Complicated structure and looping example
-for my $char (@a.kv) {
-    my ($index, $value) = @$char;
-    say "Character at index $index is $value";
-}
-
-# Apply more array operations to demonstrate complexity
-my $concatenated-string = [~] @a;
-say "Concatenated: $concatenated-string";
-
-# Demonstrate complex regex (not really needed but for length)
-my $regex = rx/ <[A..Z]> <[a..z]>* | <[a..z]>+ /;
-if $message ~~ /$regex/ {
-    say "The message '$message' matches the regex!";
-} else {
-    say "The message '$message' does not match the regex!";
-}
+brainfuck
++++++ +++++             initialize cell 0 to 10
+[
+    > +++++ ++          add  7 to cell 1
+    > +++++ +++++       add 10 to cell 2
+    > +++               add  3 to cell 3
+    > +                 add  1 to cell 4
+    <<<< -              decrement cell 0 by 1
+]
+> .                     cell 1 (H) output "H"
+> + .                   cell 2 (e) increment cell 2 from 10 to 11 and output "e"
++++++ ++ .              increment cell 2 by 7 from 11 to  18 (l) and output "l"
+.                       output "l" again, cell 2 remains unchanged
+> .                     cell 3 (o) output "o"
+<< +++++ +++++ +++++ .  increment cell 1 by 15 from 8 to 23 (space) and output space
+> .                     cell 2 (W) output "W"
++++ .                   increment cell 2 by 3 from 23 to 26 (o) and output "o"
+----- .                 decrement cell 2 by 5 from 26 to 21 (r) and output "r"
++++++ +++++ .           increment cell 2 by 10 from 21 to 31 (l) and output "l"
+> .                     cell 3 (d) output "d"
+<<< .                   back to cell 0 and output (new line character)
