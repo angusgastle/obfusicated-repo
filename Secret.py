836cc3750e6b4f847c966b0f855374c0ea3f72d8
Angus Gastle <angusgastle@gmail.com>
2024-06-08 21:30:17 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,24 +1,12 @@
 brainfuck
-+++++ +++++             initialize counter (cell #0) to 10
-[                      
-    > +++++ ++          add 7 to cell #1 (cell #1 = 7)
-    > +++++ +++++      add 10 to cell #2 (cell #2 = 10)
-    > +++ +++ +++      add 15 to cell #3 (cell #3 = 15)
-    > ++++ ++++ +++    add 20 to cell #4 (cell #4 = 20)
-    <<<< -             decrement counter (cell #0)
-]                       loop until cell #0 is zero
-
->> +++.                output 'H'
-> +.                   output 'e'
-+++ +++.               advance to 'l' and output
->.                    output 'l'
-<< +++++ +++ +++++ +++ ++ .   output 'o'
->>> +++++ +++++ +++++ +++++ +++ .  (space)
-<< +.                      output 'W'
-< ++.                    output 'o'
-++++ +.                output 'r'
--------- .             output 'l'
-<< +++ +++ .           output 'd'
->
->.                    output '!'
-
+>++++++++[<+++++++++>-]<.                   Output 'H'
+>++++[<+++++++>-]<+.                        Output 'e'
++++++++..                                    Output 'l', 'l'
++++                                         Increment cell value to 'o'
+[>+++++<-]>.                                Move to next cell and output ' '
+<<.                                         Move back to 'W' cell and output 'W'
+>--.                                        Output 'o'
+>---.                                       Output 'r'
+>++++++++.                                  Output 'l'
+>----------.                                Output 'd'
+<<.                                         Move back and output newline for cleanliness.
