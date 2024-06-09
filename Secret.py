fd2b8aa91df4bdb7a5ddbf85beb37f684c6c4289
Angus Gastle <angusgastle@gmail.com>
2024-06-09 03:30:22 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,12 +1,53 @@
 brainfuck
->++++++++[<+++++++++>-]<.                   Output 'H'
->++++[<+++++++>-]<+.                        Output 'e'
-+++++++..                                    Output 'l', 'l'
-+++                                         Increment cell value to 'o'
-[>+++++<-]>.                                Move to next cell and output ' '
-<<.                                         Move back to 'W' cell and output 'W'
->--.                                        Output 'o'
->---.                                       Output 'r'
->++++++++.                                  Output 'l'
->----------.                                Output 'd'
-<<.                                         Move back and output newline for cleanliness.
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>++++.+++++++++++.<+++++.>-----.-------.
+
+# Initialize the memory and set up the cell values:
+# Memory: [10, 0, 0, 0, 0]
+# The loop [>+++++++>++++++++++>+++>+<<<<-] will run 10 times and modify the memory as follows:
+# Memory: [0, 70, 100, 30, 10]
+
+# First, print 'H' (72):
+# ++             increment Cell[1] by 2 to get 72
+# .              output 'H'
+
+# Print 'e' (101):
+# >              move to Cell[2]
+# +              increment Cell[2] by 1 to get 101
+# .              output 'e'
+
+# Print 'l' (108):
+# +++++++        increment Cell[2] by 7 to get 108
+# .              output 'l'
+# .              output 'l'
+
+# Print 'o' (111):
+# +++            increment Cell[2] by 3 to get 111
+# .              output 'o'
+
+# Print ' ' (32):
+# >              move to Cell[3]
+# .              output ' ' (space character)
+
+# Print 'W' (87):
+# <<             move back to Cell[1]
+# +++++++++++++++ increment Cell[1] by 15 to get 87
+# .              output 'W'
+
+# Print 'o' (111):
+# >++++           move to Cell[2] and increase by 4 to get 111
+# .              output 'o'
+
+# Print 'r' (114):
+# +++++++++++     increment Cell[2] by 11 to get 114
+# .              output 'r'
+
+# Print 'l' (108):
+# <+++++          move to Cell[1] and increase by 5 to get 108
+# .              output 'l'
+
+# Print 'd' (100):
+# >-----         move to Cell[2] and decrement by 5 to get 100
+# .              output 'd'
+
+# Terminate the program with extra steps (non-functional) for complexity:
+# -------      decrement Cell[2] by 7 to nearly zero it out
