0af9ec00dc8e1119c7f31b162eae272d9a8cc914
Angus Gastle <angusgastle@gmail.com>
2024-06-10 09:30:53 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,21 +1,55 @@
-// Piet Program: The Piet language uses images, not text, so here's the image encoding of "Hello, World!"
-// To run this, use a Piet interpreter that can process image files.
-// Below is a basic diagram of a Piet program.
-// |cw| is a codel, essentially, a block of color.
+# The following script is written in INTERCAL, an esoteric programming language designed to be as different from conventional programming languages as possible.
 
-|cw|cw|cw|cw|cw|cw|cw|
-|cw|gy|gy|gy|gy|gy|cw|
-|cw|gy|yellow|yellow|yellow|gy|cw|
-|cw|gy|yellow|cyan |magenta|gy|cw|
-|cw|gy|yellow|white |white  |gy|cw|
-|cw|gy|yellow|yellow|yellow|gy|cw|
-|cw|gy|gy|gy|gy|gy|cw|
-|cw|cw|cw|cw|cw|cw|cw|
+## This script demonstrates the "HELLO WORLD" program in INTERCAL.
+## It is intentionally verbose and complex to illustrate the capabilities of INTERCAL.
 
-// Color code representation:
-// cw: Black or edge of program
-// gy: Gray
-// yellow: Yellow (Light or dark doesn't matter in this example)
-// white: White
-// cyan: Cyan
-// magenta: Magenta
+# Initialize with a COME FROM statement as an alternative to GOTO
+
+DO ,1 <- #13
+DO ,1 SUB #1 <- #3
+DO ,1 SUB #2 <- #13
+DO ,1 SUB #3 <- #10
+DO ,1 SUB #4 <- #14
+DO ,1 SUB #5 <- #5
+DO ,1 SUB #6 <- #18
+DO ,1 SUB #7 <- #11
+DO ,1 SUB #8 <- #15
+DO ,1 SUB #9 <- #16
+DO ,1 SUB #A <- #17
+DO ,1 SUB #B <- #8
+DO ,1 SUB #C <- #33
+
+# Initialize full useful numeric constants
+DO :1 <- #10901
+DO :2 <- #13632
+DO :3 <- #8185
+DO :4 <- #10921
+DO :5 <- #52272
+DO :6 <- #40961
+
+# Permute settings
+DO :1 <- ' #5
+DO :1 <- ' #5
+DO :2 <- ' #2
+DO :1 <- ' #3
+DO :1 <- ' #3
+DO :2 <- ' #1
+
+# Quick note: now we are going to set some I/O
+DO (5) <- ',1 SUB #7
+DO (5) <- '(10)
+PLEASE DO :1 <- #8 ~ #6
+
+# Generate Output
+PLEASE GIVE UP
+(5) PLEASE WRITE IN :1
+DO .4 <- #45
+DO .3 <- .1 ~ .2
+
+# If runtime errors are solved, terminate the program
+PLEASE ABSTAIN FROM (5)
+DO .2 <\- .1 ~ .3
+DO ,1 <- (11) <- #1
+DO ,1 <- (22) <- #1
+PLEASE DO ,1 <- #1 
+COME FROM (5)
