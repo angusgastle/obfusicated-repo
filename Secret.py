4e77d6a7879418f6cd386ec1ec8660f4a08d6126
Angus Gastle <angusgastle@gmail.com>
2024-06-10 03:30:24 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,10 +1,21 @@
-><> """
-  Initialize the stack with the ASCII values of 'Hello World' including the newline.
-  Note: the stack is in reverse order, so the first value pushed will be the last to be popped.
-"""
-34 *".sdaolyeR"**4>2+5/4-(2)!!<
+// Piet Program: The Piet language uses images, not text, so here's the image encoding of "Hello, World!"
+// To run this, use a Piet interpreter that can process image files.
+// Below is a basic diagram of a Piet program.
+// |cw| is a codel, essentially, a block of color.
 
-"""
-  Print the characters by popping the stack and converting the ASCII values to characters.
-"""
->:#,_@
+|cw|cw|cw|cw|cw|cw|cw|
+|cw|gy|gy|gy|gy|gy|cw|
+|cw|gy|yellow|yellow|yellow|gy|cw|
+|cw|gy|yellow|cyan |magenta|gy|cw|
+|cw|gy|yellow|white |white  |gy|cw|
+|cw|gy|yellow|yellow|yellow|gy|cw|
+|cw|gy|gy|gy|gy|gy|cw|
+|cw|cw|cw|cw|cw|cw|cw|
+
+// Color code representation:
+// cw: Black or edge of program
+// gy: Gray
+// yellow: Yellow (Light or dark doesn't matter in this example)
+// white: White
+// cyan: Cyan
+// magenta: Magenta
