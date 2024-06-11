20b5761574906abb23cd5ed6a609ed227b4111d6
Angus Gastle <angusgastle@gmail.com>
2024-06-11 15:30:40 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,14 +1,2 @@
-Jelly
-
-"""
-This is a Jelly program to display "Hello World"
-Jelly is a language focusing on code-golf and is very concise.
-We'll make this as long as possible.
-"""
-
-“Hello World”ị”Hello World”ị”Hello World”ị”Hello World”ị”Hello World”ị”Hello World”ÞKpyy”Hello””hello”¢ŒẹH→ḣeḶoBÐ€<OK⁼”Hello...
------------- ⇪
-”World...)}’→»*,⁾“L$í€^...Ẉ€B+...”Hello Y=” ++
-“Hello World” / ;Hello”)ÇLḢɠ^ɳ%+⨶‡Xà*ƤHELLOad↔HELLO생활 ,ĐJƔ !âưWḂ World➲...『“Hello→褸C^;ǸEND
-
-YṾEND<|vq_14279|>
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++.>-.<<+.>++++.------------.>---.<+++.<+++++.>++.<++++.+++++++++.--------.<+++++.<+.>-.
