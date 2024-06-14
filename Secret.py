490ea5aa5d59b74d691146e37ed90a644a25ab69
Angus Gastle <angusgastle@gmail.com>
2024-06-14 09:30:50 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,11 +1,40 @@
-/ / This is a MALBOLGE code to print "Hello World!"
-/ / MALBOLGE is an intentionally difficult programming language
-/ / The language operates in a ternary numeral system
-/ / The following code, while complex, will print "Hello World!"
+brainfuck
+// Initialize memory tape and set pointer at start
+>++++++++[<+++++++++>-]<.
 
-(=<`#9]~6ZY32Vxwv}synJPzyyx{v
-<&%d-t3mkZm;ab-Qx@kskkm*bkvj,
+// H
+>+++++++[<+++++++>-]<+.
 
-/ / The above code exploit's Malbolge's unique computational system
-/ / Each character in the program effectively bends the ASCII mechanism
-/ / This code is designed to manipulate program counter cycles efficiently and creatively
+// e
++++++++.
+
+// l
+>++++++++.
+// Bring pointer back to the start
+<.
+
+// l
+>++++++++.
+
+// o
+>+++++++++++[<+++++ +++>-]<.
+
+// space
+++.
+
+// W
+>++++[<++++++>-]<.
+
+// o
+>+++++++.
+
+// r
++++++++.
+
+// l
+>+++++++.
+
+// d
+>++.
+// Print the Output
+++++++++++.
