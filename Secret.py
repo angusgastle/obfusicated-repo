fa5775f20156a566036dbbd56088eec1cb13a28c
Angus Gastle <angusgastle@gmail.com>
2024-06-16 15:30:26 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,6 +1,2 @@
-befunge
->               v
-@               " Hello, world!"<>#                           _
-
-
-As much as possible for an otherwise simple saying of "Hello, world" and in Befunge.
+brainfuck
+++++++++++[>+++++++>++++++++++>+++<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>>+.<<-----------.+++++.
