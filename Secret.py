39b5c1ef88c40bf41094f45db69c8da13c6f03bd
Angus Gastle <angusgastle@gmail.com>
2024-06-16 03:30:18 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,30 +1,16 @@
-Piet:
+brainfuck
+++++++++++[>+++++++>+++++++++>++++++++++>+++<<<<-]>>>++.>+.+++++++..+++.>++.<<-----------.>>>-----.+++++++++++++.-----------.<<+.<+++.------.--------.
 
-# The Piet programming language uses images to represent code.
-# Below is a program that displays "Hello World" when executed with a Piet interpreter.
-#
-# Each block of colors represents an instruction: 
-# the direction pointers and color changes are used to determine the flow of the program.
-# The colors below are carefully chosen to run the desired sequence of commands.
 
-# Begin image in ASCII art (each letter represents a pixel color in Piet)
-
-#  K -> Black, Rg -> Light Red, R -> Red, O -> Orange, Y -> Yellow, Gru -> Light Green, G -> Green
-#  Cy -> Cyan, B -> Blue, Mg -> Magenta, Pu -> Light Gray, Dk -> Dark Gray, W -> White
-
-# Program starts at top-left "Yg" and moves through defined colors commands to produce the output.
-
-RGB:
-YgYgYgYgYgG            # Yellow (lightred: push colors)
-WBBBRRYYYYWW
-CYgCYgCYgWRGYYYBB
-BBYgCYgBWRYYYBB                   # Cyan, Yellow, White loops abord to skip, white exits output
-
-BRBBBBBYYYRRRR
-PUUUYgYgCCCCCU;
-BBRAAAAGGGGRGGGG
-BBBBBBRRRRRRRRRR
-RRRRRRRRRRRRRRRR
-
-# This is a visual representation. Due to the color patterns, actual execution requires 
-# running through a Piet interpreter which understands the flow control embedded within the colors.
+intercal
+DO ,1 <- #13
+DO ,1 SUB #1 <- #234
+DO ,1 SUB #2 SAME AS ,1 SUB #1
+DO ,1 SUB #1 <- #112
+PLEASE READ OUT ,1 SUB #2
+DO ,1 SUB #2 <- #170
+DO ,1 SUB #3 <- #129
+DO ,2 <- #5
+DO ,3 <- #2
+DO READ OUT ,2
+DO GIVE UP
