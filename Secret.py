493ed1c8c748dea107e5f58959c4507ffc8943dd
Angus Gastle <angusgastle@gmail.com>
2024-06-17 09:30:49 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,28 +1,51 @@
+Brainfuck:
 brainfuck
-+++++ +++++             initialize counter (cell #0) to 10
+++++++++++
 [
-    >++++++           add 6 to cell #1; this will be our loop counter for 'H'
-    [                      
-        >++++           add 4 to cell #2 on each iteration
-        >++++++         add 6 to cell #3 on each iteration
-        >+++            add 3 to cell #4 on each iteration
-        >+              add 1 to cell #5 on each iteration
-        <<<<-           decrement cell #1 (loop counter)
-    ]
-    >+.                output 'H' (ASCII 72)
-    <+++++++++++++.    output 'e' (ASCII 101)
-    [>+++++++>+<[->>-<]>-]++. output 'l' (ASCII 108)
-    >+++++++.            output 'l' (ASCII 108)
-    +++.                 output 'o' (ASCII 111)
-    <++.                 output ' ' (ASCII 32)
-    >++++++++[->+++++++<]>-. output 'W' (ASCII 87)
-    >++.                 output 'o' (ASCII 111)
-    >+.                  output 'r' (ASCII 114)
-    >+[-->+++<]>++.      output 'l' (ASCII 108)
-    >+.                  output 'd' (ASCII 100)
-    >-.                  output '!' (ASCII 33)
-<<[-]                     reset cells for termination
-]                         finish
+    >+++++++>++++++++++>+++>+<<<<-
+]
+>++.>+.+++++++..+++.>++.<<++++++++++++++.>.+++.------.--------.>+.>.
 
 
-Note: The above Brainfuck code is making use of looping constructs and ASCII-based calculations to output "Hello World!". It is intentionally complex and well-documented inline to guide the reader.
+Malbolge:
+malbolge
+('&%:9]!~}|z2Vxwv-,POqponjED#tSIH<7_^]ZWTy3vXSR+/.Mh;gLk:j
+
+
+LOLCODE:
+lolcode
+HAI 1.2
+CAN HAS STDIO?
+VISIBLE "Hello World!"
+KTHXBYE
+
+
+Piet (as an image, since Piet is an esoteric language that uses images to encode programs): ![Hello World in Piet](https://www.dangermouse.net/esoteric/piet/samples/helloworld.gif)
+
+Whitespace:
+whitespace
+     
+[TAB][SPACE][SPACE][TAB][LF]
+[TAB][SPACE][TAB][LF]
+[TAB][SPACE][LF]
+[LF][SPACE][TAB][SPACE][SPACE][LF]
+[SPACE][SPACE][SPACE][LF]
+[LF][SPACE][LF]
+[LF][TAB][SPACE][SPACE][SPACE][TAB][LF]
+[SPACE][TAB][LF]
+[LF][SPACE][TAB][LF]
+[SPACE][LF]
+
+
+HQ9+:
+hq9+
+H
+
+
+Heron:
+heron
+program HelloWorld {
+    method main() -> void {
+        "Hello World" print();
+    }
+}
