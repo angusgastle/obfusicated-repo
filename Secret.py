433338e12f9c2b4dea0b35a995153d838aee1007
Angus Gastle <angusgastle@gmail.com>
2024-06-16 21:30:32 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,46 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>>+.<<-----------.+++++.
+COW:
+
+# COW is an esoteric programming language which uses the word "moo" in various patterns as its only instruction set.
+# The following script will print "Hello World".
+
+# Comment: Initiate memory allocation
+MoO MoO mOo
+MoO MoO mOo
+MoO MoO mOo # Allocate space for H
+MoO MoO moO MoO moO mOo # Store H (72)
+MoO MoO mOo MoO moO MoO moO mOo # Store e (101)
+MoO MoO mOo MoO MoO moO MoO moO MoO mOo # Store l (108)
+MoO MoO mOo MoO MoO moO MoO moO MoO mOo
+MoO MoO mOo MoO MoO moO MoO moO MoO moO mOo # Store o (111)
+MoO MoO mOo MoO mOo moO MoO moO MoO moO MoO mOo # Store space (32)
+MoO MoO mOo MoO MoO moO MoO moO moO MoO mOo # Store W (87)
+MoO MoO mOo MoO MoO moO MoO moO MoO moO mOo # Store o (111)
+MoO MoO mOo MoO MoO moO MoO moO MoO mOo # Store r (114)
+MoO MoO mOo MoO MoO moO MoO moO MoO MoO mOo # Store l (108)
+MoO MoO mOo MoO MoO MoO MoO moO MoO moO MoO mOo # Store d (100)
+
+# Comment: Set address to the beginning
+mOo MOO mOo 
+
+# Comment: Output each character
+mOo mOo mOo MOO MoO MoO moO mOo mOo mOo # Output H
+mOo mOo MOO Moo mOo mOo mOo # Move to next
+mOo mOo mOo MOO Moo mOo mOo mOo MoO mOo MOO mOo mOo MoO # Output e
+mOo mOo MOO Moo mOo mOo mOo # Move to next
+mOo mOo mOo MOO Moo mOo mOo mOo MoO mOo MoO MoO MoO mOo MOO mOo MoO # Output l
+mOo mOo MOO Moo mOo mOo mOo # Move to next
+mOo mOo mOo MOO Moo mOo mOo mOo MoO mOo MoO MoO MoO mOo MOO mOo MoO # Output l
+mOo mOo MOO Moo mOo mOo mOo # Move to next
+mOo mOo mOo MOO Moo mOo mOo mOo MoO mOo MoO MoO MoO MoO moO mOo MOO Moo mOo # Output o
+mOo mOo MOO Moo mOo mOo mOo # Move to next
+mOo mOo mOo MOO Moo mOo mOo mOo MoO mOo mOo MOO MoO Moo mOo Moo mOo MOO mOo # Output space 
+mOo mOo MOO Moo mOo mOo mOo # Move to next
+mOo mOo mOo MOO Moo mOo mOo mOo MoO mOo MoO MoO MoO mOo MoO MoO mOo MoO moO # Output W 
+mOo mOo MOO Moo mOo mOo mOo # Move to next
+mOo mOo mOo MOO Moo mOo mOo mOo MoO mOo MoO MoO MoO MoO moO mOo MOO Moo mOo # Output o
+mOo mOo MOO Moo mOo mOo mOo # Move to next
+mOo mOo mOo MOO Moo mOo mOo mOo MoO mOo MoO MoO MoO mOo MoO moO MoO MoO mOo MOO Moo mOo # Output r
+mOo mOo MOO Moo mOo mOo mOo # Move to next
+mOo mOo mOo MOO Moo mOo mOo mOo MoO mOo MoO MoO MoO mOo MoO MoO MoO mOo MoO mOo # Output l
+mOo mOo MOO Moo mOo mOo mOo # Move to next
+mOo mOo mOo MOO Moo mOo mOo mOo MoO MoO mOo MoO MoO MoO mOo MoO mOo MoO mOo MoO moO MoO moO mOo MoO moO MoO MoO mOo MOO Moo MoO moO MoO # Output d
