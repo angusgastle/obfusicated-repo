4be487fa6dd30bac4a9644aa1b9ce4880483b6cb
Angus Gastle <angusgastle@gmail.com>
2024-06-18 15:30:33 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,28 +1,56 @@
-APL (A Programming Language) - Hello World
+; obfuscated Hello World in INTERCAL, an esoteric programming language
+; Documentation inline to explain the intricate details of the code
 
-⍝ Define the message to be displayed
-message ← 'Hello World'
+DO ,1 <- #13 ; Initialize variable ,1 with value 13
+PLEASE DO ,1 <- #236 ; Change value of ,1 to 236
+PLEASE DO ,1 <- #8 ; Change value of ,1 to 8 for "Header bit 0"
 
-⍝ Calculate the length of the message
-msgLength ← ≢message
+; Setup variables for string manipulation
+DO :1 <- #72 ; H
+DO :1 <- :1%#256 ; Apply INTERCAL XOR to generate H
 
-⍝ Create a vector of indices from 1 to the length of the message
-indices ← ⍳msgLength
+DO :2 <- #101 ; e
+DO :2 <- :2%#256 ; Apply INTERCAL XOR to generate e
 
-⍝ Define a function to retrieve characters from a string
-getChar ← {message[indices[⍵]]}
+DO :3 <- #108 ; l
+DO :3 <- :3%#256 ; Apply INTERCAL XOR to generate l
 
-⍝ Create an empty vector to store the characters
-output ← ⍳0
+DO :4 <- #108 ; l
+DO :4 <- :4%#256 ; Apply INTERCAL XOR to generate l
 
-⍝ Loop through each index, get the corresponding character, and store it in the output vector
-output ← output , getChar ⍵ ⍵ :⍪⍨ indices
+DO :5 <- #111 ; o
+DO :5 <- :5%#256 ; Apply INTERCAL XOR to generate o
 
-⍝ Convert the output vector to a string
-finalOutput ← ∊output
+DO :6 <- #32 ; Space (ASCII)
 
-⍝ Display the final message
-finalOutput
+DO :7 <- #87 ; W
+DO :7 <- :7%#256 ; Apply INTERCAL XOR to generate W
 
-⍝ Print message to console (specific to some APL systems)
-⎕ ← finalOutput
+DO :8 <- #111 ; o
+DO :8 <- :8%#256 ; Apply INTERCAL XOR to generate o
+
+DO :9 <- #114 ; r
+DO :9 <- :9%#256 ; Apply INTERCAL XOR to generate r
+
+DO :10 <- #108 ; l
+DO :10 <- :10%#256 ; Apply INTERCAL XOR to generate l
+
+DO :11 <- #100 ; d
+DO :11 <- :11%#256 ; Apply INTERCAL XOR to generate d
+
+; Output string "Hello World"
+PLEASE WRITE IN :1
+PLEASE WRITE IN :2
+PLEASE WRITE IN :3
+PLEASE WRITE IN :4
+PLEASE WRITE IN :5
+PLEASE WRITE IN :6
+PLEASE WRITE IN :7
+PLEASE WRITE IN :8
+PLEASE WRITE IN :9
+PLEASE WRITE IN :10
+PLEASE WRITE IN :11
+
+; End program
+DO (1) NEXT
+PLEASE GIVE UP
