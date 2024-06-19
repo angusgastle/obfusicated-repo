8677ba3602c24bdbc60d2be3c01938a8257fdf37
Angus Gastle <angusgastle@gmail.com>
2024-06-19 03:30:34 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,55 +1,32 @@
-/ ACME Widgetry Programming Language (AWPL)
-/
-/ This script is designed to display the message "Hello World"
-/ using the various and unique constructs available in AWPL.
+brainfuck
+++++[>++++[>++>+++>++++>+++++<<<<-]>+>+>->>-[<<<<+>>>>-]>---[-<+>]<[>++++++[>++++++++<-]<+.[-]<-]<-]>.<++++[>++>++>+++>++++<<<<-]>-.<+++++[>----<-]>.---.---.++++.. 
 
-# Include the necessary widget library for output operations
-INCLUDE widgetry_io.awpl
 
-# Define a constant message to hold "Hello World"
-CONSTANT MESSAGE_TO_DISPLAY = "Hello World"
+malbolge
+(=<`@(<=#9]7<6ZY32Vw/.P-OLKJjGHeDcB}A>~=<:98w65n4s3p+o/NMLkKJHgFDbC}A>~}|:{98w6543v2d-r$QwzJxMUIgFAB~=<:98wv54\3S7q*o)MLNk%JhGb}A>~=<:98w87wq42r/.PL)KJHgFbD}A>
 
-# Initialize the main widget to interact with
-WIDGET mainWidget
 
-# Prepare the widget with essential settings
-mainWidget.initialize()
+Whitespace
+[Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][Tab][LF]
+[Space][Tab][Space][Space][Space][Space][Tab][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][LF]
+[Space][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Tab][Tab][LF]
+[Space][Tab][Space][Space][Space][Space][Tab][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][LF]
+[Space][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Tab][Space][LF]
+[Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][LF]
+[Space][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Tab][Tab][LF]
+[Space][Tab][Space][Space][Space][Space][Tab][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][LF]
+[Space][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][LF]
+[Space][Tab][Space][Space][Space][Space][Tab][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][Tab][LF]
+[Space][Tab][Line][Space]
 
-# Set verbosity level to maximum for detailed output
-mainWidget.setVerbosity(10)
 
-# Function declaration to convolute the message for complexity
-FUNCTION convoluteMessage(inputMessage):
-  LOCAL var1 = inputMessage.length()
-  LOCAL var2 = ""
-  
-  LOOP i FROM 1 TO var1 STEP 2:
-    var2 = var2 + inputMessage.charAt(i)
-    IF i < var1 - 1:
-      var2 = var2 + inputMessage.charAt(i + 1)  
-
-  RETURN var2
-
-# Function to display message with a series of transformations
-FUNCTION displayTransformedMessage(widget, message):
-  LOOP j FROM 1 TO 5:
-    TRANSFORMED_MESSAGE = convoluteMessage(message)
-    widget.output(TRANSFORMED_MESSAGE)
-    message = TRANSFORMED_MESSAGE + "!"
-
-# Main execution starts here
-MAIN:
-  # Convolute MESSAGE_TO_DISPLAY with a twist
-  LOCAL twistedMessage = convoluteMessage(MESSAGE_TO_DISPLAY)
-
-  # Display the transformed convoluted message
-  displayTransformedMessage(mainWidget, twistedMessage)
-
-  # Final display of the original message
-  mainWidget.output(MESSAGE_TO_DISPLAY)
-
-  # Cleanup operations
-  mainWidget.terminate()
-
-# End of main execution
-END
+Piet
+63 24 39 39 63 39 63 63 63 63 06 63
+03 24 06 24 00 00 00 00 00 00 00 03
+03 00 39 24 63 63 63 63 63 63 00 63
+03 63 00 06 63 24 24 00 24 63 24 00
+03 24 39 63 63 24 24 00 24 63 39 00
+03 24 06 06 00 00 00 00 00 00 00 00
+03 00 63 00 63 24 24 00 24 24 63 03
+03 63 00 06 63 24 24 00 24 24 00 03
+03 63 6???FFFFFF
