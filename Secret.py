f02db6c50ca1e9c1208a87f95cb1a9dd287c8bfc
Angus Gastle <angusgastle@gmail.com>
2024-06-19 09:30:48 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,32 +1,29 @@
 brainfuck
-++++[>++++[>++>+++>++++>+++++<<<<-]>+>+>->>-[<<<<+>>>>-]>---[-<+>]<[>++++++[>++++++++<-]<+.[-]<-]<-]>.<++++[>++>++>+++>++++<<<<-]>-.<+++++[>----<-]>.---.---.++++.. 
+++++++++++[>+++++++>++++++++++>+++<<<-]>.>++.+++++++..+++.>-.------------.<++++++++.--------
 
 
-malbolge
-(=<`@(<=#9]7<6ZY32Vw/.P-OLKJjGHeDcB}A>~=<:98w65n4s3p+o/NMLkKJHgFDbC}A>~}|:{98w6543v2d-r$QwzJxMUIgFAB~=<:98wv54\3S7q*o)MLNk%JhGb}A>~=<:98w87wq42r/.PL)KJHgFbD}A>
+whitespace
+[S][S][S][T][N]
+[S][S][S][T][T][S][S][T][S][T][S][T][S][T][N]
+[S][S][S][T][T][S][T][S][T][S][T][N]
+[S][S][S][T][T][S][S][T][S][S][T][N]
+[S][S][S][T][T][S][T][T][N]
+[S][S][S][T][T][S][S][T][S][N]
+[S][S][S][T][T][S][T][T][S][S][N]
+[S][S][S][T][T][S][T][S][S][T][T][S][T][N]
+[S][S][S][T][T][S][T][S][T][S][S][S][N]
+[S][S][S][T][T][S][S][T][S][T][S][T][S][T][N]
+[S][S][S][T][T][S][S][T][S][T][S][S][S][N]
+[S][S][S][T][T][S][T][T][T][S][T][S][T][N]
+[T][S][S][S][N] 
+[T][N][S][T][N]
+[S][S][S][T][T][S][S][T][S][T][S][S][S][N]
+[S][S][S][T][T][S][T][S][T][S][T][N]
+[S][S][S][T][T][S][S][T][S][S][T][N]
+[S][S][S][T][T][S][T][T][N]
+[S][S][S][T][T][S][S][T][S][N]
 
 
-Whitespace
-[Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][Tab][LF]
-[Space][Tab][Space][Space][Space][Space][Tab][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][LF]
-[Space][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Tab][Tab][LF]
-[Space][Tab][Space][Space][Space][Space][Tab][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][LF]
-[Space][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Tab][Space][LF]
-[Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][LF]
-[Space][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Tab][Tab][LF]
-[Space][Tab][Space][Space][Space][Space][Tab][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][LF]
-[Space][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][LF]
-[Space][Tab][Space][Space][Space][Space][Tab][Tab][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][Space][Tab][LF]
-[Space][Tab][Line][Space]
 
-
-Piet
-63 24 39 39 63 39 63 63 63 63 06 63
-03 24 06 24 00 00 00 00 00 00 00 03
-03 00 39 24 63 63 63 63 63 63 00 63
-03 63 00 06 63 24 24 00 24 63 24 00
-03 24 39 63 63 24 24 00 24 63 39 00
-03 24 06 06 00 00 00 00 00 00 00 00
-03 00 63 00 63 24 24 00 24 24 63 03
-03 63 00 06 63 24 24 00 24 24 00 03
-03 63 6???FFFFFF
+Malbolge
+(=<`#9]~6ZY32Vxwv-,POqponljYTJ~yA*
