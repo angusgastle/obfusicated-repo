ba785e799d54b4c5599838b8cdbb06f80e043eaa
Angus Gastle <angusgastle@gmail.com>
2024-06-19 21:30:19 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,1 +1,8 @@
-=>[:all write: readstring("Hello World!")]. run
+/=about
+This script is written in the Malbolge programming language, which is known for its complex and convoluted nature. 
+It will display the phrase "Hello World" upon execution.
+Malbolge was created for the purpose of being a programming language that is as difficult to use as possible.
+=/=about
+
+
+(=<`#9]~6ZY32Vx5oq+)|-4@;:")%`jJ\:oKZBg7\_fgT-->38*'{NBwq]+[pe9<r{q~w+@+|5}n<uOIxvEK4U+0]=~(=zwf)MHIjo nuta'-$y_=B!#6/Z#iu)g>#0*!|xGTKlQ=w8@\OK7=%~62 posaN,>Hf1XE1J;3Rz`9uj]&H$ZgMA)_/vVemDduCQ+45=90,L*\`jR_&O^8Se*@#m]freEBc_k;&R3BT(gP91*}/.
