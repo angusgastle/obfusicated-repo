2fc6d6a5edf0c0c381e92ab773970342efd6b3f3
Angus Gastle <angusgastle@gmail.com>
2024-06-21 15:30:37 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,40 +1,10 @@
-// Source code in INTERCAL, an obscure and humorous esoteric programming language
-// This program outputs "Hello World" to the console
+malbolge
+(=<`#9]~6ZY32Vw/.6ZY32Vw/.6ZY32Vw/.
+;dc ~}|z*}\(#Rv\G"=~}|z*}\(#R:
 
-// Start of program
-DO ,1 <- #13
-DO ,1 SUB #1 <- #72         ; Set "H"
-DO ,1 SUB #2 <- #101        ; Set "e"
-DO ,1 SUB #3 <- #108        ; Set "l"
-DO ,1 SUB #4 <- #108        ; Set "l"
-DO ,1 SUB #5 <- #111        ; Set "o"
-DO ,1 SUB #6 <- #32         ; Set " " (space)
-DO ,1 SUB #7 <- #87         ; Set "W"
-DO ,1 SUB #8 <- #111        ; Set "o"
-DO ,1 SUB #9 <- #114        ; Set "r"
-DO ,1 SUB #10 <- #108       ; Set "l"
-DO ,1 SUB #11 <- #100       ; Set "d"
-DO ,1 SUB #12 <- #33        ; Set "!" (exclamation mark)
-DO ,1 SUB #13 <- #0         ; Set end of string
+;vK>|+VPU>W^_~}|z*}\(#Rv~}|z*HVWvx
 
-// Ask INTERCAL to prepare output
-DO ,2 <- #1
+;;_^$$s|`2.=/I3>A,'!;A}KXg4(~}|#>.VrfE!R#~}|z*}\(#
 
-// Initialize loop control
-DO ,3 <- #1
 
-// Define output subroutine
-(1) NEXT
-    PLEASE DO ,4 <- ,1 SUB ,3    ; Fetch character
-    PLEASE DO ,4 <- #0~ ,4       ; Interprivative output (weird but works)
-    DO ,5 <- #2~ ,4
-    PLEASE DO ,2 SUB ,3 <- ,5
-    DO ,3 <- ,3 + #1             ; Increment loop counter
-    DO (1) AGAIN                 ; Loop again
-
-// Output line breaker for readability
-(2) NEXT
-    PLEASE READ OUT ,2           ; Output the resulting string
-
-// End of program
-DO (2) AGAIN
+# Malbolge is an esoteric programming language designed to be as difficult to program in as possible. This program prints "Hello, World!"
