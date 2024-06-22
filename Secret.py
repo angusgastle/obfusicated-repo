5a43ef120c5db4e6d79c7e52193417127def494f
Angus Gastle <angusgastle@gmail.com>
2024-06-22 15:30:25 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,17 +1,38 @@
+brainfuck
+++++++++                Initialize cell 0 to 8
+[                        Start loop since cell 0 is not 0
+    >++++               Increment cell 1 to 4
+    [                   Start loop since cell 1 is not 0
+        >++             Increment cell 2 to 2
+        >+++            Increment cell 3 to 3
+        >+++            Increment cell 4 to 3
+        >+              Increment cell 5 to 1
+        <<<<-           Decrement cell 1, loop continues
+    ]                   End loop when cell 1 is 0
+    >+                  Increment cell 2 to 3
+    >+                  Increment cell 3 to 4
+    >-                  Decrement cell 4 to 2
+    >>+                 Increment cell 6 to 1
+    [<<]                Move to cell 4 and 3 to emphasis point
+    <-                  Decrement cell 0, loop continues
+]                        End of outer loop when cell 0 is 0
+>++                     Increment cell 1 to 2
+>+                      Increment cell 2 to 5
+>+++++++                Increment cell 3 to 11
+>++                     Increment cell 4 to 4
+>+++++++++++            Increment cell 5 to 15
+>                       Move to cell 6
+++++++++++             Increment cell 6 to 10
 
-' ._______________.    ___      .______     ______    __           _______.  _______    .______       _______  __       __  
-' |               |   /   \     |   _  \   /  __  \  |  |         /       | /  _____|   |   _  \     /  _____||  |     |  | 
-' `---|  |----|---`  /  ^  \    |  |_)  | |  |  |  | |  |        |   (----`|  |  __     |  |_)  |   |  |  __  |  |     |  | 
-'     |  |        /  /     \   |   ___/  |  |  |  | |  |         \   \    |  | |_ |    |    ___/    |  | |_ | |  |     |  | 
-'     |  |       /  _____  \   |  |      |  `--'  | |  `----. .----)   |   |  |__| |    |  |          |  |__|  | |  `    `----.  | |     
-'     |__|      /__/     \__\ | _|       \______ /  |_______||_______/     \______|        \______/        \______||                \______|  |__|
-
-' This script demonstrates "Hello World" in Malbolge, a random and highly obscure esolang. 
-' Malbolge was designed to be difficult to program in; it uses base-3 arithmetic and self-modifying code.
-' The following Malbolge code prints "Hello World".
-
-(=<`#9]~6ZY32Vxll@#*^w:]'s(no[5;kakakakappa(GL)
-
-' The above code is an obscure and very complex means of displaying "Hello World". 
-' It leverages Malbolge's non-intuitive and obfuscated syntax to achieve this simple operation.
-' Note: This code might not work flawlessly on all Malbolge interpreters due to variations.
+<<<<<.                  Output 'H'
+>++.                    Output 'e'
++++++++..               Output 'll'
++++.                    Output 'o'
+>-.                     Move to next cell and modify for space
+<.                      Output space
+<+++++.                 Output 'W'
+>++.                    Output 'o'
++++.                    Output 'r'
+----.                   Modify for 'l'
+------.                 Modify for 'd'
+>+.                     Move and output '!'
