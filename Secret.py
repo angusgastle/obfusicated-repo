9820854fa561d47e434df4c8228e23ddfd0e3184
Angus Gastle <angusgastle@gmail.com>
2024-06-21 21:30:20 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,10 +1,20 @@
-malbolge
-(=<`#9]~6ZY32Vw/.6ZY32Vw/.6ZY32Vw/.
-;dc ~}|z*}\(#Rv\G"=~}|z*}\(#R:
+brainfuck
++++++ +++++             initialize cell 0 with value 10
+[
+    >++++++ +++         shift to cell 1 and add 7*10 to it, making cell 1 value 70
+    <
+-]                      loop to decrease cell 0 till it becomes 0, repeating the decrement 10 times for cell 1
 
-;vK>|+VPU>W^_~}|z*}\(#Rv~}|z*HVWvx
-
-;;_^$$s|`2.=/I3>A,'!;A}KXg4(~}|#>.VrfE!R#~}|z*}\(#
-
-
-# Malbolge is an esoteric programming language designed to be as difficult to program in as possible. This program prints "Hello, World!"
+>++++.                 move to cell 1 and print 'H' (72)
+>---.                 move to cell 2 and print 'e' (101)
++++++++.              add 7 to cell 2 and print 'l' (108)
+.                     print 'l' (108) again
++++.                  add 3 to cell 2 to make 'o' (111) and print
+>++.                  move to cell 3 and print space (32 + 2 = 34)
+<<++++[>----<-]       shift to cell 1, add 4, move it to cell 4, then convert it to cell 1 value (10)
+>+.                   move to cell 1 and print 'W' (72 + 10 = 87)
+>++.                  move to cell 2 and add 2, making it 34, print as space (34 + 2 = 36)
+-----.-               subtract 5, making it 97, 'a' + 1 for 'r' -> (114 + 1 = 115)
+<----- .              subtract 5 to get 'd' + shift making it 'l', (120-12 = 108)
++.                    add 1 to 'd' and get '!'
+<.                    shift back and print space
