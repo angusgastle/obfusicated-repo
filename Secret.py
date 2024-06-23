7627b10b93c394e32b209ba4c6ebc97cf43b19ae
Angus Gastle <angusgastle@gmail.com>
2024-06-22 21:30:20 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,38 +1,68 @@
 brainfuck
-++++++++                Initialize cell 0 to 8
-[                        Start loop since cell 0 is not 0
-    >++++               Increment cell 1 to 4
-    [                   Start loop since cell 1 is not 0
-        >++             Increment cell 2 to 2
-        >+++            Increment cell 3 to 3
-        >+++            Increment cell 4 to 3
-        >+              Increment cell 5 to 1
-        <<<<-           Decrement cell 1, loop continues
-    ]                   End loop when cell 1 is 0
-    >+                  Increment cell 2 to 3
-    >+                  Increment cell 3 to 4
-    >-                  Decrement cell 4 to 2
-    >>+                 Increment cell 6 to 1
-    [<<]                Move to cell 4 and 3 to emphasis point
-    <-                  Decrement cell 0, loop continues
-]                        End of outer loop when cell 0 is 0
->++                     Increment cell 1 to 2
->+                      Increment cell 2 to 5
->+++++++                Increment cell 3 to 11
->++                     Increment cell 4 to 4
->+++++++++++            Increment cell 5 to 15
->                       Move to cell 6
-++++++++++             Increment cell 6 to 10
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>>>+++++++.>++.+++++++..+++.>+
+<--------.+++.------.--------.>+.<.
 
-<<<<<.                  Output 'H'
->++.                    Output 'e'
-+++++++..               Output 'll'
-+++.                    Output 'o'
->-.                     Move to next cell and modify for space
-<.                      Output space
-<+++++.                 Output 'W'
->++.                    Output 'o'
-+++.                    Output 'r'
-----.                   Modify for 'l'
-------.                 Modify for 'd'
->+.                     Move and output '!'
+# This Brainfuck script outputs "Hello World!" character by character.
+# Brainfuck uses a memory array of 30,000 cells, each initially zero.
+
+# Initialize cells:
+# 
+# Cell 1 = 10
+# Cell 2 = 70
+# Cell 3 = 100
+# Cell 4 = 30
+# Cell 5 = 10
+#
+# Loop: 
+# 
+# Cell 2 = 70 (7 * 10) 
+# Cell 3 = 100 (10 * 10) 
+# Cell 4 = 30 (3 * 10)
+# Cell 5 = 10 (1 * 10)
+
+++++++++++             # Set Cell 1 to 10
+[
+  >+++++++             # Set Cell 2 to 70 (7 * 10)
+  >++++++++++          # Set Cell 3 to 100 (10 * 10)
+  >+++                 # Set Cell 4 to 30 (3 * 10)
+  >+                   # Set Cell 5 to 10 (1 * 10)
+  <<<<-                # Decrement Cell 1 to 0
+]
+
+# Output 'H'
+>>>+++++++             # Cell 3 = 77
+.
+# Output 'e'
+>++                    # Cell 4 = 32
+.
+# Output 'l'
++++++++                # Cell 5 = 39
+.
+# Output 'l'
+.
+# Output 'o'
++++                    # Cell 6 = 42
+.
+# Space
+<
+<--------            # Cell 1 = -8, adjusting the position.
+.
+# Output 'W'
++++
+.
+# Output 'o'
+++++++                # Cell 4 = 40
+.
+# Output 'r'
+------
+.
+# Output 'l'
+--------              # Cell 3 = 60
+.
+ 
+# Output 'd'
+>+
+.
+# Exclamation mark (!)
+<
+.
