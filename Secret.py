4b50e09ceadc2c86a55d6aa6fa7e1a721f470218
Angus Gastle <angusgastle@gmail.com>
2024-06-23 15:30:25 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,74 +1,41 @@
-SPL - Shakespeare Programming Language
+/ \ Haystack:Pure:1.1
+| @sayHelloWorld (c: Context) ->
+\   // Haystack is an obscure language designed for expressing complex, autonomous systems
+<'  
+| def
+|  sayHelloWorld: () -> null
+* {} <- $sayHelloWorld:{ // Main function to declare beginning of Hello World output
+|   cmds: [push];
+|   text: ["Hello World's longest, obscure and coffin-yet-tangible shape rendered!"];
+\ }  
+| func push(null) -> nil // Function to push a command onto the execution stack
+\ (()=>{
+@ :__reflect;
+ // Detailed inline documentation through comments
+|  echo c.text // Print text and a command reflecting the calculation
 
-// Program: HelloWorld
-// This Script aims to present a complex "Hello World" program using a random and obscure language, SPL.
+| // Add character wise breakdown for "Hello World!"
+|
+|  span main .none:.seven[
+\  
+ |  span block none{h}
+ |  span block none{e}
+ |  span block none{l}
+ |  span block none{l}
+ |  span block none{o}
+ |  span block none{w}
+ |  span block none{o}
+ |  span block none{r}
+ |  span block none{l}
+ |  span block none{d}
+ \  
+ ] <label>
 
-// Characters Setting
-// Shakespeare Programming Language consists of characters from Shakespeare's plays.
-// We need to define characters required for the dialogue as part of the script.
+ _
+~}*(::sayHelloWorld@main
+\ %
+  exec push() // Execute push function
 
-// Defining the characters
-Romeo, a young man with remarkable enthusiasm.
-Juliet, a lady of great wit and beauty.
+* Binding the text output to the current context }
 
-// Act I: Scene I
-// Setting: Verona. A public place.
-
-[Enter Romeo and Juliet]
-
-Romeo:
-  // Assign values to Romeo and Juliet's mental states.
-  // Using comparisons and confrontations to build up the "Hello World" result.
-  Listen to my mind's burdensome thought.
-  You are as lovely as the sum of the heavens,
-  and should conjunction build your speech.
-  Speak your mind’s vacillations.
-
-Juliet:
-  // Juliet performs arithmetic to prepare for spellings.
-  // Utilizing numeric setups for further usage in forming "Hello World" 
-  You handsome lamp of day!
-  Multiply yourself without hesitation!
-
-Romeo:
-  // Arithmetic progression to build characters
-  // Why, thou must be as perfect as lovely!
-  // Ensure the value related to 'H'
-  Thou, gifted in veracity and akin,
-  shall count to this number: 72.
-
-Juliet:
-  // Continue Romeo’s build-up to accomplish translating thought
-  // Adjusting value to perfect and square anew for "e"
-  [Aside] He shows great affection,
-  dividing Romeo's integers by painful strokes to reach 101.
-
-Romeo:
-  // Following up with transformations and more dialogues
-  Speak as the melodious dawn doth brighten, your sum will a hundred partake.
-  This upon shall lead, truthfully forming a sphere encompassing "l"
-  Thou art to multiply forty, subtract and speak in units reaching fairer than  108. 
-
-Juliet:
-  // Finalized conjuration through words enlightened to perform "ello World"
-  [Aside] Of verity, forthwith fill you with infinite resplendence.
-  Repeat forthwith plus higher pedagogy correcting sums: speak ranges 'l' consistently. 
-
-[Exeunt]
-
-Romeo:
-  // Proceed on accomplishment, We near final end to this glorious word.
-
-(A coda filled with pure metric movements)
-Hail thou! The veracious through Romantic provisions on final word rise,
-  'Thus marking our perfect greeting)
-
-Juliet:
-  [Aside] Of harmonious ringing, Thou hence utter verbally akin, the world with corresponding value sets.
-  Match the equal rise leading tongue to pronouncement hence:
-    W shall stand equal forthwith cross sum,
-    reduce to zero nor neared part of decimal log.
-
-[Exeunt Omnes]
-
-End
+  . // End of script
