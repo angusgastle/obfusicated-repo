fa9e3744d1f793d4d49628f840a9e616fac5971c
Angus Gastle <angusgastle@gmail.com>
2024-06-24 03:30:19 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,10 +1,4 @@
-
-# The following script is written in Malbolge, an esoteric programming language known for its complexity and obscurity.
-# It prints "Hello World" to the console.
-
-('&%:9]!~}|z2Vxwv-,POqponlJk*5`)BA98?>M=<;:65^43210/.I-MLK**HG$CBA@?>=<;?@:C98F)ED)BAZYXWVUTRQPONMLKJIHGFEDCBA@?>=<;:?98(:BA=:87654:!wvxzyxuMLKJHgGfdCBa`BA@?>=<;:97654321/xwvuts8SRQPONMLK*bH
-
-
-# This program makes use of the Malbolge language's peculiar encrypting and decrypting behavior.
-# The instructions are encoded in such a way that the execution results in the output "Hello World".
-# Malbolge's syntax and operations are intentionally designed to be nearly incomprehensible.
+brainfuck
+>+++++++++[<++++++++>-]<-.>++++[<+++++++>-]<+.+++++++..+++.---
+.>++++[<+++++++>-]<+.>+++++[<+++++++>-]<.>++[<+++++++>-]<+.<<
+.>+++++++++[<+++++++>-]<-.
