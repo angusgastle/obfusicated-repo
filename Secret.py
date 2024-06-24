67384e612f5ef4c50b191c2e783e1619daeb63b5
Angus Gastle <angusgastle@gmail.com>
2024-06-24 09:30:45 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,4 +1,21 @@
 brainfuck
->+++++++++[<++++++++>-]<-.>++++[<+++++++>-]<+.+++++++..+++.---
-.>++++[<+++++++>-]<+.>+++++[<+++++++>-]<.>++[<+++++++>-]<+.<<
-.>+++++++++[<+++++++>-]<-.
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+++.>+.+++++++..+++.[-]++++++++++.
+
+
+whitespace
+[S       S    S    S    S    S     S   S    S     S    S   S       S       L   S   S   T  T  S     S  S     L   S   S   T  S  T  T  T L  S   T  S   L  T  S   S   S   L   T  T  S   T  T  S  T  L  S     L   N
+S   S     L   T  N
+S     S     S    S   T   S   S      S  T   T  T  T S  L  S  L  T  T T  N
+S     S    T   S  S  S    S     S    L   N
+S   S   S  S     S  T  T T S  L  S  S  N
+S   S    S  T T   S L  T  N
+S     S   T    T   T S  S L  S   S  S   T  N
+]
+[ N
+S    S    S      S   S     T   T  T S  S L  S  N
+T     S   S    T T   N
+T           N
+S    S   S   S   S   T    S N
+]
+[L    N
+]
