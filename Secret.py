e46be14e723ce7db58ea2dcedb05ee30f4b6828c
Angus Gastle <angusgastle@gmail.com>
2024-06-26 03:30:26 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,73 +1,92 @@
-# Hello World Program in INTERCAL, a very obscure and esoteric programming language.
-# This program will demonstrate how to print "Hello World".
-#
-# INTERCAL, short for 'Compiler Language With No Pronounceable Acronym,' 
-# is a deliberately difficult and esoteric programming language that parodies programming languages.
-# Below, we meticulously construct a program to print "Hello World".
+;; Script for displaying "Hello World" in INTERCAL (a deliberately obscure and complex language)
 
-DO ,1 ← #13 
-DO ,1 SUB #1 ← #238 
-DO ,1 SUB #2 ← #108 
-DO ,1 SUB #3 ← #112 
-DO ,1 SUB #4 ← #0 
-DO ,1 SUB #5 ← #64 
-DO ,1 SUB #6 ← #194 
-PLEASE DO ,1 SUB #7 ← #48 
-DO ,1 SUB #8 ← #154 
+DO ,1 <- #13
+DO ,1 <~ #13
+DO ,2 <- #0
+DO ,3 <- #32 
+DO ,3 <~ #0 
 
-PLEASE DO ,1 SUB #9 ← #18 
-DO ,1 SUB #10 ← #36 
-DO ,1 SUB #11 ← #22 
-DO ,1 SUB #12 ← #248 
-DO ,1 SUB #13 ← #168 
-DO ,1 SUB #14 ← #24 
+PLEASE DO :1 <- #72 
+DO :1 <~ #72 
+PLEASE DO :2 <- #101 
+DO :2 <~ #101 
+PLEASE DO :3 <- #108 
+DO :3 <~ #108 
+PLEASE DO :4 <- #108 
+DO :4 <~ #108 
+PLEASE DO :5 <- #111 
+DO :5 <~ #111 
+PLEASE DO :6 <- #32 
+DO :6 <~ #32 
+PLEASE DO :7 <- #87 
+DO :7 <~ #87 
+PLEASE DO :8 <- #111 
+DO :8 <~ #111 
+PLEASE DO :9 <- #114 
+DO :9 <~ #114 
+PLEASE DO :10 <- #108 
+DO :10 <~ #108 
+PLEASE DO :11 <- #100 
+DO :11 <~ #100 
 
-PLEASE DO ,2 ← #256 
-PLEASE DO ,3 ← #6 
+PLEASE WRITE IN ,1
 
-DO ,4 ← #4 
-PLEASE FORGET #4 
-PLEASE DO ,5 ← #256 
+DO READ OUT ,2
+PLEASE WRITE IN ,2 
 
-(PLEASE READ OUT ,2) 
-DO ,6 ← #3 
-DO ,7 ← #1 
+DO .1 <- .2
+PLEASE DO .2 <- :1
+DO (1) NEXT
+PLEASE READ OUT ,2 
 
-PLEASE STASH #7 
-DO ,7 ← #4 
-(READ OUT ,2) 
-DO ,8 ← #0
+DO .1 <- .3
+PLEASE DO .2 <- :2
+DO (2) NEXT
+PLEASE READ OUT ,2
 
-(PLEASE GIVE UP)
+DO .1 <- .3 
+PLEASE DO .2 <- :3 
+DO (3) NEXT 
+PLEASE READ OUT ,2 
 
-(WRITE IN ,1) 
-DO ,9 ← #1 
-DO ,10 ← #1 
-DO ,11 ← #1 
-DO ,12 ← #1 
-DO ,13 ← #1
-DO ,14 ← #1 
+DO .1 <- .3 
+PLEASE DO .2 <- :4 
+DO (4) NEXT 
+PLEASE READ OUT ,2 
 
-(READ IN ,1)
+DO .1 <- .3 
+PLEASE DO .2 <- :5 
+DO (5) NEXT 
+PLEASE READ OUT ,2 
 
-PLEASE DO ,4 ← #4 
-DO ,2 ← #256 
+DO .1 <- .3 
+PLEASE DO .2 <- :6 
+DO (6) NEXT 
+PLEASE READ OUT ,2 
 
-(PLEASE GIVE UP)
+DO .1 <- .3 
+PLEASE DO .2 <- :7 
+DO (7) NEXT 
+PLEASE READ OUT ,2
 
-DO ,15 ← #6 
-DO ,5 ← #256 
-PLEASE DO ,16 ← #2 
+DO .1 <- .3 
+PLEASE DO .2 <- :8 
+DO (8) NEXT 
+PLEASE READ OUT ,2 
 
-DO ,1 ← #12
-PLEASE DO ,1 SUB #1 ← #152 
-DO ,1 SUB #2 ← #25 
-PLEASE DO ,1 SUB #3 ← #160 
+DO .1 <- .3 
+PLEASE DO .2 <- :9 
+DO (9) NEXT 
+PLEASE READ OUT ,2 
 
-(PLEASE READ OUT ,2)
+DO .1 <- .3 
+PLEASE DO .2 <- :10 
+DO (10) NEXT 
+PLEASE READ OUT ,2 
 
-DO ,17 ← #1 
-PLEASE DO ,18 ← #256
-DO ,19 ← #256 
+DO .1 <- .3 
+PLEASE DO .2 <- :11 
+DO (11) NEXT 
+PLEASE READ OUT ,2
 
-PLEASE GIVE UP
+DO GIVE UP
