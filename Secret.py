1a12c7f033c62f9dadea2253edb4dd5a1ba087a4
Angus Gastle <angusgastle@gmail.com>
2024-06-25 21:30:24 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,10 +1,73 @@
-; Language: Malbolge
+# Hello World Program in INTERCAL, a very obscure and esoteric programming language.
+# This program will demonstrate how to print "Hello World".
+#
+# INTERCAL, short for 'Compiler Language With No Pronounceable Acronym,' 
+# is a deliberately difficult and esoteric programming language that parodies programming languages.
+# Below, we meticulously construct a program to print "Hello World".
 
-; Malbolge is a public domain esoteric programming language created by Ben Olmstead in 1998.
-; It's designed to be as difficult to program in as possible.
-; Below is a Malbolge program that outputs "Hello World".
+DO ,1 ← #13 
+DO ,1 SUB #1 ← #238 
+DO ,1 SUB #2 ← #108 
+DO ,1 SUB #3 ← #112 
+DO ,1 SUB #4 ← #0 
+DO ,1 SUB #5 ← #64 
+DO ,1 SUB #6 ← #194 
+PLEASE DO ,1 SUB #7 ← #48 
+DO ,1 SUB #8 ← #154 
 
-('&%:9]!~}|zng @}xiwvutsrponmlkjihgfedcba`_^]\[ZYXWVUTSRQPONMLKJIHGFEDCBA@>]UTSRQPO+MLKJIHGFEDCBA98<75/w32qp.-,k*&%$#"!~
-('&%:9]!~}|zng @}xiwvutsrponmlkjihgfedcba`_^]\[ZYXWVUTSRQPONMLKJIHGFEDCBA@>]UTSRQPO+MLKJIHGFEDCBA98<75/w32qp.-,k*&%$#"!~
-('&%:9]!~}|zng @}xiwvutsrponmlkjihgfedcba`_^]\[ZYXWVUTSRQPONMLKJIHGFEDCBA@>]UTSRQPO+MLKJIHGFEDCBA98<75/w32qp.-,k*&%$#"!~
-('&%:9]!~}|zng @}xiwvutsrponmlkjihgfedcba`_^]\[ZYXWVUTSRQPONMLKJIHGFEDCBA@>]UTSRQPO+MLKJIHGFEDCBA98<75/w32qp.-,k*&%$#"!~
+PLEASE DO ,1 SUB #9 ← #18 
+DO ,1 SUB #10 ← #36 
+DO ,1 SUB #11 ← #22 
+DO ,1 SUB #12 ← #248 
+DO ,1 SUB #13 ← #168 
+DO ,1 SUB #14 ← #24 
+
+PLEASE DO ,2 ← #256 
+PLEASE DO ,3 ← #6 
+
+DO ,4 ← #4 
+PLEASE FORGET #4 
+PLEASE DO ,5 ← #256 
+
+(PLEASE READ OUT ,2) 
+DO ,6 ← #3 
+DO ,7 ← #1 
+
+PLEASE STASH #7 
+DO ,7 ← #4 
+(READ OUT ,2) 
+DO ,8 ← #0
+
+(PLEASE GIVE UP)
+
+(WRITE IN ,1) 
+DO ,9 ← #1 
+DO ,10 ← #1 
+DO ,11 ← #1 
+DO ,12 ← #1 
+DO ,13 ← #1
+DO ,14 ← #1 
+
+(READ IN ,1)
+
+PLEASE DO ,4 ← #4 
+DO ,2 ← #256 
+
+(PLEASE GIVE UP)
+
+DO ,15 ← #6 
+DO ,5 ← #256 
+PLEASE DO ,16 ← #2 
+
+DO ,1 ← #12
+PLEASE DO ,1 SUB #1 ← #152 
+DO ,1 SUB #2 ← #25 
+PLEASE DO ,1 SUB #3 ← #160 
+
+(PLEASE READ OUT ,2)
+
+DO ,17 ← #1 
+PLEASE DO ,18 ← #256
+DO ,19 ← #256 
+
+PLEASE GIVE UP
