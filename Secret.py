7036b1f48faffdf765680ab3937d5ba8606256ae
Angus Gastle <angusgastle@gmail.com>
2024-06-27 09:30:47 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,35 +1,28 @@
-:prog "Hello World Display"
+///// Brainfuck Code to display "Hello World"
 
-(:let ((#'o# :proc ()))
-  (#'o#
-    (let 
-      ; Define the main program body
-      (#'$("#result").html("Hello, World!");$#
-        (init ())
-        ; Initialize the "Display" box
-        (#'($("#displayBox")) 
-          (: DisplayBox)
-          :init ((: Constructor)
-            ; Body of the display box constructor
-            (:begin :section (init Construct)
-              ; Set up the box dimensions
-              (#'this.width = 200;$#
-                #'this.height = 200;$#
-                #'(this.innerHTML = " ";)$#)
-            ; End of Constructor
-            :end Construct)
-          )
-          (#';$())
-        )
-        ; Call to run the "Display" box
-        (#'( $("#displayBox").init() )#$))
-    )
-  )#
-)
+// Initialize memory cells
++++++ +++++ initialize cell 0 to 10 (ASCII 10)
+[ move loop start
+> +++++ +++ initialize cell 1 to 7 (ASCII 7)
+> +++++ +++++ initialize cell 2 to 10 (ASCII 10)
+> +++ initialize cell 3 to 3 (ASCII 3)
+> + initialize cell 4 to 1 (ASCII 1)
+<<<<<- decrement cell 0 (loop control) by 1
+] loop until cell 0 is zero
 
-; Entry Point to the program
-(#'o# main entry
-  ; Invoke the main sequence
-  (#'("#result").html(init ())$()
-  )
-)
+// Sequence established now to store and print
+> ++ . print 'H' (cell 1+2, ASCII 72)
+> + . print 'e' (cell 2+1, ASCII 101)
+>> . reset and print 'l' (cell 2+3, set cell 4 to 108)
+. print 'l' again
+> + . print 'o' (cell 3+1, ASCII 111)
+<<<<< . reset cell pointer to start and move to cell 4
++. print ' ' (cell 0, ASCII 32)
+> +++++ + . print 'W' starting from cell 3, with overflow to next cell (ASCII 87)
++++++ +++ . print 'o' (cell 3+1, ASCII 111)
+. print 'r' (cell 4-1, ASCII 114)
+<<< . reset and go to initial memory
+. print 'l' again (cell 3+3)
+> . print 'd' (cell 4+1)
++++ . print '!' (cell 3, ASCII 33)
+++++.+++++ . end (cell 2, newline or chosen end symbol)
