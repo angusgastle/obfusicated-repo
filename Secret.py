baa8e455e7d3c30b610c8dd101102ba869fe5b02
Angus Gastle <angusgastle@gmail.com>
2024-06-27 03:30:19 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,35 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.
+:prog "Hello World Display"
+
+(:let ((#'o# :proc ()))
+  (#'o#
+    (let 
+      ; Define the main program body
+      (#'$("#result").html("Hello, World!");$#
+        (init ())
+        ; Initialize the "Display" box
+        (#'($("#displayBox")) 
+          (: DisplayBox)
+          :init ((: Constructor)
+            ; Body of the display box constructor
+            (:begin :section (init Construct)
+              ; Set up the box dimensions
+              (#'this.width = 200;$#
+                #'this.height = 200;$#
+                #'(this.innerHTML = " ";)$#)
+            ; End of Constructor
+            :end Construct)
+          )
+          (#';$())
+        )
+        ; Call to run the "Display" box
+        (#'( $("#displayBox").init() )#$))
+    )
+  )#
+)
+
+; Entry Point to the program
+(#'o# main entry
+  ; Invoke the main sequence
+  (#'("#result").html(init ())$()
+  )
+)
