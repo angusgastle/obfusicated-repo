4f0a67a8a4e1e99815bb5f5a2572ff4aed5fd06f
Angus Gastle <angusgastle@gmail.com>
2024-06-28 03:30:21 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,68 +1,15 @@
-// The following code is written in INTERCAL, an esoteric programming language designed to be as different from conventional programming languages as possible.
+apl
+⍝ Initialize matrix size
+⍝ Multiply identity matrix by z
+z ← 3
+⍝ Outer product with concatenation
+mat ← ('Hello' , 'World') ∘.≠ ⍨ z
+mat
 
-// This is a program to print "Hello World" in INTERCAL.
-
-// Set up variables and arrays
-DO ,1 <- #13
-DO ,1 SUB #1 <- #238
-DO ,1 SUB #2 <- #108
-DO ,1 SUB #3 <- #112
-DO ,1 SUB #4 <- #0
-DO ,1 SUB #5 <- #64
-DO ,1 SUB #6 <- #240
-DO ,1 SUB #7 <- #69
-DO ,1 SUB #8 <- #48
-DO ,1 SUB #9 <- #80
-DO ,1 SUB #10 <- #70
-DO ,1 SUB #11 <- #55
-DO ,1 SUB #12 <- #25
-DO ,1 SUB #13 <- #9
-DO ,2 <- #0
-
-// Labels for flow control
-(1) DO ,2 <- ,2 + #1
-
-DO (17) NEXT
-(17) DO (7) NEXT
-(7) PLEASE READ OUT ,1 SUB #1 + ,2
-PLEASE DO ,3 <- #0
-
-DO FORGET #1
-
-(2) DO ,4 <- #3
-DO ,5 <- #1
-(WRITEHE) DO ,6 <- #2
-KEEP #23457
-DO (9) NEXT
-(9) DO COME FROM (7)
-DO ,6 <- #4
-(WRITE) PLEASE READ OUT ,1 SUB ,2
-
-(WRITELO) DO ,6 <- #2
-PLEASE DO (INPUT) NEXT
-DO ,7 <- ,5
-DO (GOOUT) NEXT
-(OK) PLEASE GIVE UP
-#[LABEL FOR COMPLETION]
-(GOOUT) DO (DONE) NEXT
-PLEASE DO (FINISH) NEXT
-PLEASE DO (FINISH) NEXT
-PLEASE DO (FINISH) NEXT
-
-(11) DO ,1 SUB ,2 <- #68
-DO ,4 <- #1
-
-(12) IF ,4 NOT SENTENCE (15)
-DO (STOP) NEXT
-
-(PROMPT) DO (INPUT) NEXT
-(15) DO FORGET ,1 SUB #17
-(GOON) PLEASE RESUME
-(END) DO (BEGIN) NEXT
-
-// This will print "Hello World!"
-(DONE) DO RESUME
-(YES) DO (0) NEXT
-(BEGIN) DO (11) NEXT
-
-# End of the INTERCAL Hello World program
+⍝ Display Hello World
+disp ← {
+    ⍺←0 ⋄ ⍵←   ⎕rc⍵
+    ⍵ ((⍴⍵) /⍨ (1 ⍵∘⍴0)) ⍴ ⍨
+     ⍳1 5
+} mat
+disp
