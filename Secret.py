a0abe3ae45bfd1a30e831e651de6995ab7f27e6b
Angus Gastle <angusgastle@gmail.com>
2024-06-27 21:30:24 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,34 +1,68 @@
-# Befunge-93 Code
+// The following code is written in INTERCAL, an esoteric programming language designed to be as different from conventional programming languages as possible.
 
-# Befunge-93 is a 2D esoteric programming language.
-# The instructions are laid out on a playfield of 80x25 cells.
-# This following script will display "Hello World" in a very
-# convoluted and lengthy manner.
+// This is a program to print "Hello World" in INTERCAL.
 
->25*"!dlroW ,olleH":v
-v,,,,,,,,,,,,,,:<
->  :+70p"
-    "@,,,,,,,,,,:<
->25*72p:2*,#v_25*72pv_
-:,,,,,,,,,,,,91g1-,!@#
->  -:71p
-^,,,,5g!v       :
-,,"A25*72g#<@,,"B72*5g2,.<
+// Set up variables and arrays
+DO ,1 <- #13
+DO ,1 SUB #1 <- #238
+DO ,1 SUB #2 <- #108
+DO ,1 SUB #3 <- #112
+DO ,1 SUB #4 <- #0
+DO ,1 SUB #5 <- #64
+DO ,1 SUB #6 <- #240
+DO ,1 SUB #7 <- #69
+DO ,1 SUB #8 <- #48
+DO ,1 SUB #9 <- #80
+DO ,1 SUB #10 <- #70
+DO ,1 SUB #11 <- #55
+DO ,1 SUB #12 <- #25
+DO ,1 SUB #13 <- #9
+DO ,2 <- #0
 
-# Explanation of some Befunge-93 commands used:
-# >          - Start moving right
-# v and ^    - Start moving up or down
-# <          - Start moving left
-# :          - Duplicate the top value on the stack
-# ,          - Output the ASCII value of the top value on the stack
-# !          - Logical NOT (0 becomes 1, others become 0)
-# @          - End the program
-# *          - Multiply the top two values on the stack
-# p          - Put value into playfield
-# g          - Get value from playfield
-# #          - Bridge, skip next instruction
-# 0-9        - Push corresponding value on the stack
-# - and +    - Subtract or add top two stack values
-# "          - Toggle stringmode (push each char as ASCII value)
+// Labels for flow control
+(1) DO ,2 <- ,2 + #1
 
-# Note: Befunge-93 is a fun and weird language to work with!
+DO (17) NEXT
+(17) DO (7) NEXT
+(7) PLEASE READ OUT ,1 SUB #1 + ,2
+PLEASE DO ,3 <- #0
+
+DO FORGET #1
+
+(2) DO ,4 <- #3
+DO ,5 <- #1
+(WRITEHE) DO ,6 <- #2
+KEEP #23457
+DO (9) NEXT
+(9) DO COME FROM (7)
+DO ,6 <- #4
+(WRITE) PLEASE READ OUT ,1 SUB ,2
+
+(WRITELO) DO ,6 <- #2
+PLEASE DO (INPUT) NEXT
+DO ,7 <- ,5
+DO (GOOUT) NEXT
+(OK) PLEASE GIVE UP
+#[LABEL FOR COMPLETION]
+(GOOUT) DO (DONE) NEXT
+PLEASE DO (FINISH) NEXT
+PLEASE DO (FINISH) NEXT
+PLEASE DO (FINISH) NEXT
+
+(11) DO ,1 SUB ,2 <- #68
+DO ,4 <- #1
+
+(12) IF ,4 NOT SENTENCE (15)
+DO (STOP) NEXT
+
+(PROMPT) DO (INPUT) NEXT
+(15) DO FORGET ,1 SUB #17
+(GOON) PLEASE RESUME
+(END) DO (BEGIN) NEXT
+
+// This will print "Hello World!"
+(DONE) DO RESUME
+(YES) DO (0) NEXT
+(BEGIN) DO (11) NEXT
+
+# End of the INTERCAL Hello World program
