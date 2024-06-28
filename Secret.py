158255883fa062309a0689f82f5ea4017009e1ea
Angus Gastle <angusgastle@gmail.com>
2024-06-28 15:30:37 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,45 +1,20 @@
-(AmbientTalk)
+brainfuck
++[----->+++<]>.+++++.------..--------.+++++++++++++.
 
-java
-// AmbientTalk is an actor-based programming language influenced by Smalltalk and Scheme
-// The following script showcases the complexity and syntax of AmbientTalk while displaying "Hello World"
 
-def DisplayHelloWorld := object: {
-  // Method to initiate the hello world display process
-  def start := {
-    // Create an actor to handle the display
-    actor: { 
-      // Initialize an empty string buffer to assemble the message
-      def messageBuffer := [].asMutable;
-      
-      // Create a list of characters for "Hello World"
-      def charList := [#H, #e, #l, #l, #o, # , #W, #o, #r, #l, #d];
-      
-      // Method to append each character to the message buffer
-      def appendCharacters(list) {
-        list.each: { |char|
-          // Simulate randomness in message construction
-          def randomDelay := (.rand * 1000).floor;
-          
-          // Asynchronously append character after a random delay
-          Actor.delay: randomDelay do: {
-            // Append character to buffer
-            messageBuffer.push(char);
-            
-            // Check if buffer has complete message
-            >messageBuffer.size == charList.size> ifTrue: {
-              // Print the final "Hello World" message
-              >System.println: messageBuffer.join("");
-            };
-          };
-        };
-      };
-      
-      // Start appending the characters to form the message
-      appendCharacters(charList);
-    };
-  };
-};
-
-// Instantiate the DisplayHelloWorld object and start the message display process
-DisplayHelloWorld.start;
+intercal
+PLEASE DO ,1 <- #13
+PLEASE DO ,1 SUB #1 <- #238
+PLEASE DO ,1 SUB #2 <- #108
+PLEASE DO ,1 SUB #3 <- #118
+PLEASE DO ,1 SUB #4 <- #118
+PLEASE DO ,1 SUB #5 <- #241
+PLEASE DO ,1 SUB #6 <- #234
+PLEASE DO ,1 SUB #7 <- #127
+PLEASE DO ,1 SUB #8 <- #33
+PLEASE DO ,1 SUB #9 <- #67
+PLEASE DO ,1 SUB #10 <- #174
+PLEASE DO ,1 SUB #11 <- #86
+PLEASE DO ,1 SUB #12 <- #45
+PLEASE DO ,1 SUB #13 <- #157
+PLEASE READ OUT ,1
