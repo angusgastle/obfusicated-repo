c714c338d0328d427683cbee8823eccc478703f0
Angus Gastle <angusgastle@gmail.com>
2024-06-29 03:30:18 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,15 +1,9 @@
-(: comment: MUMPS (Massachusetts General Hospital Utility Multi-Programming System) code to display "Hello World" :)
-main ; This is the main entry point
- new message ; Declare a new variable 'message'
- set message="Hello World" ; Assign "Hello World" to 'message' variable
- do displayMessage(message) ; Call 'displayMessage' subroutine with 'message' argument
- quit ; End of the program
-
-displayMessage(msg) ; Subroutine to display the message
- new len,i,tempChar ; Declare variables 'len', 'i', and 'tempChar'
- set len=$length(msg) ; Get the length of the message
- for i=1:1:len { ; Loop through each character in the message
- . set tempChar=$extract(msg,i) ; Extract the ith character from the message
- . write tempChar ; Write/output the character
- }
- quit ; End of the subroutine
+brainfuck
+>+++++++++[<++++++++>-]<. # H
+>++++[<+++++++>-]<+.      # e
++++++++..+++.             # lloo
+>>>++++[<+++++++>-]<.     #  ,W
+<<++[>------<]>.<+.       # o
+-------------.            # r
+++++[>+++++++<]>++.       # l
+>++++[>++++++++<-]>+.     # d
