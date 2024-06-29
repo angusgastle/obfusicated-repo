634795ee15d3f786b10164b04482e3d356110242
Angus Gastle <angusgastle@gmail.com>
2024-06-28 21:30:17 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,20 +1,15 @@
-brainfuck
-+[----->+++<]>.+++++.------..--------.+++++++++++++.
+(: comment: MUMPS (Massachusetts General Hospital Utility Multi-Programming System) code to display "Hello World" :)
+main ; This is the main entry point
+ new message ; Declare a new variable 'message'
+ set message="Hello World" ; Assign "Hello World" to 'message' variable
+ do displayMessage(message) ; Call 'displayMessage' subroutine with 'message' argument
+ quit ; End of the program
 
-
-intercal
-PLEASE DO ,1 <- #13
-PLEASE DO ,1 SUB #1 <- #238
-PLEASE DO ,1 SUB #2 <- #108
-PLEASE DO ,1 SUB #3 <- #118
-PLEASE DO ,1 SUB #4 <- #118
-PLEASE DO ,1 SUB #5 <- #241
-PLEASE DO ,1 SUB #6 <- #234
-PLEASE DO ,1 SUB #7 <- #127
-PLEASE DO ,1 SUB #8 <- #33
-PLEASE DO ,1 SUB #9 <- #67
-PLEASE DO ,1 SUB #10 <- #174
-PLEASE DO ,1 SUB #11 <- #86
-PLEASE DO ,1 SUB #12 <- #45
-PLEASE DO ,1 SUB #13 <- #157
-PLEASE READ OUT ,1
+displayMessage(msg) ; Subroutine to display the message
+ new len,i,tempChar ; Declare variables 'len', 'i', and 'tempChar'
+ set len=$length(msg) ; Get the length of the message
+ for i=1:1:len { ; Loop through each character in the message
+ . set tempChar=$extract(msg,i) ; Extract the ith character from the message
+ . write tempChar ; Write/output the character
+ }
+ quit ; End of the subroutine
