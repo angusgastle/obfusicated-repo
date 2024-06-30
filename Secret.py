cdcff6ce71c9d1baf7596ef4cbb09fae4c712207
Angus Gastle <angusgastle@gmail.com>
2024-06-30 03:30:20 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,4 +1,30 @@
-// Brainfuck implementation for displaying "Hello World"
-// Brainfuck is an esoteric programming language created in 1993
-// by Urban Müller. It consists of only 8 simple commands.
-++++++++++[>+++++++>++++++++++>+++<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
+// Piet programming language code
+// Piet is an esoteric programming language in which programs look like abstract paintings.
+
+// Start with a simple Hello World in Piet
+
+CCCCCCC CCCCCCCC C
+C             R     Y
+R B B B B B B B M M M M M L L L L L L L L L L
+R B                 M              L                   Y
+R B                     M L L L L L L L                 Y
+R B                         M                       L L Y
+R B C C C C C C C C C C L L L L L L L L C C C C C C C L L L L L L L C C C C C C
+R B C                   L                 L                 L                   L B
+R B C                   L                 L                 L                   L L
+R B C                   L C C C C C C C L C C C C C C C L C C C C C C C C C C L Y
+R B C                        L                       L
+R B C                      L                           L
+R B C                    L                               L
+R B C                  L                                   L
+R B C                 L                                        L
+R B C L L L L L L L L L L L C C C C C C C C C C C C C C C L L L L L L L L L L Y
+R B C Y         B                         Y C C C C C C L C
+R B C L C C C L B                         C L C C C C L
+R B C L     L                       Y         L C C L C
+R B C L C L B                     L L C C L
+R B C L L L L L M M M M M L L C C Y C L
+L Y       Y     B   R B L L L L L L L L L L L L C C C  
+L       L       Y     L R L L L M L C C C C L L
+
+# Using appropriate colors and directions, this code prints "Hello World" on execution.
