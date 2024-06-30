f93ea364c9717d300356d0ab701da6dcbb44921f
Angus Gastle <angusgastle@gmail.com>
2024-06-30 15:30:22 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,22 +1,8 @@
-// Brainfuck code to print "Hello World"
-+++++++++++++++++++++++++++++++++++++++++++++
->+++++++++++++++++++++++++++++++++++++
->+++++++++++++++++++++++++++++++++++++
->+++++++++++++++++++++++++++++++++++++++++++++
->+++++++++++++++++++++++++++++++++++++
-<<<<
-[
->++++++++++++++++++++[>++++++++++++++++>----
->+<<<<[->+>+>+>>>>>.--[->+++++<]>+++.>[--->+<
-]>----.++[----->++<]>++++.----.+++.>>[--->++<
-]>+.+++.------.<-.>[--->++<]>++.[----->+<]>-.
->.+++.-----.-[--->+<]>-.[-->+++<]>>>.<++++++++
-[-->+++++<]>-.]>.<<
-]
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++.>.+++.------.--------.>+.>.
 
-# This code might seem random and difficult to understand, but it essentially manipulates machine memory
-# to print characters one by one
-# The `>` and `<` move the memory pointer to the right and left respectively
-# The `+` and `-` increment and decrement the byte at the memory pointer respectively
-# The `.` command outputs the byte at the memory pointer as a character
-# The `[` and `]` commands create loops that repeat until the byte at the memory pointer is zero
+
+befunge
+>25*"!dlroW ,olleH":v
+                 v:,_@
+                 >  ^ 
