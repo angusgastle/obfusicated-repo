6e3af226dcbcb61c9904f62a2ce89c728c4146b9
Angus Gastle <angusgastle@gmail.com>
2024-06-30 09:30:23 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,30 +1,22 @@
-// Piet programming language code
-// Piet is an esoteric programming language in which programs look like abstract paintings.
+// Brainfuck code to print "Hello World"
++++++++++++++++++++++++++++++++++++++++++++++
+>+++++++++++++++++++++++++++++++++++++
+>+++++++++++++++++++++++++++++++++++++
+>+++++++++++++++++++++++++++++++++++++++++++++
+>+++++++++++++++++++++++++++++++++++++
+<<<<
+[
+>++++++++++++++++++++[>++++++++++++++++>----
+>+<<<<[->+>+>+>>>>>.--[->+++++<]>+++.>[--->+<
+]>----.++[----->++<]>++++.----.+++.>>[--->++<
+]>+.+++.------.<-.>[--->++<]>++.[----->+<]>-.
+>.+++.-----.-[--->+<]>-.[-->+++<]>>>.<++++++++
+[-->+++++<]>-.]>.<<
+]
 
-// Start with a simple Hello World in Piet
-
-CCCCCCC CCCCCCCC C
-C             R     Y
-R B B B B B B B M M M M M L L L L L L L L L L
-R B                 M              L                   Y
-R B                     M L L L L L L L                 Y
-R B                         M                       L L Y
-R B C C C C C C C C C C L L L L L L L L C C C C C C C L L L L L L L C C C C C C
-R B C                   L                 L                 L                   L B
-R B C                   L                 L                 L                   L L
-R B C                   L C C C C C C C L C C C C C C C L C C C C C C C C C C L Y
-R B C                        L                       L
-R B C                      L                           L
-R B C                    L                               L
-R B C                  L                                   L
-R B C                 L                                        L
-R B C L L L L L L L L L L L C C C C C C C C C C C C C C C L L L L L L L L L L Y
-R B C Y         B                         Y C C C C C C L C
-R B C L C C C L B                         C L C C C C L
-R B C L     L                       Y         L C C L C
-R B C L C L B                     L L C C L
-R B C L L L L L M M M M M L L C C Y C L
-L Y       Y     B   R B L L L L L L L L L L L L C C C  
-L       L       Y     L R L L L M L C C C C L L
-
-# Using appropriate colors and directions, this code prints "Hello World" on execution.
+# This code might seem random and difficult to understand, but it essentially manipulates machine memory
+# to print characters one by one
+# The `>` and `<` move the memory pointer to the right and left respectively
+# The `+` and `-` increment and decrement the byte at the memory pointer respectively
+# The `.` command outputs the byte at the memory pointer as a character
+# The `[` and `]` commands create loops that repeat until the byte at the memory pointer is zero
