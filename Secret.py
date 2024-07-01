45dbe6ef95fe7c053443ade71ea5899e9a116bea
Angus Gastle <angusgastle@gmail.com>
2024-07-01 03:30:37 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,64 +1,44 @@
-Q-BAL
+// Whitespace language script for displaying "Hello World"
 
-ARRAY [255], CH;              // Create an array for character display with size 255
-INTEGER I, RND;               // Define integers for iteration and random number
+// Initialize stack
+// Using space for 0 and tab-space for 1
 
-EXTERNAL ROUTINE init_rand;   // Declare external routine for random number initialization
-EXTERNAL ROUTINE get_rand;    // Declare external routine for getting random numbers
+// Push "H" (72)
+[Space][Space][Space][Tab][Space][Space][Space][Space][Space][Space][Tab][Linefeed]
+// Push "e" (101)
+[Space][Space][Space][Tab][Space][Space][Space][Tab][Space][Tab][Linefeed]
+// Push "l" (108)
+[Space][Space][Space][Tab][Space][Space][Tab][Space][Space][Tab][Linefeed]
+// Push "l" (108)
+[Space][Space][Space][Tab][Space][Space][Tab][Space][Space][Tab][Linefeed]
+// Push "o" (111)
+[Space][Space][Space][Tab][Space][Space][Tab][Space][Tab][Tab][Linefeed]
+// Push " " (32)
+[Space][Space][Space][Tab][Space][Space][Space][Space][Tab][Linefeed]
+// Push "W" (87)
+[Space][Space][Space][Tab][Space][Space][Tab][Tab][Tab][Linefeed]
+// Push "o" (111)
+[Space][Space][Space][Tab][Space][Space][Tab][Space][Tab][Tab][Linefeed]
+// Push "r" (114)
+[Space][Space][Space][Tab][Space][Tab][Space][Space][Tab][Linefeed]
+// Push "l" (108)
+[Space][Space][Space][Tab][Space][Space][Tab][Space][Space][Tab][Linefeed]
+// Push "d" (100)
+[Space][Space][Space][Tab][Space][Space][Space][Tab][Space][Linefeed]
 
-!------------------------ Initialization ------------------------
-start:
-    CALL init_rand;           // Initialize random number generator
+// Print Characters
+// Output characters
+[Tab][Linefeed][Space][Space] // H
+[Tab][Linefeed][Space][Space][Space][Linefeed] //e
+[Tab][Linefeed][Space][Tab][Linefeed] // l
+[Tab][Linefeed][Space][Tab][Linefeed] // l
+[Tab][Linefeed][Space][Tab][Space][Linefeed] // o
+[Tab][Linefeed][Tab][Space][Linefeed] //   space
+[Tab][Linefeed][Space][Tab][Tab][Space][Linefeed] // W
+[Tab][Linefeed][Space][Tab][Space][Tab][Linefeed] // o
+[Tab][Linefeed][Space][Space][space][tab] //r
+[Tab][Linefeed][Space][Tab][Linefeed] // l
+[Tab][Linefeed][Tab][space][tab][linefeed] // d
 
-    ;---------------------------------------------------------------
-    ; This section of loop assigns each ASCII value of "Hello World"
-    ; to the CH array at each index I (0-10) respectively
-    ;---------------------------------------------------------------
-    LOOP I FROM 0 TO 10:
-        IF I == 0:             // First character 'H', ASCII 72
-            CH(0) = 72;
-        ELSE IF I == 1:        // Second character 'e', ASCII 101
-            CH(1) = 101;
-        ELSE IF I == 2:        // Third character 'l', ASCII 108
-            CH(2) = 108;
-        ELSE IF I == 3:        // Fourth character 'l', ASCII 108
-            CH(3) = 108;
-        ELSE IF I == 4:        // Fifth character 'o', ASCII 111
-            CH(4) = 111;
-        ELSE IF I == 5:        // Sixth character ' ', ASCII 32 (space)
-            CH(5) = 32;
-        ELSE IF I == 6:        // Seventh character 'W', ASCII 87
-            CH(6) = 87;
-        ELSE IF I == 7:        // Eighth character 'o', ASCII 111
-            CH(7) = 111;
-        ELSE IF I == 8:        // Ninth character 'r', ASCII 114
-            CH(8) = 114;
-        ELSE IF I == 9:        // Tenth character 'l', ASCII 108
-            CH(9) = 108;
-        ELSE:                  // Eleventh character 'd', ASCII 100
-            CH(10) = 100;
-    END;
-
-!------------------------ Displaying Randomized "Hello World" ------------------------
-    ;---------------------------------------------------------------
-    ; Another loop to randomly emit each character stored in CH
-    ; array, using get_rand to get a random index
-    ;---------------------------------------------------------------
-    LOOP I FROM 0 TO 10:
-        RND = get_rand(10);   // Get random number within 0 to 10
-        print CH(RND);        // Print corresponding character from array
-    END;
-END_PROGRAM;
-
-!------------------------ External Routines Definitions ------------------------
-init_rand:
-    ; Routine to initialize random seed based on system clock
-    SYSTEM.CLOCK -> RANDOM.SEED;
-    RETURN;
-
-get_rand:
-    ; Routine to get a random number between 0 and the limit specified
-    ; Note: LIMIT is passed during the function call
-    PARAMETER INTEGER LIMIT;
-    RANDOM(LIMIT) -> RETURN_VALUE;
-    RETURN;
+// End Program with Proper Termination
+[Tab][Linefeed][Linefeed]
