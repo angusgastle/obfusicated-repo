ce8e49d126cf7f1c0d7687a9b04125562cb8aafe
Angus Gastle <angusgastle@gmail.com>
2024-07-03 09:31:26 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,19 +1,10 @@
-
-; Malbolge programming language
-; This script will output "Hello World" when executed.
-; The Malbolge programming language is designed to be extremely difficult to program in.
-
-(=<`#9]~6ZY32VxwvutTSRQPonm<k**H@}>zy+{
-VUTSRPONMLKJM"
-zzz}
-.. )
-!/.}
+(=: mod@_ 144>",0(?(<((>06714<.(,@_2*(,2*(I*:" "))))?1/(''.2+(*I*:(I-:@__=.(****10-value ?" "=[_T)-:tO_.))*2)=.(01)3-@_ %((!)(..("))(61)=)_ !!__!%(@=(*((((.=(cC))))))"
+.:()) if(cC)(.(*****).)*!(,(--@I)x (*:I:" "),(! (___,) !))))@): (c)-) (                                        
+ (*((for=)_!('=))=__(__(.__'__))*(--I)*(:_tVALUET@__.t%.::(: )).__.!!__;x*(::((__*]( c_ if!!!cx@c_STREAM@@x)).!{t}@.(if!=@c_(::=).*)*
+(60)_T@)((,@!!_,@__{for)*!(c)(:*cx.')[mE@(.{-=_Mtx*).!!@)._(.!!__@,).,,==)cx(_--,,_ ** (.)*M=_T(@!value,).*=!_=IF__% ==,[M@*)))=**cv.!_ .=. ==**,._!!) IF!!x= (.(M()@value) ([if)^^. =_,@*%.tSTREAM_==.=if. (%)FOR=_.(cT@(==,(value)*_(^@C,x@). ._!.}@@,_.) ,@!.==))
 !
-^< :9*432/.-,+)&%$$#"!" o"
-  ~}|{zyw u~gVQ >Ed ^@A<sLo/kjZnJeDcY`;|:9v87r54 };`x9ul#"
- kwf ZY
-  xwvutsrp0 gQN}< k "<,"
-  zY ~x~r~o ~lj~hf~[WYzO :
-
-
-Note: This code is meant for the incredibly esoteric and intentionally convoluted Malbolge language, which is difficult to understand and write. The above script should output "Hello World" in a highly unconventional and complex way.
+Hello World ::.__)) :. :x (.)***_!__**
+I-@!.@(:,!,:,**,.)((_***=(@=)
+=I@)=(%,(___).!!();)!@_**Hello World==(00)	random_obscure_language(!!
+";@!
+	Hello World is displayed here"
