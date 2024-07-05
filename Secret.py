79e4b5bb36f4479f40dc095c4a0c804860e60a06
Angus Gastle <angusgastle@gmail.com>
2024-07-05 15:31:10 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,55 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++<<<-]>.>++.+++++++..+++.<++++++++.--------.+++.------.--------.>+.>.
+
+REM * Single Line Comment (Used for Brief Descriptions)
+REM Multi-Line Comment: Start 
+REM This script is written in INTERCAL, a very obscure esolang.
+REM The primary goal is to produce the famous "Hello World" output.
+REM Multi-Line Comment: End 
+
+DO ,1 <- #13
+DO ,2 <- #3
+DO ,3 <- #91
+DO ,4 <- #69
+DO ,5 <- #62
+DO ,6 <- #111
+DO ,7 <- #87
+DO ,8 <- #127
+DO ,9 <- #32
+DO ,10 <- #121
+
+DO :1 <- #72
+DO :2 <- #101
+DO :3 <- #108
+DO :4 <- #108
+DO :5 <- #111
+DO :6 <- #32
+DO :7 <- #87
+DO :8 <- #111
+DO :9 <- #114
+DO :10 <- #108
+DO :11 <- #100
+
+PLEASE DO ,1 SUB#1 <- #1
+PLEASE READ OUT ,1
+
+PLEASE DO (5) NEXT
+PLEASE DO ,9 <- .1$+1#1.
+PLEASE DO (24000) NEXT
+PLEASE DO ,2 <- .2$+1#2.
+PLEASE DO ,3<- .3$+1#3.
+PLEASE DO [1] NEXT
+PLEASE DO ,4 <- .4$+1#4.
+PLEASE DO ,5 <- .5$+1#5.
+PLEASE DO [2] NEXT
+PLEASE DO ,6 <- .6$+1#6.
+PLEASE DO ,7 <- .7$+1#7.
+PLEASE DO ,,6 NEXT
+PLEASE DO ,8 <- .8$+1#8.
+PLEASE DO ,9 <- .9$+1#9.
+PLEASE DO >2$#9 NEXT
+PLEASE DO :3 <- .3$+1#10.
+PLEASE DO [3] NEXT
+PLEASE DO ,1 <-> :7$#10.
+PLEASE DO ,9 <- .6#11.
+
+PLEASE READ OUT ,9
+PLEASE GIVE UP
