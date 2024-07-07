eb2626186425a40dfe097fc470e4b46a9f881a50
Angus Gastle <angusgastle@gmail.com>
2024-07-07 15:30:44 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,33 +1,53 @@
 brainfuck
-+++++ +++++             initialize cell[0] to 10
-[                       start loop
-    >++++++            increment cell[1] to 60
-    [                  start nested loop
-        >++            increment cell[2] to 2, this will be used for 'h'
-        >+++           increment cell[3] to 3, this will be used for 'e'
-        >+++++         increment cell[4] to 5, this will be used for 'l'
-        >++.           increment cell[5] to 2, then 1 step for '.' (68 in ASCII), this will be used for 'o'
-        >++            increment cell[6] to 2, this will be used for ' '
-        >---.          decrement cell[7] to 3, then 1 step back, resulting in '/' (47 in ASCII), this will be used for 'W'
-        >+.            increment cell[8] to 1, + steps more for 'o' => resulting in 'r'
-        <++++++        increment cell[6] (current cell) to 63 for 'd' by - 28 steps then + 3 steps
-        <<             return position to cell[4] (holding 'o')
-        <              return cell[3] to 3 (holding 'e'), 3 + 1 = 'l'
-        -              decrement cell to 2
-        <              go to cell[2] skip 2 steps, find cell 1 -> decrement cell to 1 remaining steps at 60
-    -                  end nested loop decrement cell[1]
-<<<<<+                add remaining increment to cell[0] to 60 steps for ending loop
-]                     end loop
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++.>+++.------.--------.>+.>.
 
-Print "Hello World!"
->---.               cell[1] is now 72; 'H'
->++.               cell[2] is now 101; 'e'
->+.                cell[3] is now 108; 'l'
->.                 cell[4] is now 108; 'l'
-<->.               decrement cell for holding letter 'o' = 111, rest steps add it forward
-<-.                cell[1] is now 32; space ' '
->.                 cell[2] is now 87; 'W'
-+++.               increment cell holding;adding letters for keeping the result 'o' to 'r'
-<<++.              decrement cell at particular holding steps;from starting at
-<-.                go to cell at global holding steps
->.                 final result character 'd'
+# Explanation:
+# ++++++++++        -> This sets the first cell to 10
+# [                 -> Start of loop, repeat until first cell is 0
+#   >+++++++        -> Move to the second cell and increment it 7 times (second cell = 70)
+#   >++++++++++     -> Move to the third cell and increment it 10 times (third cell = 100)
+#   >+++            -> Move to the fourth cell and increment it 3 times (fourth cell = 30)
+#   >+              -> Move to the fifth cell and increment it once (fifth cell = 10)
+#   <<<<-           -> Move back to the first cell and decrement it once
+# ]                 -> End of loop
+
+# Now that cells are set up:
+# [ 10, 70, 100, 30, 10 ]
+
+# We will start printing "Hello, World!"
+
+# Print 'H'
+# >++.              -> Move to the second cell and print character (cell 70, ASCII 'H')
+
+# Print 'e'
+# >+.               -> Move to the third cell and print character (cell 100, ASCII 'e')
+
+# Print 'l'
+# ++++++++.         -> Increment the third cell 7 times (107, ASCII 'l'), print character
+
+# Print 'l'
+# .                 -> Print character at the third cell again
+
+# Print 'o'
+# +++.              -> Increment the third cell 3 times (110, ASCII 'o'), print character
+
+# Print ','
+# >++.              -> Move to the fourth cell and print character (cell 30, ASCII ',')
+
+# Print ' '
+# <<+++++++++++++.  -> Move back to the second cell, increment it 15 times (85, ASCII ' '), print character
+
+# Print 'W'
+# >+++.             -> Move to the third cell, increment its content 3 times (103, ASCII 'W'), print character
+
+# Print 'o'
+# ------.           -> Decrement the third cell 6 times (97, ASCII 'o'), print character
+
+# Print 'r'
+# --------.         -> Decrement the third cell 8 times (89, ASCII 'r'), print character
+
+# Print 'l'
+# >+.               -> Move to the fourth cell and print character (cell 30, ASCII 'l')
+
+# Print 'd'
+# >.                -> Move to the fifth cell and print character (cell 10, ASCII 'd')
