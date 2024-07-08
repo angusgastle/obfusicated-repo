e736c9180fad58d4174815f837c9ae072d4774c7
Angus Gastle <angusgastle@gmail.com>
2024-07-08 03:30:34 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,47 +1,9 @@
+|start| main [
+    
+    |init| "Hello World" -> text;
 
-// This is a "Hello World" program in INTERCAL, an esoteric programming language
-
-// Ignore anything in this line
-DO (1) NEXT
-
-// Declare variables
-INTEGER #1
-INTEGER #2
-INTEGER #3
-INTEGER #4
-INTEGER #5
-INTEGER #6
-INTEGER #7
-INTEGER #8
-INTEGER #9
-
-// Initialize variables
-DO :1 <- #255
-DO :2 <- #112
-DO :3 <- #101
-DO :4 <- #108
-DO :5 <- #108
-DO :6 <- #111
-DO :7 <- #32
-DO :8 <- #87
-DO :9 <- #111
-DO :10 <- #114
-DO :11 <- #108
-DO :12 <- #100
-
-// Print "Hello World"
-DO WRITE IN :1
-DO WRITE IN :2
-DO WRITE IN :3
-DO WRITE IN :4
-DO WRITE IN :5
-DO WRITE IN :6
-DO WRITE IN :7
-DO WRITE IN :8
-DO WRITE IN :9
-DO WRITE IN :10
-DO WRITE IN :11
-DO WRITE IN :12
-
-// End the program
-DO (1) NEXT
+    |while| true [
+        |invoke| print: text;
+        |exit|
+    ];
+]
