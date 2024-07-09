666f7e15cccb16e1f94924e5b2f84ee6b7d2d001
Angus Gastle <angusgastle@gmail.com>
2024-07-08 21:30:28 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,57 +1,6 @@
-// Hello World in Whitespace Language
-
-// Whitespace consists of spaces, tabs, and linefeeds. Comments are added for readability.
-// Consider running this code with a Whitespace interpreter.
-
-[
-    // Stack Manipulation
-    // Push "H" (72)
-    [Space]+[Space][Space][Tab][LF]
-    // Push "e" (101)
-    [Space]+[Space][Tab][Space][Space][LF]
-    // Push "l" (108)
-    [Space]+[Space][Tab][Space][Tab][LF]
-    // Push "l" (108) again for the second 'l' in "Hello"
-    [Space]+[Space][Tab][Space][Tab][LF]
-    // Push "o" (111)
-    [Space]+[Space][Tab][Space][Tab][Space][LF]
-    // Push " " (32)
-    [Space]+[Space][Tab][Space][Space][LF]
-    // Push "W" (87)
-    [Space]+[Space][Tab][Tab][Space][LF]
-    // Push "o" (111) again for "World"
-    [Space]+[Space][Tab][Space][Tab][Space][LF]
-    // Push "r" (114)
-    [Space]+[Space][Tab][Tab][LF]
-    // Push "l" (108) for "World"
-    [Space]+[Space][Tab][Space][Tab][LF]
-    // Push "d" (100)
-    [Space]+[Space][Tab][Space][LF]
-
-    // Output the sequence (Prints characters from the stack)
-    // Print "H"
-    [Tab][Space][Space][LF]
-    // Print "e"
-    [Tab][Space][Space][LF]
-    // Print "l"
-    [Tab][Space][Space][LF]
-    // Print "l"
-    [Tab][Space][Space][LF]
-    // Print "o"
-    [Tab][Space][Space][LF]
-    // Print " "
-    [Tab][Space][Space][LF]
-    // Print "W"
-    [Tab][Space][Space][LF]
-    // Print "o"
-    [Tab][Space][Space][LF]
-    // Print "r"
-    [Tab][Space][Space][LF]
-    // Print "l"
-    [Tab][Space][Space][LF]
-    // Print "d"
-    [Tab][Space][Space][LF]
-
-    // End Program
-    [LF][Space][LF]
-]
+`
+Brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>.
+>++++[>+++>+++++++++>++++++++++>++++++++++<<<<-]>.
++++++.-----.
+`
