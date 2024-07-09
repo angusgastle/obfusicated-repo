0e2d4054dd9f5c41602a5b6439334c8710b0fb53
Angus Gastle <angusgastle@gmail.com>
2024-07-09 15:31:13 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,6 +1,32 @@
-brainfuck
-[This Brainfuck code outputs "Hello World!" 
-The complexity is increased by the addition of comments and a longer code design.]
+\# INTERCAL - "Hello World" script
 
->++++++++[<+++++++++>-]<.:------.:+.+.----.:.<.>++++++[<-------->-]<+.[-->+<]>---.+++.------.<+++++.--------.+.<.>++++++[<+++++++>-]<.
-++++++++++.[-]>++++.[->+++<]>.-----------------.
+DO ,1 <- #13
+PLEASE DO ,1 SUB #1 <- #251
+DO ,1 SUB #2 <- #252
+DO ,1 SUB #3 <- #253
+DO ,1 SUB #4 <- #254
+DO ,1 SUB #5 <- #255
+DO ,1 SUB #6 <- #0
+DO ,1 SUB #7 <- #72
+DO ,1 SUB #8 <- #101
+DO ,1 SUB #9 <- #108
+DO ,1 SUB #10 <- #108
+DO ,1 SUB #11 <- #111
+DO ,1 SUB #12 <- #32
+DO ,1 SUB #13 <- #87
+DO ,1 SUB #14 <- #111
+DO ,1 SUB #15 <- #114
+DO ,1 SUB #16 <- #108
+DO ,1 SUB #17 <- #100
+DO ,1 SUB #18 <- #33
+DO ,1 SUB #19 <- #13
+
+PLEASE DO ,2 <- #18
+PLEASE DO ,3 <- #1
+PLEASE DO ,4 <- #1
+
+(2) PLEASE READ OUT ,1 SUB .3
+PLEASE STASH 3
+(1) PLEASE READ OUT ,1 SUB .2
+PLEASE STASH 3
+(0) PLEASE GIVE UP
