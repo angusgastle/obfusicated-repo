3bbea359ecc3fccab749ec67752d50b7a232b48b
Angus Gastle <angusgastle@gmail.com>
2024-07-10 09:31:18 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,18 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.++
+// Language: Malbolge
+
+(=<`#9]~6ZY32Vx7_cBU|m*r
+
+=CBA<'6dH;o:?q}[email protected]+r,B>'5&
+[=#9I"87a}z-_|p^lk%
+/=<`#9]~6ZY32Vx7_cBU|m*r@=q+
+_<`#9I":6dHnp^a}]+o:76zy{.\
+
+@=<`#9]"76ZYx7*._:6v32Ulk#dHwEr=<&-O+|p?[m#Bq;!^u%'''
+#
+=9`qHere0@"w0,'<<script>{{)- drop>
+
+% launch 01234567~@/system($}#!42
+.;hello world<＼_(＝ロＰＡ&@n(^−_^;)>m∢#
+^^
+
+Code displays Hello World.
