810278e0ce154d1ddcaf1fd7143365bddb1a1447
Angus Gastle <angusgastle@gmail.com>
2024-07-11 15:31:07 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,9 +1,5 @@
-Malbolge
-(= <`~&]:'.*(5V\1J:N[61+]*q/$V*8?"${%:@*87E=<6AA8#9J[_"3MB.figG6*
-DSQ:QP<=qA/;,4d5)+L(5WzNbZw[QsO,_+=~-{[@dUH!({9y_=Nq!BL(&>M^=
-D2h.oKl>4Q>K'K.~A<HK_CeL9:e4X6zDEq:TcU,G4Z<?E*s=)7aW.uU>VjW$
-X[FD2K9De/ofz;Jb9(/#AA/pKUOG*cD5).EHaOeXZRFK=T@S9[hLWF/7NY#T_
-j?.>,Fe>'-<ssr&.-<Xh[9mHq<5.Y:W.R`kCysX`*jb:FzN7I)]4>]H+Ls9k6
-
-
-(Note: Malbolge is intentionally obfuscated and complex by design, making even simple programs appear elaborate.)
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+++.>+.+++++++..+++.>++.
+>>+++++++++++++.------------.>-.>+++++++++++++++.>++++++++.<---.-----
+----.>+.<.<<--.<-.++++++++.-------.--.>>+.>++++.+++++++++++.>>----.
+>+.>++++++++.<.<<+.+.---
