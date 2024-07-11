7cd96b0b8f7485860edd3cfad5db8b1c57abcb9b
Angus Gastle <angusgastle@gmail.com>
2024-07-11 03:30:41 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,16 +1,101 @@
-befunge
->    v
-v <  >.v
-|    ; |
-v^  ,  <
->  >$"dlroW olleH" ,,,,,,,,,,,,,,,,,,,
-v,,,,,,,,,,,,,,,,v
-@,,,,,,,,,,,,,,,,^< 
-# 
-# Push characters onto the stack in reverse order
-> 84*1-1-1-1-1-1+9*8*$"!dlroW olleH">:<
+brainfuck
+++++++++++[>+++++++>++++++++++>+++<<<-]>>>+++++++++++++>--.>+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>++++.
 
-# Print characters by popping them off the stack
-v >  "<,"<_@
-   ; # @, Executes the main instruction and exits when it hits '@'.
-v^   >,,<
+
+whitespace
+[S S S T    S S S S N
+_Push_104]
+[S S S T S S S S N
+_Push_101]
+[S S S T  S S S N
+_Push_108]
+[S S S T  S S S  S T    T    N
+_Push_108_LF]
+[S S S T S S S    S T N
+_Push_111]
+[S S S T    S S    S S N
+_Push_32]
+[S S S T    S T N
+_Push_87]
+[S S S T    S S S    S N
+_Push_111]
+[S S S T    S S N
+_Push_114]
+[S S S T    S S S  S N
+_Push_108]
+[S S S T S S S S N
+_Push_100]
+[L F S L
+_Label_]
+[L L S N
+_Call_Label]
+[S S S T  S N
+_Push_10]
+[S S S T  S T N
+_Push_1]
+[T    S T    S L
+_Copy_2nd_Stack_Item]
+[T    S S T  T S N
+_Add]
+[L F S L
+_Label_]
+[L F N
+_End_Subroutine]
+
+
+Piet
+# Color code for 'Hello, World!' using Piet:
+# codel size: 1x1.
+[   c* a_ ]     [   c9 a! ]
+# This code generates the image-based program. Import it into a Piet interpreter.
+
+
+LOLCODE
+HAI 1.2
+CAN HAS STDIO?
+VISIBLE "HELLO WORLD"
+KTHXBYE
+
+
+INTERCAL
+DO ,1 <- #13
+DO :1 <- #192
+PLEASE WRITE OUT ,1
+DO ,1 <- #256
+PLEASE GIVE UP
+DO ,1 <- #256
+PLEASE FORGET #0
+DO ,1 <- #256
+PLEASE IGNORE #0
+DO ,1 <- #256
+PLEASE RESUME
+DO ,1 <- #256
+PLEASE DESTROY #0
+DO ,1 <- #256
+PLEASE ABSTAIN FROM #1
+DO ,1 <- #256
+PLEASE REMEMBER #1
+DO ,1 <- #256
+PLEASE SACRIFICE #1
+DO ,1 <- #256
+PLEASE REINSTATE #1
+DO ,1 <- #256
+PLEASE RESCIND #1
+DO ,1 <- #256
+PLEASE DIVERT TO #1
+DO ,1 <- #256
+PLEASE RETURN
+
+
+Befunge-93
+"!dlroW ,olleH">:#,_@
+
+
+Malbolge
+(=<`#9]~6ZY32Vxrvz _y3Rd(c4D_bu$i)P9uzOP(_^}gfD3@nb)
+
+
+ETA
+X?>
+X@000000
+X@000046H@0048656c6c6f20576f726c642100
