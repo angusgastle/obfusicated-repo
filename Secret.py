d739dbea93df327e949cffde198e0f44239556ed
Angus Gastle <angusgastle@gmail.com>
2024-07-11 21:30:31 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,5 +1,11 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+++.>+.+++++++..+++.>++.
->>+++++++++++++.------------.>-.>+++++++++++++++.>++++++++.<---.-----
-----.>+.<.<<--.<-.++++++++.-------.--.>>+.>++++.+++++++++++.>>----.
->+.>++++++++.<.<<+.+.---
+
+(LET ((string (NEW-ARRAY (SIMPLE-ARRAY CHARACTER (*)) 0)))
+  (DECLARE (IGNORE (CL:PRIN1 "Hello World")))
+  (PROGN (CLC:ENCAPSULATE-FROM-WORLDS
+    #4A(() (() () () () () () () () () ()) ()
+            ((|BYTES|) NIL (:NO-CODE :MOVABLE :UINT32)))
+   (NLA:CONSTRAINTS NIL))
+ (PROG2 (TERPRI)
+  (CLC:%WARN-IF-FOUND-LABEL)
+  (RETURN-FROM (CLC:"HELLO")
+     43 "(LOCAL-DECL NIL  #())))))
