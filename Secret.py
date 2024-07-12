6555c42d675189a860fe6d862a2f629e379fd855
Angus Gastle <angusgastle@gmail.com>
2024-07-12 03:30:34 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,11 +1,12 @@
-
-(LET ((string (NEW-ARRAY (SIMPLE-ARRAY CHARACTER (*)) 0)))
-  (DECLARE (IGNORE (CL:PRIN1 "Hello World")))
-  (PROGN (CLC:ENCAPSULATE-FROM-WORLDS
-    #4A(() (() () () () () () () () () ()) ()
-            ((|BYTES|) NIL (:NO-CODE :MOVABLE :UINT32)))
-   (NLA:CONSTRAINTS NIL))
- (PROG2 (TERPRI)
-  (CLC:%WARN-IF-FOUND-LABEL)
-  (RETURN-FROM (CLC:"HELLO")
-     43 "(LOCAL-DECL NIL  #())))))
+brainfuck
+>++++++++[<+++++++++>-]<.           [ 'H' - 72 ]
+>++++[<+++++++>-]<+.                [ 'e' - 101 ]
++++++++..                           [ 'l' - 108 ]
++++.                                [ 'o' - 111 ]
+>++++[<+++++++>-]< .                [ ' ' - 32 ]
+>++++++[<+++++++++>-]<-.            [ 'W' - 87 ]
+>+++++++[<+++++++++>-]<--.          [ 'o' - 111 ]
+---------.                          [ 'r' - 114 ]
+>+++++++[<+++++++++>-]<+.           [ 'l' - 108 ]
+>++++[<+++++++>-]<+.                [ 'd' - 100 ]
++++ +++++.                          [ '!' - 33 ]
