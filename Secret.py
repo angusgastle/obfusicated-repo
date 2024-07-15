11c9f5306940fcf652e2d50eea0be424f61a890e
Angus Gastle <angusgastle@gmail.com>
2024-07-15 03:30:34 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,18 +1,2 @@
-APL (A Programming Language)
-
-⍝ APL script to display "Hello World"
-
-⍝ Define a function to convert ASCII values to characters
-ASCIItoChar ← {⎕ucs ⍵}
-
-⍝ Define a function to generate the ASCII values for "Hello World"
-generateASCII ← {72 101 108 108 111 32 87 111 114 108 100}
-
-⍝ Define a function to display the result
-display ← {⎕← ⍵}
-
-⍝ Generate "Hello World" by converting ASCII values to characters
-helloWorld ← ASCIItoChar generateASCII ⍬
-
-⍝ Display "Hello World"
-display helloWorld
+brainfuck
+++++++++++[>+++++++>++++++++++>+++<<<-]>++++.-.>+++++++.<<-.>>>+.<<+.>>---.<+.+.>>-.<<.<--.>>>++.<<-.>>+.<<+.>>>.<<-.>>++.<<.>>-.<<+.>>>.<<-.>>>-.<<<<<<<<.>>>+.<<+.>>>-.<<.>>++.<<.--.--.>>>+.<<+.>>>.<-.
