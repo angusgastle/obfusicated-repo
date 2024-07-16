e04ecd03a4dbffa1b44e533bf10f33a233148f5a
Angus Gastle <angusgastle@gmail.com>
2024-07-15 21:30:36 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,25 +1,54 @@
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.>
 
-# Language: Brainfuck
-# Brainfuck is an esoteric programming language created in 1993 by Urban Müller. It consists of only eight commands: >, <, +, -, ., ,, [, and ].
-# The following code will print "Hello World!" to the screen. The comments will explain each step in detail.
+// Here's a breakdown of the Brainfuck code for "Hello World":
+// Initialize the memory cells
+// Cell [0] set to 10:                   ++++++++++
+// Use loop to set values in subsequent cells
+// Loop starting brace:                  [
+// Increment Cell [1] by 7:              >+++++++
+// Increment Cell [2] by 10:             >++++++++++
+// Increment Cell [3] by 3:              >+++
+// Increment Cell [4] by 1:              >+
+// Move back to Cell [0] and decrement:  <<<<-]
+// End of the loop
+// Now Cell [1] = 70, Cell [2] = 100, Cell [3] = 30, Cell [4] = 10
 
-# Initialize cell 0 to 0
-++++++++++             # Set cell[0] to 10
-
-# Set cell 1 to 100, by multiplying 10 (cell 0) by 10
-[
-    >+++++++>++++++++++>+++>+<<<<- 
-]                         # cell[0]=0, cell[1]=70, cell[2]=100, cell[3]=30, cell[4]= 10
->++.                      # print 'H', cell[1]=72 ('H')
->+.                       # print 'e', cell[2]=101 ('e')
-+++++.                    # cell[2]=108, print 'l'
-.                         # print 'l'
-+++.                      # cell[2]=111 ('l'), print 'o'
->++.                      # cell[3]=33, print space ('!')
-<<+++++++++++++.          # cell[1]=100('W'), print 'W'
->.                        # print 'o'
-+++.                      # print 'r'
-------.                   # print 'l'
----------.                # print 'd'
->+.                       # print '!'
->.                        # print new line
+// Print 'H'
+// Move to Cell [1] and increment by 2:  >++
+// Output 'H':                           .
+// Print 'e'
+// Decrement Cell [1] by 3:              >---
+// Output 'e':                           .
+// Print 'l'
+// Increment Cell [1] by 7:              +++++++
+// Output 'l':                           .
+// Print 'l' again
+// Output 'l':                           .
+// Print 'o'
+// Increment Cell [1] by 3:              +++
+// Output 'o':                           .
+// Print ' ' (space)
+// Move to Cell [3] and decrement by 4:  >>----
+// Output ' ':                           .
+// Print 'W'
+// Increment Cell [3] by 3:              +++
+// Shift to next cell and increment by 6:>++++++
+// Output 'W':                           .
+// Print 'o'
+// Decrement Cell [4] by 1:              -
+// Shift to Cell [3] by moving back by 1:<
+// Output 'o':                           .
+// Print 'r'
+// Decrement Cell [4] by 10:             ----------
+// Shift to start cell and increment by 3:<+++
+// Output 'r':                           .
+// Print 'l'
+// Decrement Cell [1] by 6:              ------
+// Output 'l':                           .
+// Print 'd'
+// Decrement Cell [1] by 8:              --------
+// Output 'd':                           .
+// Print '!'
+// Move to Cell [0] and increment by 1:  >+
+// Output '!':                           .
