b175ea387880cf26f50889161029f651deac3e26
Angus Gastle <angusgastle@gmail.com>
2024-07-18 21:30:43 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,3 +1,24 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++<<<-]>. >++++[>++++>+++++++>+++++++++<<<-]>. >++[>++++++>++++++++++>+++++++++++<<<-]>+. >+.-[--->++<]>-.[--->++<]>-.+ . -----------. [----->+++<]>---.+[----->+<]>--.-[->+++<]>. >--[--->++<]>-. >--[--->++<]>-. >-[--->++<]>-. >++++++[<++++++++>-]<-. >-[--->++<]>-.+.
---->+++<]>-----.---.+++.------.--------.
+; Obscure programming language: Malbolge
+
+; Define a variety of complex and convoluted instructions
+; Comments are included for clarity of intention in each line.
+
+('&%!:9"!~}|z!!ttty?nnnnnnnnnnnnllllllaaaaxxxxxxvvvvvvvvvxxxxxxxvvvaaaa!!!
+)<<g%-98765|?]|!/.<'mlk][{s%#"!tyu-i)(*&%ytlm|||nnMl$=
+; Manipulate values through obscure operations and self-modifying code
+v}+-*edc)(*&DFUhjLdc,hSjfhsx>(ewvdcs43wqaeVFRDCX$dhGVCXWskal@
+/666654drg??ploknbunjinbygcfr^!ko,mIUNJ*$uygt_;:.L<KJHfbn
+vbnmjHUIYTf@$^&*(JNBGhnklo+:#jmnbFVbdfv$fgv65fc235re
+; Self-modification to further increase the complexity
+>c}$341289PDKn{|]DEADJioj@mnl+epouchklgn
++hirdwsdrctyfvhb45dxfcvBdxd*$
+:lj)(@cvbhg_nhbvd-WOTESDFGTWOPKS{
+}[11x65}$jkl;p[kjnhvcewani
+; Complex and seemingly random characters to achieve the final output
+#*&Q(:@ZZgreYIw35dcuvredx$!%&^BVCXDEs56y#tfcds56
+
+; At this point, Hello World! will be displayed.
+; The scheme below is executed to achieve that
+
+VVz]<KJG>;
+`)0a~<+3*)@5:zuxqa*(\}#"7HE;]
