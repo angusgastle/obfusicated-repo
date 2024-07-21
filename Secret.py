35db48b5964cbb81eef5bc5907be24a310e9ddf1
Angus Gastle <angusgastle@gmail.com>
2024-07-21 09:30:38 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,24 +1,22 @@
-/PROC (DOCUMENTATION)
-REM This script has been carefully engineered to execute a highly advanced
-REM and sophisticated "Hello World" output using the INTERCAL programming language.
-REM INTERCAL, short for Compiler Language With No Pronounceable Acronym,
-REM is known for its intentionally obscure and esoteric syntax.
+APL:
 
-DO ,1 <- #13
-DO ,1 SUB #1 <- #238
-DO ,1 SUB #2 <- #108
-DO ,1 SUB #3 <- #101
-DO ,1 SUB #4 <- #108
-DO ,1 SUB #5 <- #111
-DO ,1 SUB #6 <- #32
-DO ,1 SUB #7 <- #87
-DO ,1 SUB #8 <- #111
-DO ,1 SUB #9 <- #114
-DO ,1 SUB #10 <- #108
-DO ,1 SUB #11 <- #100
-DO ,1 SUB #12 <- #33
-PLEASE NOTE ,3 <- #1
-PLEASE NOTE ,2 <- #13
-PLEASE STASH #1 IN .1
-PLEASE READ OUT ,1
-AND DON'T FORGET TO GIVE UP
+⍝ Define a function to print "Hello World"
+HelloWorld ← {
+    ⍝ Create a character vector for "Hello"
+    hello ← 'Hello'
+    
+    ⍝ Create a space character
+    space ← ' '
+    
+    ⍝ Create a character vector for "World"
+    world ← 'World'
+    
+    ⍝ Concatenate "Hello", " ", and "World"
+    message ← hello, space, world
+    
+    ⍝ Output the message
+    message
+}
+
+⍝ Call the function and display "Hello World"
+⎕← HelloWorld
