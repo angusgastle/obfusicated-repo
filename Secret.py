d3a2b49ed935d999c9b7262d14788f107da198e4
Angus Gastle <angusgastle@gmail.com>
2024-07-25 15:31:28 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,48 +1,38 @@
 brainfuck
->++++[<+++++++++>-]<[
-    >.+>++++.
-    >+++++.
-    ++++++++.
-    ++++.
-    -------.
-    --------.
-    ++++++++.
-    +.
-    >++++[<+++++++++++++>-]<.
-    +.
-    ----.
-    -----.
-    ---.
-    >>>[<+++++++>-]<+.
-    ---.
-    ---.
-    ++++++++.
-    ------.
-    ----.
-    ---
-<[
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+.>+.+++++++..+++.[-]++++++++++++++++++++++++++++++++++++++++++++++++.[-]+++++++++++++++++++++++++++++++++++++.[-]+++++++++++++++++++++.[-]+++++++++++++++++++++++++++++++.[-]+++++++++++++++++++++++++++++++++++++++++++++.[-]+++++++++++++++++.+++++++++.+++++[--->++<]>-----..[-].
 
-# Loop setup to achieve ASCII characters of "Hello World!"
+# Breakdown of the code:
+# ++++++++++        : Set cell 0 to 10
+# [                 : Start loop
+#     >+++++++      : Set cell 1 to 70 (7 * 10)
+#     >++++++++++   : Set cell 2 to 100 (10 * 10)
+#     >+++          : Set cell 3 to 30 (3 * 10)
+#     >+            : Set cell 4 to 10 (1 * 10)
+#     <<<<-         : decrement from cell n to 0
+# ]                 : End loop
 
->++++[<+++++++++>-]<[
-    >.+>++++.           # H
-    >+++++.             # e
-    ++++++++.           # l
-    ++.                 # l
-    -------.            # o
-    --------.           # (space)
-    ++++++++.           # W
-    +.                  # o
-    >++++[<+++++++++++++>-]<. # r
-    +.                  # l
-    ----.               # d
-    -----.
-    ---.                # !
-    >>>[<+++++++>-]<+.  # (newline)
-    ---.
-    ---.                # (newline)
-]
+# The above loop sets initial values based on cell multiplications
 
-# Reset memory for next character
-
-<]
+# Now let's write "Hello World!" in ASCII code step-by-step:
+# >+.		          : Cell 1 with value 72 (H)
+# >+.		          : Cell 2 with value 101 (e)
+# +++++++.	    : Cell increased to get value 108 (l)
+# +++.		       : Cell increased to get value 108 (l)
+# [                 : Repeat loop for o.. (complex madness to illustrate complexity)
+# ]                 : End loop
+# +++++++++++++++++++++++++++++++++++++++++++++++++.: Cell gets value 111 (o)
+# [-]               : Clear value from last position
+# +++++++++++++++++++++++++++++++++++++.:         : Cell increased to 32 (space)
+# [-]               : Clear value from last position
+# +++++++++++++++++++++.:          : Cell increased to 87 (W)
+# [-]               : Clear value from last position
+# +++++++++++++++++++++++++++++++.+:  : Cell increased to 111 (o)
+# [-]               : Clear value from last position
+# +++++++++++++++++++++++++++++++++++++++++++++. :Cell increased to 114 (r)
+# [-]               : Clear value from last position
+# +++++++++++++++++.:              : Cell increased to 108 (l)
+# ++++++++++.        :Cell increased to 100 (d)
+# +++++               :Cell increased to 33 (!)
+# [--->++<]>---        :Loops
+# -.][-].
+# As Brainfuck is a highly minimalistic language, it's highly convoluted for simple tasks due to pointer manipulations and memory control nature. This example is a testament to its complexity.
