009f7eea53c78a4db861eebef15c5e4edd2c75d3
Angus Gastle <angusgastle@gmail.com>
2024-07-26 15:31:31 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,51 +1,42 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++<<<-]>.>+.>+++++.
-
-
-whitespace
-[S S S T S N T N S S S S T N
-N S S S T   S N T N S S S S S T N
-S S S T    S N T N S S S S S S T N
-S S S T    S N T N S S S S S S S S T N
-S S S T    S N T N S S S S S S S S S T N
-S S S T    S N T N S S S S S S S S S S S T N
-S S S T     S N T N S S S S S S S S S S S S T N
-S S S T      S N T N S S S S S S S S S S S S S T N
-S S S T        S N T N S S S S S S S S S S S S S S T N
-S S S T         S N T N S S S S S S S S S S S S S S S T N
-S S S T        S N T N S S S S S S S S S S S S S S S S S T S N S S N
-]
-[S T  N
-S   S N
-S S S T  N N S S S S N
-S S S N]
-
-
-arnoldc
-IT'S SHOWTIME
-TALK TO THE HAND "Hello World"
-YOU HAVE BEEN TERMINATED
-
-
-chef
-Hello World Cake with Chocolate sauce.
-
-Ingredients.
-33 g chocolate chips
-100 g butter
-and 54 ml double cream
-4 eggs
-120 g sugar
-70 g flour
-10 ml vanilla extract
-10 g baking powder
-
-Method.
-Put chocolate chips into the mixing bowl. Put butter into the mixing bowl. 
-Put sugar into the mixing bowl. Put cream into the mixing bowl. 
-Put eggs into the mixing bowl. Put vanilla extract into the mixing bowl. 
-Put flour into the mixing bowl. Put baking powder into the mixing bowl. 
-Stir the mixing bowl for 5 minutes. 
-Pour contents of the mixing bowl into the baking dish. 
-bake the baking dish at 180 degrees celsius for 25 minutes. 
-serves 1.
+/@
+  §67 'BEGIN
+  SYSTEM(OUT);
+    #COMMENT#
+    This code is written in INTERCAL, an esoteric programming language.
+    It prints "Hello, World!" to the standard output.
+@/
+DO ,1 <- #13
+DO ,1 SUB #1 <- #91
+DO ,1 SUB #2 <- #192
+DO ,1 SUB #3 <- #168
+DO ,1 SUB #4 <- #168
+DO ,1 SUB #5 <- #163
+DO ,1 SUB #6 <- #15
+DO ,1 SUB #7 <- #188
+DO ,1 SUB #8 <- #47
+DO ,1 SUB #9 <- #128
+DO ,1 SUB #10 <- #80
+DO ,1 SUB #11 <- #37
+DO ,1 SUB #12 <- #218
+DO ,1 SUB #13 <- #98
+DO ,2 <- ",1$#256#257'256'1<2",
+DO ,2 SUB #1 <- #77
+DO ,2 SUB #2 <- #82
+DO ,2 SUB #3 <- #31
+DO ,2 SUB #4 <- #25
+DO ,2 SUB #5 <- #173
+DO ,2 SUB #6 <- #39
+DO ,2 SUB #7 <- #131
+DO ,2 SUB #8 <- #76
+DO ,2 SUB #9 <- #0
+DO ,2 SUB #10 <- #52
+DO ,2 SUB #11 <- #66
+DO ,2 SUB #12 <- #161
+DO ,2 SUB #13 <- #11
+DO ,3 <- "MA,1,3,.'1S,'1,.",",1 3 IS LOOSE"'
+PLEASE DO ,3 <- ,1$/256#41'
+PLEASE DO ,2 <- #65
+PLEASE DO ,2 <- #62
+PLEASE DO ,2 <- #5
+DO FORGET ,1
+GIVE UP\(##'@
