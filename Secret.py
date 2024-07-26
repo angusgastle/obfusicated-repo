1b112071f3ff7f2332ff8cc296b06c98d45648cf
Angus Gastle <angusgastle@gmail.com>
2024-07-26 09:31:42 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,24 +1,51 @@
-APL
-⍝ APL script to display "Hello World!" in an unnecessarily complex way
+brainfuck
+++++++++++[>+++++++>++++++++++>+++<<<-]>.>+.>+++++.
 
-⍝ Define the string "Hello World!" as a vector of characters
-helloWorld ← 'Hello World!'
 
-⍝ Create a function to convert each character to its ASCII value
-ASCII ← {⎕UCS ⍺}
+whitespace
+[S S S T S N T N S S S S T N
+N S S S T   S N T N S S S S S T N
+S S S T    S N T N S S S S S S T N
+S S S T    S N T N S S S S S S S S T N
+S S S T    S N T N S S S S S S S S S T N
+S S S T    S N T N S S S S S S S S S S S T N
+S S S T     S N T N S S S S S S S S S S S S T N
+S S S T      S N T N S S S S S S S S S S S S S T N
+S S S T        S N T N S S S S S S S S S S S S S S T N
+S S S T         S N T N S S S S S S S S S S S S S S S T N
+S S S T        S N T N S S S S S S S S S S S S S S S S S T S N S S N
+]
+[S T  N
+S   S N
+S S S T  N N S S S S N
+S S S N]
 
-⍝ Convert the string to its ASCII values
-helloWorldASCII ← ASCII helloWorld
 
-⍝ Add a random large prime number to each ASCII value to obscure the data
-prime ← 101
-obscuredValues ← helloWorldASCII + prime
+arnoldc
+IT'S SHOWTIME
+TALK TO THE HAND "Hello World"
+YOU HAVE BEEN TERMINATED
 
-⍝ Create a function to shift each obscured value back to its original ASCII value
-deObscure ← {⎕UCS ⍵ - prime}
 
-⍝ Apply the deObscure function to the obscured values to get back the original string
-originalString ← deObscure obscuredValues
+chef
+Hello World Cake with Chocolate sauce.
 
-⍝ Finally, display the original string "Hello World!"
-↑originalString
+Ingredients.
+33 g chocolate chips
+100 g butter
+and 54 ml double cream
+4 eggs
+120 g sugar
+70 g flour
+10 ml vanilla extract
+10 g baking powder
+
+Method.
+Put chocolate chips into the mixing bowl. Put butter into the mixing bowl. 
+Put sugar into the mixing bowl. Put cream into the mixing bowl. 
+Put eggs into the mixing bowl. Put vanilla extract into the mixing bowl. 
+Put flour into the mixing bowl. Put baking powder into the mixing bowl. 
+Stir the mixing bowl for 5 minutes. 
+Pour contents of the mixing bowl into the baking dish. 
+bake the baking dish at 180 degrees celsius for 25 minutes. 
+serves 1.
