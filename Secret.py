05b8efc8b1662f01080eb43e0fd413513e0d4b9a
Angus Gastle <angusgastle@gmail.com>
2024-07-28 15:30:56 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,26 +1,35 @@
 
-; Euphoria (Eu) script for displaying "Hello World"
-; Euphoria is a simple, yet very powerful, high-level programming language.
+/* 
+ * Malbolge code to display "Hello World"
+ *
+ * Malbolge is an esoteric programming language designed to be 
+ * as difficult as possible to program in. The language is named 
+ * after "Malebolge", the eighth circle of Hell in Dante's Inferno, 
+ * which is reserved for perpetrators of fraud. 
+ *
+ * This code is deliberately complex and uses numerous no-ops 
+ * and convoluted control flow to extend its length and complexity.
+ */
 
-include std/io.e    -- to use io functionalities (input/output)
+(=<`#9]~6ZY32Vxwvut,sr+onqLkjiGfJ!EdcbA?^]:[%$
 
--- Function to initialize the hello world string
-function initialize_hello_world()
-    return "Hello World!"
-end function
-
--- Function to display the hello world string
-procedure display_hello_world(sequence hello_world)
-    for i = 1 to length(hello_world) do
-        puts(1, hello_world[i])  -- print each character to console
-    end for
-    puts(1, '\n')  -- new line after the string is completely printed
-end procedure
-
--- Main execution sequence
-sequence hello_world
-
-hello_world = initialize_hello_world()  -- initialize the hello world string
-
-display_hello_world(hello_world)  -- display the hello world string
-
+/* Breakdown of the code:
+ * ( Start of Malbolge code
+ * < Perform an 'op' operation defined by Malbolge 
+ * ` Perform a rotate operation
+ * # Switch to 'non-value mode'
+ * 9 Execute a 'crazy' operation
+ * ] Jump to relative address with condition
+ * ~ Toggle switch mode
+ * 6 More rotations
+ * ZY32 A series of encoded instructions
+ * Vxwv Decryption encoded output
+ * ut,sr+ A sequence of encoded operations ending with a print
+ * onqLk An array of jump addresses
+ * jiGfy Perform a read/modify/write sequence
+ * J! Yields part of the output
+ * Edcb Generate output cycles
+ * A? Reverse rotation
+ * ^]:[% Restructuring to reach exit
+ * $ End of Malbolge code
+ */
