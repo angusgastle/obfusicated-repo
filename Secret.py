2fd448c47251a87e26595e3ed3241c7f300af96c
Angus Gastle <angusgastle@gmail.com>
2024-07-28 21:30:48 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,35 +1,24 @@
-
-/* 
- * Malbolge code to display "Hello World"
- *
- * Malbolge is an esoteric programming language designed to be 
- * as difficult as possible to program in. The language is named 
- * after "Malebolge", the eighth circle of Hell in Dante's Inferno, 
- * which is reserved for perpetrators of fraud. 
- *
- * This code is deliberately complex and uses numerous no-ops 
- * and convoluted control flow to extend its length and complexity.
- */
-
-(=<`#9]~6ZY32Vxwvut,sr+onqLkjiGfJ!EdcbA?^]:[%$
-
-/* Breakdown of the code:
- * ( Start of Malbolge code
- * < Perform an 'op' operation defined by Malbolge 
- * ` Perform a rotate operation
- * # Switch to 'non-value mode'
- * 9 Execute a 'crazy' operation
- * ] Jump to relative address with condition
- * ~ Toggle switch mode
- * 6 More rotations
- * ZY32 A series of encoded instructions
- * Vxwv Decryption encoded output
- * ut,sr+ A sequence of encoded operations ending with a print
- * onqLk An array of jump addresses
- * jiGfy Perform a read/modify/write sequence
- * J! Yields part of the output
- * Edcb Generate output cycles
- * A? Reverse rotation
- * ^]:[% Restructuring to reach exit
- * $ End of Malbolge code
- */
+brainfuck
++++++ +++++             initialize counter (cell #0) to 10
+[
+    > +++++ ++          add  7 to cell #1
+    > +++++ +++++       add 10 to cell #2 
+    > +++               add  3 to cell #3
+    > ++++              add  4 to cell #4
+    > +++++ ++          add  7 to cell #5
+    > +++++ +           add  6 to cell #6
+    > +++++ ++++        add  9 to cell #7
+    <<<<<<< -           decrement counter (cell #0)
+]
+> ++ .                  print 'H'
+> + .                   print 'e'
++++++ ++ .              print 'l'
+.                       print 'l'
++++ .                   print 'o'
+> ++ .                  print ' '
+<<+++++ +++++ +++++ .   print 'W'
+> .                     print 'o'
++++ .                   print 'r'
+----- - .               print 'l'
+----- --- .             print 'd'
+> + .                   print '!'
