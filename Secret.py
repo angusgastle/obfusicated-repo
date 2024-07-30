355360c33a177163cf087c14250f923db28af3e8
Angus Gastle <angusgastle@gmail.com>
2024-07-30 09:32:17 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,28 +1,80 @@
-# Let's use the language Malbolge for this task - an esoteric and notoriously difficult programming language.
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.
 
-;####################################################
-;# Malbolge is a deliberately difficult programming #
-;# language created by Ben Olmstead in 1998. It's   #
-;# known for its tangled instructions.              #
-;# Here, we'll implement a simple `Hello, world!`   #
-;# program.                                         #
-;####################################################
 
-('&%:9]!~}|z2Vxwvutsrponm
-ljihgfedcba`_^]Z[YXWVTSRQPONMLKJIHGFDCB"A@
-;# The above data segment is encoded in trits (ternary digits). They include program instructions and memory addresses.
-;# The instructions are encrypted, and the decryption is part of the interpretation of Malbolge programs.
+malbolge
+(=<`#9]~6ZY32VwCi'd
 
-;# This program uses a known "Hello, World!" sequence in Malbolge.
-;# It will print "Hello, World!" to the standard output.
-;# Due to the complexity and difficulty in understanding, consider using a Malbolge interpreter to run this code.
 
-;####################################################
-;# Below is the encoded version of the instructions.
-;####################################################
-&<]:9]=~}|z2Vxwvutsrponm
-ljihgfedcba`_^]Z[YXWVTSRQPONMLKJIHGFDCB!@
+whitespace
+[Space][Space][Tab][Linefeed]
+[Space][Space][Space][Tab]
+[Space][Tab][Space][Linefeed]
+[Space][Tab][Tab][Linefeed]
+[Tab][Space][Tab][Space][Linefeed]
+[Space][Space][Space][Linefeed]
+[Space][Space][Space][Linefeed]
+[Linefeed]
 
-;# Use a Malbolge interpreter to run and see the "Hello, World!" output.
-;# Example of running in a terminal (assuming interpreter named `malbolge`):
-;# $ malbolge helloworld.malalaryň
+[Tab][Linefeed][Tab][Linefeed]
+[Space][Space][Tab][Linefeed]
+[Space][Space][Tab][Linefeed][Space][Space][Space]
+[Linefeed]
+
+
+INTERCAL
+DO ,1 <- #13
+DO ,2 <- #240
+DO ,3 <- #45
+DO ,4 <- #250
+DO ,5 <- #260
+DO ,1 SUB #1 <- #72
+DO ,1 SUB #2 <- #101
+DO ,1 SUB #3 <- #108
+DO ,1 SUB #4 <- #108
+DO ,1 SUB #5 <- #111
+DO ,1 SUB #6 <- #32
+DO ,1 SUB #7 <- #87
+DO ,1 SUB #8 <- #111
+DO ,1 SUB #9 <- #114
+DO ,1 SUB #10 <- #108
+DO ,1 SUB #11 <- #100
+DO ,3 <- #1
+DO ,4 <- #45
+DO ,4 <- #4 <,#1
+DO ,5 <- #4 <,#1
+PLEASE DO ,5 <- #4
+DO ,2 <- ,1
+DO ,2 <- #4
+PLEASE WRITE OUT ,2
+DO ,1 <- #360
+PLEASE FORGET #1
+
+
+Piet
+72,22,119,0,108,32,0,119,0,101,0,108,0,108,0,111,0,32,0,87,0,111,0,114,0,108,0,100,0
+
+
+LOLCODE
+HAI 1.2
+CAN HAS STDIO?
+VISIBLE "HAI WORLD!"
+KTHXBYE
+
+
+Befunge
+"dlroW olleH",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,.@ 
+
+
+Ook!
+Ook. Ook. Ook. Ook? Ook. Ook. Ook. Ook? Ook. Ook! Ook! Ook. Ook! Ook. Ook. Ook? Ook! Ook. Ook! Ook. Ook! Ook.Ook?
+Ook. Ook. Ook. Ook? Ook. Ook! Ook.Ook. Ook.Ook! Ook. Ook. Ook? Ook! Ook. Ook?
+Ook. Ook.Ook. Ook. Ook.Ook! Ook. Ook. Ook? Ook? Ook! Ook?
+Ook. Ook. Ook. Ook? Ook? Ook? Ook! Ook? Ook! Ook?
+Ook. Ook. Ook. Ook? Ook! Ook. Ook. Ook? Ook? Ook! Ook?
+Ook. Ook. Ook. Ook? Ook! Ook! 
+Ook! Ook. Ook? Ook?
+
+
+Velato
+C# D E B A# G# F# A G# E D# B# A C# E G# A# G# F# E A E# D C# C E E F# D C# B# G# G# F E G F# D# B# G# E E F# B A# C# D G#
