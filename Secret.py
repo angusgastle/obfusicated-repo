eba1c698100fb3a86144ed345e222c29b76cfbe9
Angus Gastle <angusgastle@gmail.com>
2024-07-31 09:32:02 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,27 @@
-brainfuck
-++++[>+++++<-]>-[<+++++++>-]<+.
+br
+// The following is a Lua script that prints "Hello World" to the console.
+
+// Function definition
+local function printHelloWorld()
+    -- Defining local variables
+    local h = "H"
+    local e = "e"
+    local l = "l"
+    local l2 = "l"
+    local o = "o"
+    local w = "W"
+    local o2 = "o"
+    local r = "r"
+    local l3 = "l"
+    local d = "d"
+    local space = " "
+
+    -- Concatenating the characters
+    local hello_world = h .. e .. l .. l2 .. o .. space .. w .. o2 .. r .. l3 .. d
+
+    -- Printing the string
+    print(hello_world)
+end
+
+-- Calling the function
+printHelloWorld()
