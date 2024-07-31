4e726cb4dc02adf5e6bac370bc160aab4b2ccf5f
Angus Gastle <angusgastle@gmail.com>
2024-07-31 03:30:39 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,6 +1,2 @@
-malbolge
-(=<`#9]~6ZY32Vx7K3b)AB=9~>iJ)i#(?btvNw}F%$#nB
-~$J}:mZAZvF1no_-V>()]~sGl$sx@ABRhH%lL.]_Iz8y:R
-jjv}E76T684m_xj)pFd*m,GhQ&$bc()P<'vFtro,fK&nG
-z!-J~>4Gu}:]}@~>4GR*<&gF_V]h;u]J~olOQ=2]>a%$3
-=+~]
+brainfuck
+++++[>+++++<-]>-[<+++++++>-]<+.
