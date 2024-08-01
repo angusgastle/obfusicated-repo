c01cceffdad1623d0c2724c538d787ad4f22b13a
Angus Gastle <angusgastle@gmail.com>
2024-08-01 08:03:00 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,3 +1,14 @@
 brainfuck
-+++++++++++++[->++>>>+++++>++>+<<<<<<]>>>>--->+>-->>>>>>+[[-]+]++++>----->+++[->++<]>+.+++.------.<----.----->+.>-[>+++<[-]]>.<<+.>++.<<-------.>>+.<++.+++.------.<++.----.<+.+++++.---.<---
-++++++++++[->+++<]>--.+++++.-------.
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>+++++++.<------.<---.>>>+.<<+.++++.
+
+
+3DeadMonkey
+(START)
+    PRINT "Hello World"
+(EXIT)
+
+
+Piet
+Piet programs use images rather than text. Below is an example of a Piet program for "Hello World":
+
+(Piet image - a link to an image would be used here as it cannot be represented in plain text.)
