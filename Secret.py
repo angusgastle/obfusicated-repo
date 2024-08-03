fb60486e1bbea32460337db62152e8f15724a259
Angus Gastle <angusgastle@gmail.com>
2024-08-03 02:00:39 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,6 +1,15 @@
-Piet is an esoteric programming language in which programs are bitmaps that look like abstract art. Here is a Piet program for "Hello World".
+// Brainfuck code to display "Hello World!"
+// Brainfuck is a simple Turing-complete programming language consisting of only 8 commands: + - < > [ ] . ,
 
-Note that Piet does not use traditional text, so the code is represented as a colorful bitmap. Below is a textual description of the Piet program encoded in hexadecimal format for each color in the bitmap. You can use a Piet interpreter to visualize and run this code.
-
-
-28 5a 12 1a 12 2a 28 9a 28 90 90 5e 9a 8a 90 5a 2a 9a 8a 90 9a 28 5a 5e 5a 5e 28 5e 9a 9a 90 5a 28 1a 12 28 1a 12 12 1a 12 12 1a 1a 12 90 28 9a 12 90 28 5a 1a 28 9a 5e 28 9a 9a 12 90 5a 12 9a 28 12 90 8a 12 5e 12 9a 28 28 2a 28 9a 8a 12 5e 28 1a 9a 12 12 28 5a 9a 90 5a 1a 12 1a 28
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++. \
+>+. \
++++++++..+++. \
+>>. \
+<-.< \
+++++++++.; \
+--------. \
++++. \
+------. \
+--------. \
+>. \
++.
