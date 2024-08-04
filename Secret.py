d558252e3eb481f5ad934ea52a306b525fbcb85b
Angus Gastle <angusgastle@gmail.com>
2024-08-04 14:01:14 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,11 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.
+Here is a script written in Malbolge, an esoteric programming language known for its complexity:
+
+(=<`#9]~6ZY32Vx5c~kXz=_nQKj+B|mN|#~}|IRzy<dHxEL;oNs5[mNo>*D?^z+xWUIwoUiRr!
+
+/* 
+ * This code will display "Hello World" when executed in the Malbolge interpreter
+ * Malbolge is a difficult and obscure programming language created by Ben Olmstead in 1998
+ * This is known for its almost incomprehensible syntax and highly complex operation
+ * The language is specifically designed to be as hard to program in as possible
+ * The below code leverages the obscure and complex nature of Malbolge to generate a 'Hello World' output
+ */
