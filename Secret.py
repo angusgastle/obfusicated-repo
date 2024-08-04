7a56552c435b89a3cec61928aaf1b583d011d3af
Angus Gastle <angusgastle@gmail.com>
2024-08-03 20:00:58 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,16 +1,50 @@
 brainfuck
->+[--->++<]>+.             (Initialize memory cell at index 1 with 8)
-+++[->+++<]>+.             (Initialize memory cell at index 1 with 20)
-+++++++..+++.              (Output 'H')
->>>++++++++[->+++<]>       (Initialize memory cell at index 4 with 24)
-++++++++.                  (Output 'e')
----.                       (Output 'l')
----------.                 (Output 'l')
-+++++++++++++.             (Output 'o')
->>>++++++++[->++++<]>      (Move to index 4 and initialize memory cell with 32)
---.                        (Output ' ')
-------------.              (Output 'W')
------.                     (Output 'o')
-+++++++.                   (Output 'r')
------------.               (Output 'l')
-++++++.                    (Output 'd')
+++++++++++[>+++++++>++++++++++>+++<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>>>+.>.++.
+
+
+malbolge
+(=<`#9]9<;;;{!#=_@!>6YXaKzq_xZz2VOGz5J_y2<&WZE5]4K|ab}$~/=s*Px>$XEqL4V8P9mn9nD.DnP|}Otegz718~aRMYg{1+6D^
+Kmfj!.0fL9hiTC]!..+/2Q><V=H^*YREJ>BD#y9N$Ls}8D<>mzUwsHBzRf;qDLvytt3Nki2}7xt!oTCCq6B!}+1*|kWpQ9cRh2v$4z
+
+
+intercal
+DO ,1 <- #72
+DO ,1 SUB #1 <- #32
+DO ,1 SUB #2 <- #72
+DO ,1 SUB #3 <- #101
+DO ,1 SUB #4 <- #108
+DO ,1 SUB #5 <- #108
+DO ,1 SUB #6 <- #111
+DO ,1 SUB #7 <- #32
+DO ,1 SUB #8 <- #87
+DO ,1 SUB #9 <- #111
+DO ,1 SUB #10 <- #114
+DO ,1 SUB #11 <- #108
+DO ,1 SUB #12 <- #100
+DO ,1 SUB #13 <- #33
+DO ,1 SUB #14 <- #32
+DO ,1 SUB #15 <- #64
+PLEASE DO ,1 <- #1
+DO ,1 SUB #16 <- #10
+DO ,1 SUB #17 <- #13
+DO ,1 SUB #18 <- #0
+PLEASE WRITE OUT ,1
+
+
+whitespace
+[S S S T S S S S S S T S N
+T T T   S S S T S N   N
+S S S T S S T N   T N
+S S S T S S N T N
+S S S T S S T S N   T S
+S S S T S S T T N   T S
+S S S T S S T S N   T N
+S S S T S S T S S T   T S
+S S S T S S N T S S T   T S
+S S S T S T S N T S
+S S S T S S N T T N   T T
+S S S T S S T S N   T N
+S S S T S S T S N
+S S S T S S T S S T   T S
+S S S T S S T S T N   T T
+]
