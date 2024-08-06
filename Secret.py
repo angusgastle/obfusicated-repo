bf647c119c97edec0c813d8c926651263efcf520
Angus Gastle <angusgastle@gmail.com>
2024-08-06 02:02:37 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,19 +1,67 @@
-; Malbolge: An example of confusing code for "Hello, World!"
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>++++.
 
-(=<`#9]~6ZY32Vx5f;gKzNPOe7W4z3V-M}5J@D!-#xA3s*{|pFom3V`fK$mche;q_@TVKz~>jLQd]t+K<+
 
-; Code begins here
-; '(<' is a representation of 3 instructions: rotr, move right, and crazy 
-; each of these lines rotate the memory 9 (0-based) spaces 
-; #9]~6ZY32Vx5 -> self-modifies to make sense to Malbolge interpreter
+whitespace
+[S][S][S][S][S][S][S][S][S][L][S][S][L][T][L][S][T][S][S][T][S][T][L][S][T][S][S][S][S][T][L][S][T][T][L][S][T][L][S][S][T][L][L][L][T][T][L][S][T][S][L][S][S][S][L][S][T][S][S][L][L][S][S][S][S][L][L][T][S][T][S][L][
+S][T][S][S][S][S][S][L][S][S][L][S][S][S][S][S][S][S][S][L][T][S][S][S][S][S][S][S][L][L][L][S][S][S][S][S][S][S][S][S][L][S][S][S][S][S][S][S][S][S][L][S][S][S][S][S][S][S][S][S][L][L][S][S][S][S][S][S][S][L][L][L][
+S][S][S][S][L][L][L][S][S][S][S][S][S][S][S][L][L][S][S][S][S][S][S][S][S][L][S][S][S][S][S][S][S][S][L][L][S][S][S][S][S][S][L][S][S][S][S][S][S][S][L][S][S][S][S][S][S][S][S][S][S][L][T][S][S][T][S][S][T][L][S][
+T][T][L][S][T][S][S][L][S][S][S][S][S][S][S][L][S][S][S][S][S][S][S][S][S][S][S][L][S][S][S][S][S][L][S][L][S][S][S][S][S][S][S][L][S][S][S][S][S][S][S][S][S][S][S][S][L][S][S][S][S][S][L][S][L][S][S][S][S][S][S][
+S][S][S][L][T][L][S][T][T][L][S][T][S][S][T][S][L][S][S][S][S][S][S][S][L][L][L][T][T][L][S][S][S][S][S][S][S][S][S][S][S][S][S][S][L][L][S][S][S][S][S][S][S][S][S][S][S][S][S][S][S][S][S][S][L][L][L][T][T][L][T][
+S][S][S][S][S][S][S][S][S][S][S][S][S][S][S][L][L][L][T][L][S][S][S][S][S][S][S][S][S][S][S][S][S][L][S][S][S][S][S][S][S][S][S][S][S][S][S][S][S][S][S][L][L][L][T][T][L][L][S][S][S][S][S][S][S][S][S][S][L][T][L][
+S][L][L][S][S][S][S][S][S][S][S][S][S][L][T][T]
 
-=<`#9]~6ZY32Vx5f;gKzN -> complex operations, applying transformations 
 
-; Following are series of transformations leading to character values print out
-POe7W4z3V-M}5J@D!- -> deeply obfuscated steps to construct output 'Hello'
+INTERCAL
+DO ,1 <- #13
+PLEASE DO ,1 SUB #1 <- #238
+DO ,1 SUB #2 <- #108
+DO ,1 SUB #3 <- #118
+DO ,1 SUB #4 <- #114
+DO ,1 SUB #5 <- #31
+DO ,1 SUB #6 <- #109
+DO ,1 SUB #7 <- #227
+DO ,1 SUB #8 <- #108
+DO ,1 SUB #9 <- #225
+DO ,1 SUB #10 <- #114
+DO ,1 SUB #11 <- #40
+DO ,1 SUB #12 <- #0
+DO ,1 SUB #13 <- #32
+PLEASE DO ,2 <- #1
 
-; Final segment to complete the message
-#xA3s*{|pFom3V`fK$mche;q_@TVKz~>jLQd]t+K<+
+(1) DO .1 <- #87
+    DO .2 <- #111
+    DO .3 <- #114
+    DO .4 <- #108, DECREMENT .4 VALUE 2
+    DO .5 <- #100
+    DO .6 <- #0
+    DO .7 <- #72
+    DO .8 <- #101
+    DO .9 <- #108
+    DO .10 <- #108
+    DO .11 <- #111, DECREMENT .11 VALUE 4
 
-; The string “Hello World” is ultimately printed by 
-; interpreting above values through Malbolge's deterministic chaos system
+PLEASE DO ,3 <- ,1 SUB #7
+PLEASE DO ,3 SUB #0 <- ,1 SUB #2
+PLEASE DO ,3 SUB #1 <- ,1 SUB #4
+PLEASE DO ,3 SUB #2 <- ,1 SUB #3
+PLEASE DO ,3 SUB #3 <- ,1 SUB #5
+PLEASE DO ,3 SUB #4 <- ,1 SUB #8
+PLEASE DO ,3 SUB #5 <- ,1 SUB #13
+
+PLEASE DO ,4 <- #2
+
+DO COME FROM (1)
+
+DO (1) NEXT
+
+DO RESUME .7 <- #4
+DO RESUME .8 <- #7
+DO RESUME .9 <- #3
+DO RESUME .10 <- #1
+DO RESUME .11 <- #4
+
+PLEASE DO ,1 SUB #6 <- #32
+PLEASE DO ,1 SUB #10 <- #32
+
+PLEASE NEXT
