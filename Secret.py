54da592f2a6f4270d183d7c52db1f0819fed8b2f
Angus Gastle <angusgastle@gmail.com>
2024-08-06 14:02:46 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,25 +1,2 @@
-befunge
-> v
-v  "Hello, World!"<   
-> >48*,          v
-v,       v     
->48*40p>00g00p:
-            ^___^
- @
-# Starting point of the program. 
-
-# Push the ASCII values of each character onto the stack
-"eployeeP expty *neolet!*dWlrll,oeH" v
-                                      <
-# Output characters in reverse order
-^ ,<
-  
-;v
->?49m   # Loop to check stack 
-
-$>48*   # Print character
-
-# End of loop
-< ,@
-
-# End program execution
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+++.>+.+++++++..+++.>++.<<+++++++++++++++.>++++.<<+.>>-.<++++++++.--------.+++.------.--------.>+.
