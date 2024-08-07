c0a108ff3fc91b08b8c40860c4089de9f934c064
Angus Gastle <angusgastle@gmail.com>
2024-08-07 14:02:47 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,43 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++.>.+++.------.--------.>+.>.
+
+/*
+ * INTERCAL: "Hello World" program 
+ * INTERCAL is a parody programming language designed to be as different from other languages as possible.
+ * This program outputs "HELLO WORLD" to the console.
+ */
+
+DO ,1 <- #13
+DO ,2 <- #1
+DO ,3 <- #256
+PLEASE DO ,1 <- #57
+PLEASE DO ,4 <- #12
+PLEASE DO ,5 <- #0
+PLEASE DO ,4 SUB #6 <- #112
+PLEASE DO ,5 SUB #5 <- #114
+PLEASE DO ,4 SUB #5 <- #107
+PLEASE DO ,5 SUB #4 <- #100
+PLEASE DO ,4 SUB #4 <- #117
+PLEASE DO ,5 SUB #3 <- #33
+PLEASE DO ,3 <- #0
+PLEASE DO ,1 <- #1
+PLEASE DO ,1 <- #4
+(PLEASE READ OUT ,1!)
+DO ,5 <- #2
+READ OUT ,1 PLEASE
+DO ,5 <- #1 PLEASE
+DO ,1 <- #6 PLEASE
+DO ,1 <- #3 PLEASE ,1 SUB #3 <- #64
+DO ,1 <- #1 PLEASE
+DO ,1 <- #2 PLEASE
+DO ,1 <- #7 PLEASE READ OUT ,1 PLEASE
+DO ,1 <- #8 PLEASE
+DO ,1 <- #5 PLEASE
+DO ,1 <- #7 PLEASE
+DO ,1 <- #2 PLEASE
+DO ,1 <- #5 PLEASE
+DO ,1 <- #1 PLEASE
+DO ,1 <- #4 PLEASE
+DO ,1 <- #6 PLEASE
+DO ,1 <- #3 PLEASE
+ONCE AGAIN READ OUT ,1 PLEASE
+FORGET #3
+PLEASE GIVE UP
