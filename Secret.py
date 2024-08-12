819d1d21efd5407d5c96b9819a38837037c9d180
Angus Gastle <angusgastle@gmail.com>
2024-08-11 20:01:10 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,159 +1,29 @@
-INTERCAL:
+# Befunge-93 script for displaying "Hello World"
 
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
+>25*"!dlroW olleH":v
+                     |:
+                     >,@
+     ^    _v
+            "<"v
+        ^`" :"_1 v
+           ^"0v
+              ^^^^
 
-PLEASE DO :1 <- #13
-PLEASE DO :2 <- #169
+# Explanation of the code:
+# - Befunge-93 is a two-dimensional esolang, meaning the instruction pointer can move in multiple directions.
+# - `>` starts moving the instruction pointer to the right.
+# - `25*"` pushes 25 to the stack, which will later be used.
+# - `!dlroW olleH` pushes the ASCII values of "Hello World!" to the stack in reverse order.
+# - `:` duplicates the top item of the stack.
+# - `v` changes the direction of the instruction pointer to downwards.
+# - `|` is a vertical conditional statement.
+# - If the top of the stack is 0, it will change the instruction pointer’s direction to down.
+# - If the top of the stack is non-0, it will change the instruction pointer’s direction to up.
+# - `,@` terminates the program and pops the top value to print as ASCII.
+# - `>_v` is a direct path-instruction pointer downwards.
+# - `"<"v` is an instruction to reverse the direction to upwards by "less than" content.
+# - The space along with `"` is a reverse hack for non-real go-up paths.
+# - `^` control character moves the pointer further upward.
 
-PLEASE GIVE UP
-PLEASE FORGET #13
-PLEASE FORGET #169
-
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-PLEASE DO :3 <- #91
-PLEASE DO :4 <- #94
-
-PLEASE FORGET #91
-PLEASE FORGET #94
-
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-PLEASE DO :5 <- #139
-
-PLEASE FORGET #139
-
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-PLEASE DO :6 <- #196
-
-PLEASE FORGET #196
-
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-PLEASE DO :7 <- #155
-PLEASE DO :8 <- #225
-PLEASE DO :9 <- #156
-PLEASE DO :10 <- #151
-PLEASE DO :11 <- #162
-PLEASE DO :12 <- #225
-
-PLEASE FORGET #155
-PLEASE FORGET #225
-PLEASE FORGET #156
-PLEASE FORGET #151
-PLEASE FORGET #162
-PLEASE FORGET #225
-
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-PLEASE DO :13 <- #193
-PLEASE DO :14 <- #225
-PLEASE DO :15 <- #169
-
-PLEASE FORGET #193
-PLEASE FORGET #225
-PLEASE FORGET #169
-
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-
-PLEASE NOTE THAT :1 SUB #15 IS 12
-PLEASE	NOTE THAT :2 SUB #15 IS 21
-PLEASE	NOTE THAT :3 SUB #15 IS 8
-PLEASE	NOTE THAT :4 SUB #15 IS 5
-PLEASE	NOTE THAT :5 SUB #15 IS 12
-PLEASE	NOTE THAT :6 SUB #15 IS 15
-PLEASE NOTE THAT :7 SUB #15 IS 32
-PLEASE	NOTE THAT :8 SUB #15 IS 23
-PLEASE	NOTE THAT :9 SUB #15 IS 15
-PLEASE	NOTE THAT :10 SUB #15 IS 18
-PLEASE	NOTE THAT :11 SUB #15 IS 12
-PLEASE	NOTE THAT :12 SUB #15 IS 4
-
-PLEASE FORGET #15
-
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-
-PLEASE WRITE IN :1 SUB #12, #32
-PLEASE WRITE IN :2 SUB #21, #72
-PLEASE WRITE IN :3 SUB #8, #101
-PLEASE WRITE IN :4 SUB #5, #108
-PLEASE WRITE IN :5 SUB #12, #108
-PLEASE WRITE IN :6 SUB #15, #111
-PLEASE WRITE IN :7 SUB #32, #32
-PLEASE WRITE IN :8 SUB #23, #87
-PLEASE WRITE IN :9 SUB #15, #111
-PLEASE WRITE IN :10 SUB #18, #114
-PLEASE WRITE IN :11 SUB #12, #108
-PLEASE WRITE IN :12 SUB #4, #100
-
-PLEASE FORGET #32
-PLEASE FORGET #72
-PLEASE FORGET #101
-PLEASE FORGET #108
-PLEASE FORGET #108
-PLEASE FORGET #111
-PLEASE FORGET #32
-PLEASE FORGET #87
-PLEASE FORGET #111
-PLEASE FORGET #114
-PLEASE FORGET #108
-PLEASE FORGET #100
-
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-
-PLEASE DO READ OUT :1
-PLEASE DO READ OUT :2
-PLEASE DO READ OUT :3
-PLEASE DO READ OUT :4
-PLEASE DO READ OUT :5
-PLEASE DO READ OUT :6
-PLEASE DO READ OUT :7
-PLEASE DO READ OUT :8
-PLEASE DO READ OUT :9
-PLEASE DO READ OUT :10
-PLEASE DO READ OUT :11
-PLEASE DO READ OUT :12
-
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
-DO (1) NEXT
+# The script continues to manipulate the instruction pointer through a series of intricate motions,
+# finally causing the output process to occur in such a manner that results in proper print of "Hello World".
