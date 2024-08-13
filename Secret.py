1d70ba1ab594193d2070cf3add5a4fc23f9be792
Angus Gastle <angusgastle@gmail.com>
2024-08-13 14:02:45 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,16 +1,12 @@
-
-# Malbolge
-# Malbolge is an esoteric programming language known for its complexity and difficulty to decipher.
-# The following code will display "Hello, World!" when executed. Given the nature of Malbolge,
-# the specific operations and instructions are highly obfuscated and not easy to follow. Enjoy!
-
-('&%:9]!~}|z2Vxwv-,POqponlLjJgGDa|[XWvT4RdRSq?pOnZYx*VvJxYvTjvOk_vQpMjGRrBpJ$6dVbS9VrD?-4POC$nMLk
-eVu|W9tMkU!ew[gV3tM]8zN)ewB8}~Bo_t_)EkM+ASGp}N;ODCmV)bIKT:M-1n:GO%m'9Ls0Bl|AJKHb%@i0Zs(kPU9cYO)&o
-Urc8Vh^:#wvY@Qa[qDnJ:MkNadgV9bKJ]Hg)t/z+<cn+AxQ&lU}N&<2uGsW|XMN:7.V)cMKk)cwGu<dpbc:)ZQeQsofNV@vPj
-lYT)KsJ~=&Hn`VKr]DvedQhbZQH|Y6PdU7g:MgIRONMENT, PRIMARY_LOCALIZATION_Ye9DTVXW_vTjvOnZYx*VvJxYvTdvj
-x_wVnZYu*vVvJxYvTnZYu*vVvdwPtx*wPPtx*wQNjw*xwDNJvH19`XzX^+kL`!A_J|Jw*JvUwvJ::J}ZwWXYQB+J=.J/.uPv:J
-$xpuXU+jv[|_XXvPu9.uXPU)jv]9.vK*!6,#d]=>\$6=aJxuJYO0zX$:;JAm!~Yv;}`uJxXz$!`u0X$:P+uB;~v^x:;uXy#{%
-$YTJBg9!:ZZ_!E?pWpv_F-Wa6#4DJK_<+rb*[JJcP4L$v!OYJ>ChGrpIe$eM_YxJpORPVjI_>'=!6,*!6a$:eCRB[KNpPYIzRz
-zBNv]~!!^=zVqW:}=~{=3JJef9<=EZZ&(H3Be`vTbbcYaP"eWWpbXWvu_uJ|Ye~JxwLu:u)cu:X]:uErJxwNu:x0vW{JrkZUw
-$@wvHW@}uwU$m@)Qu[BKZzxZcX~u$X<$Au.rV#{uVwTJuJruUwxJx5wPR6M/ywRu-`e3XP_~yV6vQMsEWxWc7b/dq_u+PB>Gr
-pEfp41BVJ[MxIVkvM?dDMb2w|A~/`W+)PDR6-)OwKpBIU)ErBQzSQz$BK`zTv_eCJ#OJvx_WuJ1W?lZ7EYOgWYVC_YW)_'QJC
+brainfuck
+>++++++++[<+++++++++>-]<. # Print 'H'
+>++++[<+++++++>-]<+. # Print 'e'
++++++++..+++. # Print 'llo'
+>++++[<+++++++>-]<+. # Print 'o'
+>>>++++[<+++++++++++++>-]<-. # Print ' '
+>++++++[<+++++++++>-]<. # Print 'W'
+>++++[<+++++++>-]<++. # Print 'o'
+>+++++++[<+++++++>-]<---. # Print 'r'
+>++++[<+++++++++++>-]<. # Print 'l'
+>++++[<+++++++>-]<+. # Print 'd'
+>++. # Print '!'
