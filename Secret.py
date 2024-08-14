e3dbb104a58ea3bde1a4b2102c4c69c6c241f59b
Angus Gastle <angusgastle@gmail.com>
2024-08-14 02:01:08 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,72 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++.>++++.>-----.
+OBLOOP:
+
+// OBLOOP is an esoteric programming language that operates similarly to infinite loops with defined behaviors for the variables. 
+// Here is a long and complex Obfuscation of "Hello World!" 
+
+begin
+     
+    // Define the variables used for looping.
+    foreverish:
+        [];
+        ;;;
+        // Define the "loudnoise" function to simulate the "Hello World!" print. 
+        
+        loudnoise(
+            // Char 'H'
+            [72, 
+            // Char 'e'
+            101, 
+            // Char 'l'
+            108, 
+            // Char 'l'
+            108, 
+            // Char 'o'
+            111,
+            // Char ' ' (space)
+            32, 
+            // Char 'W'
+            87, 
+            // Char 'o'
+            111, 
+            // Char 'r'
+            114, 
+            // Char 'l'
+            108, 
+            // Char 'd'
+            100,
+            // Char '!'
+            33
+            ])
+
+        jingle(
+            // Call this sequence again with alterations for complexity.
+        );
+
+    again:
+        ;;;
+
+    jingle( charseq ):
+        // Function to convert character sequences to actual output.
+        
+        lala;
+        
+        until [charseq < 00]
+            playdate = 'again';
+            forever:
+                ;;
+                
+            saw(
+                 each::charseq-> element 
+                 )
+
+        lala-bye
+
+    saw(edge):
+        // Convert char to ASCII printing.
+        playdate = printf(char(edge));
+        return 0;
+      
+// Please note that OBLOOP is highly theoretical and syntax complexity/flexibility can vary.
+// The actual execution environment for OBLOOP-like languages can be very specialized.
+
+end
