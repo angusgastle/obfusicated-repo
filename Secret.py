f8292953394ea567bd3db8b1fdb98a9d9890f204
Angus Gastle <angusgastle@gmail.com>
2024-08-15 20:01:31 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,49 +1,34 @@
-# This script is written in Brainfuck, an esoteric programming language.
-# Brainfuck operates on an array of memory cells, each initially set to zero.
-# The language uses a minimalistic set of commands.
-# Commands are: > (increment memory pointer), < (decrement memory pointer),
-# + (increment byte at memory pointer), - (decrement byte at memory pointer),
-# . (output byte at memory pointer to stdout), , (input byte to memory pointer),
-# [ (jump past matching ] if byte at memory pointer is zero),
-# ] (jump back to matching [ if byte at memory pointer is non-zero).
+(This demonstrates "Hello World" in INTERCAL, an esoteric and deliberately obtuse programming language known for its complexity.)
 
-# Initialize required memory cells for 'H', 'e', 'l', 'o', 'W', 'r', 'd' using the patterns below.
+PLEASE DO ,1 <- #13
+DO ,1 SUB #1 <- #72
+DO ,1 SUB #2 <- #101
+DO ,1 SUB #3 <- #108
+DO ,1 SUB #4 <- #108
+DO ,1 SUB #5 <- #111
+DO ,1 SUB #6 <- #32
+DO ,1 SUB #7 <- #87
+DO ,1 SUB #8 <- #111
+DO ,1 SUB #9 <- #114
+DO ,1 SUB #10 <- #108
+DO ,1 SUB #11 <- #100
+DO ,1 SUB #12 <- #33
+DO ,1 SUB #13 <- #0
 
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
->+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
->+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
->++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
->+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
->+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
->+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
->++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-<<<<<++++++.
-
-# Memory cells now contain: 72 ('H'), 101 ('e'), 108 ('l'), 111 ('o'), 87 ('W'), 114 ('r'), 100 ('d').
-# We will now print "Hello World" using these memory cells.
-
-# Print 'H'
->++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.
-
-# Move to 'e'
->+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.
-# Move to 'l'
->+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.
-# Reuse 'l'
->+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.
-# Move to 'o'
->++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.
-# Print space by moving to a new memory cell and setting it to 32
->++++++++++++++++++++++++++++++++++++++++++++++++.
-# Move to 'W'
->++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.
-# Move back to 'o'
->++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.
-# Move to 'r'
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.
-# Move to 'l'
->+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.
-# Move to 'd'
->++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.
-
-# The output should be "Hello World" now. End of program.
+PLEASE DO ,2 <- #13
+PLEASE DO ,3 <- #1
+(1) DO ,3 <- ,3 + #1
+DO (5) NEXT
+DO ,4 <- ,1 SUB ,3
+DO (4) NEXT
+DO (2) NEXT
+(2) DO ,4 <- ,1 SUB ,3
+DO (4) NEXT
+PLEASE DO (1) NEXT
+(3) DO ,4 <- ,4 - #1
+DO (4) NEXT
+DO READ OUT ,4
+(4) DO FORGET #1
+DO (3) NEXT
+(5) DO ,5 <- #2
+DO (2) NEXT
