68a3f54ab32d22d0f00dae3c2fc0825fbc54f100
Angus Gastle <angusgastle@gmail.com>
2024-08-17 02:00:45 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,24 +1,83 @@
-bf
---[[
-Brainfuck code to print "Hello, World!" to the console.
-Brainfuck is a minimalistic and obscure programming language that uses only 8 commands.
-The following code is extensive in its steps, ensuring complexity and length,
-but ultimately results in printing "Hello, World!" followed by a newline.
-]]
+;; This script is written in INTERCAL (Compiler Language With No Pronounceable Acronym)
+;; INTERCAL is designed to be as different from conventional programming languages as possible.
+;; The goal here is to make an already 'esoteric' language behave in a convoluted manner for "Hello World"
 
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.
-+++++++++++++++++++++++++++++.+++++++..+++.------------------------------------.
-+++++++++++++++++++++++++++++.
-------------.++++++++++++++++++++++++++++.
--------.
-++++++++++++++++++++++++++++++++++++++++++++++++++++.
-----------.
-++++++++++++++++++++++++++++++++++++++++++++++++++++++.
-----.
---------.
-++++++++++++++++++++++++++++++++++++++++++++++++++++.
-----------.
-++++++++++++++++++++++++++++++++++++++++++++++++.
-----.
-++++++++++++++++++++++++++++++++++++.
--------.++++++++++.
+DO ,1 <- #13
+DO ,1 SUB #1 <- #256
+DO ,1 SUB #2 <- #72
+DO ,1 SUB #3 <- #29
+DO ,1 SUB #4 <- #7
+DO ,1 SUB #5 <- #64
+DO ,1 SUB #6 <- #19
+DO ,1 SUB #7 <- #0
+DO ,1 SUB #8 <- #777
+DO ,1 SUB #9 <- #23
+DO ,1 SUB #10 <- #49
+DO ,1 SUB #11 <- #101
+DO ,1 SUB #12 <- #33
+DO ,1 SUB #13 <- #57
+DO ,1 SUB #0 <- #91
+DO ,1 SUB #14 <- #3
+DO ,1 SUB #15 <- #78
+DO ,10 <- #233
+DO ,10 SUB #1 <- #240
+DO ,10 SUB #2 <- #717
+DO ,10 SUB #3 <- #55
+DO ,10 SUB #4 <- #0
+DO ,10 SUB #10 <- #1
+DO ,10 SUB #5 <- #342
+DO ,10 SUB #0 <- #92
+DO ,10 SUB #7 <- #364
+DO ,10 SUB #11 <- #192
+DO ,10 SUB #12 <- #29
+DO ,10 SUB #13 <- #592
+DO ,10 SUB #15 <- #10
+DO ,10 SUB #14 <- #399
+DO ,10 SUB #9 <- #1
+DO ,6 <- #91
+DO ,51 <- #233
+DO ,520 <- #1
+PLEASE COME FROM (1) GOTO (20)(30) NEXT (110)
+(1) DO ,290 <- #1
+PLEASE DO ,290 SUB #2 <- #0
+DO ,33 <- #1
+DO ,72 <- #0
+DO READ OUT ,601
+PLEASE COME FROM (90) GOTO (2)
+(20) DO (290) NEXT ,33
+DO ,291 <- #1
+DO ,460 <- #100
+DO ,461 <- #101
+DO ,462 <- #108
+DO ,463 <- #111
+DO ,464 <- #32
+DO ,465 <- #87
+DO ,466 <- #111
+DO ,467 <- #114
+DO ,468 <- #108
+DO ,469 <- #100
+DO (30) NEXT ,4
+DO COUNT ,40 <- #0
+DO (90) NEXT ,1
+(30) PLEASE ABSTAIN FROM RESUMPTION
+(2) DO ,33 <- #33
+PLEASE DO ,33 SUB #35 <- #0
+DO REINSTATE ,33
+DO (304) NEXT ,33
+PLEASE COME FROM (5) GOTO (3)
+(5) DO RESUME ,33
+PLEASE DO ,601 SUB #3 <- #0
+DO ,601 <- #0
+DO RESUME #1
+DO COUNT ,40 <- #4
+PLEASE (40) NEXT #1
+DO WRITE IN ,601
+PLEASE COME FROM (100) GOTO (3)
+(3) DO ,601 <- ,291
+DO NEXT ,601
+PLEASE RESUME ,5
+(100) PLEASE RESUME ,40
+DO GIVE ,291 SUB #3
+HELLO WORLD!
+(70) DO ,600 <- #65000
+(110) RESUME (70)
