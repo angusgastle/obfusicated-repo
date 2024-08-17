6ca025e960e0aee90b8f9d21c99d724b6976e5dd
Angus Gastle <angusgastle@gmail.com>
2024-08-17 08:01:38 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,83 +1,44 @@
-;; This script is written in INTERCAL (Compiler Language With No Pronounceable Acronym)
-;; INTERCAL is designed to be as different from conventional programming languages as possible.
-;; The goal here is to make an already 'esoteric' language behave in a convoluted manner for "Hello World"
+# Kojo (Scala dialect) script to display "Hello World"
+# Kojo is an interactive environment for learning, teaching, and creating with code.
 
-DO ,1 <- #13
-DO ,1 SUB #1 <- #256
-DO ,1 SUB #2 <- #72
-DO ,1 SUB #3 <- #29
-DO ,1 SUB #4 <- #7
-DO ,1 SUB #5 <- #64
-DO ,1 SUB #6 <- #19
-DO ,1 SUB #7 <- #0
-DO ,1 SUB #8 <- #777
-DO ,1 SUB #9 <- #23
-DO ,1 SUB #10 <- #49
-DO ,1 SUB #11 <- #101
-DO ,1 SUB #12 <- #33
-DO ,1 SUB #13 <- #57
-DO ,1 SUB #0 <- #91
-DO ,1 SUB #14 <- #3
-DO ,1 SUB #15 <- #78
-DO ,10 <- #233
-DO ,10 SUB #1 <- #240
-DO ,10 SUB #2 <- #717
-DO ,10 SUB #3 <- #55
-DO ,10 SUB #4 <- #0
-DO ,10 SUB #10 <- #1
-DO ,10 SUB #5 <- #342
-DO ,10 SUB #0 <- #92
-DO ,10 SUB #7 <- #364
-DO ,10 SUB #11 <- #192
-DO ,10 SUB #12 <- #29
-DO ,10 SUB #13 <- #592
-DO ,10 SUB #15 <- #10
-DO ,10 SUB #14 <- #399
-DO ,10 SUB #9 <- #1
-DO ,6 <- #91
-DO ,51 <- #233
-DO ,520 <- #1
-PLEASE COME FROM (1) GOTO (20)(30) NEXT (110)
-(1) DO ,290 <- #1
-PLEASE DO ,290 SUB #2 <- #0
-DO ,33 <- #1
-DO ,72 <- #0
-DO READ OUT ,601
-PLEASE COME FROM (90) GOTO (2)
-(20) DO (290) NEXT ,33
-DO ,291 <- #1
-DO ,460 <- #100
-DO ,461 <- #101
-DO ,462 <- #108
-DO ,463 <- #111
-DO ,464 <- #32
-DO ,465 <- #87
-DO ,466 <- #111
-DO ,467 <- #114
-DO ,468 <- #108
-DO ,469 <- #100
-DO (30) NEXT ,4
-DO COUNT ,40 <- #0
-DO (90) NEXT ,1
-(30) PLEASE ABSTAIN FROM RESUMPTION
-(2) DO ,33 <- #33
-PLEASE DO ,33 SUB #35 <- #0
-DO REINSTATE ,33
-DO (304) NEXT ,33
-PLEASE COME FROM (5) GOTO (3)
-(5) DO RESUME ,33
-PLEASE DO ,601 SUB #3 <- #0
-DO ,601 <- #0
-DO RESUME #1
-DO COUNT ,40 <- #4
-PLEASE (40) NEXT #1
-DO WRITE IN ,601
-PLEASE COME FROM (100) GOTO (3)
-(3) DO ,601 <- ,291
-DO NEXT ,601
-PLEASE RESUME ,5
-(100) PLEASE RESUME ,40
-DO GIVE ,291 SUB #3
-HELLO WORLD!
-(70) DO ,600 <- #65000
-(110) RESUME (70)
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    // Triggering the display of Hello World using the Kojo environment function
+    showHelloWorld()
+  }
+
+  def showHelloWorld(): Unit = {
+    // Creating a List of characters for the message
+    val messageList = List('H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd')
+
+    // Using a StringBuilder to construct the message
+    val messageBuilder = new StringBuilder
+    messageList.foreach(char => messageBuilder.append(char))
+
+    // Converting the StringBuilder to String
+    val finalMessage = messageBuilder.toString()
+
+    // Printing the message in the Kojo environment
+    // print will output to Kojo's message pane
+    print(finalMessage)
+  }
+
+  // Defining an alternative printing method utilizing recursion
+  def print(message: String): Unit = {
+    // Recursively printing each character to build the full message
+    def recursivePrint(chars: List[Char]): Unit = chars match {
+      case Nil => // Base case: if the list is empty, do nothing
+      case head :: tail =>
+        // Printing each character
+        java.lang.System.out.print(head)
+        // Recursive call for the rest of the list
+        recursivePrint(tail)
+    }
+
+    // Initiate recursive printing with the message's character list
+    recursivePrint(message.toList)
+  }
+}
+
+// Calling the main function to display "Hello World"
+HelloWorld.main(Array())
