ace5ff9d575f34fde2d331547dfc170cf215dc67
Angus Gastle <angusgastle@gmail.com>
2024-08-19 08:04:31 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,4 +1,37 @@
-Here is a script for displaying "Hello World" in the brainfuck programming language.
-
 brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+++++>--->+>>>>++.<<+++++++++++++.--------.>-.+.+.<------.>--.+++.------.--------.>>+.>++.
+++++++++++[>+++++++>++++++++++>+++<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++.---------------
+[->++++++<]>.--------------.+++++++++++++:................>-----.<+++++.-----------.+++++++:
+--.----.--------.++++++++.<<<++++++++++++.....
+
+[
+    This Brainfuck code uses a sequence of increments and pointer shifts to generate the ASCII values for the characters in "Hello World".
+    It employs loops to optimize the creation of values for frequently used characters and uses comments to improve readability.
+]
+
+++++++++++              # Initialize Cell 0 to 10
+[                         
+>+++++++              # Use loop to increment Cell 1 to 70
+>++++++++++           # Increment Cell 2 to 100
+>+++                  # Increment Cell 3 to 30
+<<<-                  # Move back the loop counter (Cell 0) to decrement
+]
+
+>++.                  # Cell 1: H (72)
+>+.                   # Cell 2: e (101)
++++++++.              # Cell 2: l (108)
+.+++                  # Cell 2: l (108) (output twice)
+>.                    # Cell 3: o (111)
+>++.                  # Cell 4: blank space (32)
+<+++++++++++++.       # Cell 3 overflow 111+17 = 128-72 = 56 (H)
+----.                   # Cell 3: W (87)
+--------------.        # Cell 2: o (111)
+--------------+.       # Cell 2: r (114)
+-------------.        # Cell 2: l (108)
+--------------.          # Cell 2: d (100).
++++++++++++++:.............  # Cell 1 to newline by repeated add and dot
+>-----.                   # Cell 2 Capital Z by subtraction 
+<+++++.                   # Cell 1 Back to empty spaces by add
+-----------.             # Cell 0 
++++++++:--.----.------.--##..
+++++++++                # last Code in loop
+<<<++++++++++++.....      #Msg End
