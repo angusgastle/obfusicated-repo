ed08267f8d7ff97bd8d674263da20d913401d060
Angus Gastle <angusgastle@gmail.com>
2024-08-19 14:03:04 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,37 +1,99 @@
+Brainfuck:
 brainfuck
-++++++++++[>+++++++>++++++++++>+++<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++.---------------
-[->++++++<]>.--------------.+++++++++++++:................>-----.<+++++.-----------.+++++++:
---.----.--------.++++++++.<<<++++++++++++.....
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+.>+.+++.+++++.----.+++.---.<-.>+.>++.
 
-[
-    This Brainfuck code uses a sequence of increments and pointer shifts to generate the ASCII values for the characters in "Hello World".
-    It employs loops to optimize the creation of values for frequently used characters and uses comments to improve readability.
-]
 
-++++++++++              # Initialize Cell 0 to 10
-[                         
->+++++++              # Use loop to increment Cell 1 to 70
->++++++++++           # Increment Cell 2 to 100
->+++                  # Increment Cell 3 to 30
-<<<-                  # Move back the loop counter (Cell 0) to decrement
-]
+Piet:
+The Piet programming language uses images to execute code. Here's a PNG representation of "Hello, World!" (You'll need an image viewer to see this):
 
->++.                  # Cell 1: H (72)
->+.                   # Cell 2: e (101)
-+++++++.              # Cell 2: l (108)
-.+++                  # Cell 2: l (108) (output twice)
->.                    # Cell 3: o (111)
->++.                  # Cell 4: blank space (32)
-<+++++++++++++.       # Cell 3 overflow 111+17 = 128-72 = 56 (H)
-----.                   # Cell 3: W (87)
---------------.        # Cell 2: o (111)
---------------+.       # Cell 2: r (114)
--------------.        # Cell 2: l (108)
---------------.          # Cell 2: d (100).
-+++++++++++++:.............  # Cell 1 to newline by repeated add and dot
->-----.                   # Cell 2 Capital Z by subtraction 
-<+++++.                   # Cell 1 Back to empty spaces by add
------------.             # Cell 0 
-+++++++:--.----.------.--##..
-++++++++                # last Code in loop
-<<<++++++++++++.....      #Msg End
+![Piet Hello World](https://esolangs.org/wiki/File:Piet_hello.png)
+
+INTERCAL:
+intercal
+DO ,1 <- #13
+DO ,1 SUB #1 <- #238
+DO ,1 SUB #2 <- #108
+DO ,1 SUB #3 <- #112
+DO ,1 SUB #4 <- #0
+DO ,1 SUB #5 <- #64
+DO ,1 SUB #6 <- #194
+DO ,1 SUB #7 <- #48
+DO ,1 SUB #8 <- #27
+DO ,1 SUB #9 <- #80
+DO ,1 SUB #10 <- #58
+DO ,1 SUB #11 <- #47
+DO ,1 SUB #12 <- #57
+DO ,1 SUB #13 <- #195
+DO ,1 SUB #14 <- #82
+DO ,1 SUB #15 <- #16
+DO ,1 SUB #16 <- #30
+DO ,1 SUB #17 <- #93
+DO ,1 SUB #18 <- #3
+DO READ OUT ,1 SUB #1
+DO READ OUT ,1 SUB #2
+DO READ OUT ,1 SUB #3
+DO READ OUT ,1 SUB #4
+DO READ OUT ,1 SUB #5
+DO READ OUT ,1 SUB #6
+DO READ OUT ,1 SUB #7
+DO READ OUT ,1 SUB #8
+DO READ OUT ,1 SUB #9
+DO READ OUT ,1 SUB #10
+DO READ OUT ,1 SUB #11
+DO READ OUT ,1 SUB #12
+DO READ OUT ,1 SUB #13
+DO READ OUT ,1 SUB #14
+DO READ OUT ,1 SUB #15
+DO READ OUT ,1 SUB #16
+DO READ OUT ,1 SUB #17
+DO READ OUT ,1 SUB #18
+
+
+Malbolge:
+malbolge
+('&%:9]!~}|z/PX5POQQSUT\PO]ON,9K9DC6"43$$"&(17295.*1''+#&!( !'(7(qR}|yPo]mlKj)(182/0O^`_^\XsutPNORfF_\_^XXW/[VQP4JI]G8B2xw~ut # !&$ !!#~}|y#"! #  ! !&$ 
+
+
+Befunge:
+befunge
+>25*"!dlroW olleH":v
+                v:,_@
+                >:^
+
+
+Whitespace:
+whitespace
+[S][S][S][T][S][S][N]
+[S][S][S][T][S][S][T][S][N]
+[S][S][S][T][S][T][S][N]
+[S][S][S][T][S][T][S][T][S][T][S][N]
+[S][S][S][T][S][T][S][T][S][S][S][N]
+[S][S][S][T][T][S][N]
+[S][S][S][T][T][S][T][S][N]
+[S][S][S][T][T][T][S][N]
+[S][S][S][T][T][T][S][T][N]
+[S][S][S][T][T][T][T][S][N]
+[S][S][S][T][T][T][T][T][N]
+[S][S][S][T][S][N]
+[S][T][T][S][N]
+[S][T][T][S][T][N]
+[S][T][T][T][N]
+[S][T][T][T][T][N]
+[T][N]
+[T][S][S][T][N]
+[T][S][S][S][N]
+[T][S][S][T][S][T][T][N]
+[T][S][S][T][S][S][T][T][N]
+[T][S][S][T][S][S][T][S][T][S][T][N]
+[T][S][S][T][S][N]
+[T][S][S][T][S][S][T][N]
+[T][S][T][T][N]
+[T][S][N]
+[T][N]
+[S][S][S][N]
+[S][N]
+[S][S][T][S][N]
+[S][S][N]
+[S][S][T][T][S][N]
+[S][S][S][T][N]
+[S][S][T][N]
