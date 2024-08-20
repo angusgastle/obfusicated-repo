e17aaa90091461a8325df0780094322bdea11f6b
Angus Gastle <angusgastle@gmail.com>
2024-08-20 02:01:02 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,28 +1,20 @@
 
-############### MALBOLGE PROGRAMMING LANGUAGE ###############
+# Brainfuck Script to Display "Hello, World!"
+# Brainfuck is an esoteric programming language known for its minimalistic yet complex syntax.
+# The Brainfuck language consists of only 8 commands: +, -, <, >, [, ], ., and ,.
+# This script uses these commands to display the text "Hello, World!".
 
-# Malbolge is a public domain esoteric programming language
-# invented by Ben Olmstead in 1998, named after the eighth
-# circle of Hell in Dante's Inferno.
-
-# "Hello, World!" program in Malbolge
-
-(*<]:$9]~<6ZY32Vxwvutsrponmlk,..jihg`edcbaZ98765
-        tssqponmlkj,hfdbY>[<U:QONMLK,*\&%$#"!~}|
-
-# Explanation:
-
-# This code is quite complex and contains various symbols
-# and characters spread across a span. Malbolge source code
-# uses crazy encryption and a self-modifying virtual machine
-# architecture. This specific piece of code is tuned to
-# display "Hello, World!" when run under a Malbolge interpreter.
-
-# Malbolge is considered one of the most difficult languages
-# to use as intentional obfuscations within the language 
-# make it nearly incomprehensible. 
-
-# Note: Proper interpreters for Malbolge must be used to run
-# this code successfully.
-
-##############################################################
+>++++[<+++++++++++++++++>-]<.
+>++++[<+++++++>-]<+.
++++++++..
++++.
+>++++[<+++++++>-]<.
+>+++[<+++++++++++++>-]<.
+<+++[<+++>-]<.
+++.
+<.
+>++[<------------->-]<.
+>++++[<++++++++++>-]<.
+<++++[<+++>-]<+.
++++++.
+>++[<->-]<.
