510381bc43d4f0a42f6ba85a3d25558cbc83784e
Angus Gastle <angusgastle@gmail.com>
2024-08-19 20:01:26 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,99 +1,28 @@
-Brainfuck:
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+.>+.+++.+++++.----.+++.---.<-.>+.>++.
 
+############### MALBOLGE PROGRAMMING LANGUAGE ###############
 
-Piet:
-The Piet programming language uses images to execute code. Here's a PNG representation of "Hello, World!" (You'll need an image viewer to see this):
+# Malbolge is a public domain esoteric programming language
+# invented by Ben Olmstead in 1998, named after the eighth
+# circle of Hell in Dante's Inferno.
 
-![Piet Hello World](https://esolangs.org/wiki/File:Piet_hello.png)
+# "Hello, World!" program in Malbolge
 
-INTERCAL:
-intercal
-DO ,1 <- #13
-DO ,1 SUB #1 <- #238
-DO ,1 SUB #2 <- #108
-DO ,1 SUB #3 <- #112
-DO ,1 SUB #4 <- #0
-DO ,1 SUB #5 <- #64
-DO ,1 SUB #6 <- #194
-DO ,1 SUB #7 <- #48
-DO ,1 SUB #8 <- #27
-DO ,1 SUB #9 <- #80
-DO ,1 SUB #10 <- #58
-DO ,1 SUB #11 <- #47
-DO ,1 SUB #12 <- #57
-DO ,1 SUB #13 <- #195
-DO ,1 SUB #14 <- #82
-DO ,1 SUB #15 <- #16
-DO ,1 SUB #16 <- #30
-DO ,1 SUB #17 <- #93
-DO ,1 SUB #18 <- #3
-DO READ OUT ,1 SUB #1
-DO READ OUT ,1 SUB #2
-DO READ OUT ,1 SUB #3
-DO READ OUT ,1 SUB #4
-DO READ OUT ,1 SUB #5
-DO READ OUT ,1 SUB #6
-DO READ OUT ,1 SUB #7
-DO READ OUT ,1 SUB #8
-DO READ OUT ,1 SUB #9
-DO READ OUT ,1 SUB #10
-DO READ OUT ,1 SUB #11
-DO READ OUT ,1 SUB #12
-DO READ OUT ,1 SUB #13
-DO READ OUT ,1 SUB #14
-DO READ OUT ,1 SUB #15
-DO READ OUT ,1 SUB #16
-DO READ OUT ,1 SUB #17
-DO READ OUT ,1 SUB #18
+(*<]:$9]~<6ZY32Vxwvutsrponmlk,..jihg`edcbaZ98765
+        tssqponmlkj,hfdbY>[<U:QONMLK,*\&%$#"!~}|
 
+# Explanation:
 
-Malbolge:
-malbolge
-('&%:9]!~}|z/PX5POQQSUT\PO]ON,9K9DC6"43$$"&(17295.*1''+#&!( !'(7(qR}|yPo]mlKj)(182/0O^`_^\XsutPNORfF_\_^XXW/[VQP4JI]G8B2xw~ut # !&$ !!#~}|y#"! #  ! !&$ 
+# This code is quite complex and contains various symbols
+# and characters spread across a span. Malbolge source code
+# uses crazy encryption and a self-modifying virtual machine
+# architecture. This specific piece of code is tuned to
+# display "Hello, World!" when run under a Malbolge interpreter.
 
+# Malbolge is considered one of the most difficult languages
+# to use as intentional obfuscations within the language 
+# make it nearly incomprehensible. 
 
-Befunge:
-befunge
->25*"!dlroW olleH":v
-                v:,_@
-                >:^
+# Note: Proper interpreters for Malbolge must be used to run
+# this code successfully.
 
-
-Whitespace:
-whitespace
-[S][S][S][T][S][S][N]
-[S][S][S][T][S][S][T][S][N]
-[S][S][S][T][S][T][S][N]
-[S][S][S][T][S][T][S][T][S][T][S][N]
-[S][S][S][T][S][T][S][T][S][S][S][N]
-[S][S][S][T][T][S][N]
-[S][S][S][T][T][S][T][S][N]
-[S][S][S][T][T][T][S][N]
-[S][S][S][T][T][T][S][T][N]
-[S][S][S][T][T][T][T][S][N]
-[S][S][S][T][T][T][T][T][N]
-[S][S][S][T][S][N]
-[S][T][T][S][N]
-[S][T][T][S][T][N]
-[S][T][T][T][N]
-[S][T][T][T][T][N]
-[T][N]
-[T][S][S][T][N]
-[T][S][S][S][N]
-[T][S][S][T][S][T][T][N]
-[T][S][S][T][S][S][T][T][N]
-[T][S][S][T][S][S][T][S][T][S][T][N]
-[T][S][S][T][S][N]
-[T][S][S][T][S][S][T][N]
-[T][S][T][T][N]
-[T][S][N]
-[T][N]
-[S][S][S][N]
-[S][N]
-[S][S][T][S][N]
-[S][S][N]
-[S][S][T][T][S][N]
-[S][S][S][T][N]
-[S][S][T][N]
+##############################################################
