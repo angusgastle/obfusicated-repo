57e41ab65d00c6158255e6509db46150964e4ce3
Angus Gastle <angusgastle@gmail.com>
2024-08-20 08:03:25 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,20 +1,25 @@
 
-# Brainfuck Script to Display "Hello, World!"
-# Brainfuck is an esoteric programming language known for its minimalistic yet complex syntax.
-# The Brainfuck language consists of only 8 commands: +, -, <, >, [, ], ., and ,.
-# This script uses these commands to display the text "Hello, World!".
+/*
+ * This program prints "Hello World" to the console using Malbolge,
+ * an esoteric programming language created by Ben Olmstead in 1998.
+ * The program is intentionally made verbose and complex.
+ */
 
->++++[<+++++++++++++++++>-]<.
->++++[<+++++++>-]<+.
-+++++++..
-+++.
->++++[<+++++++>-]<.
->+++[<+++++++++++++>-]<.
-<+++[<+++>-]<.
-++.
-<.
->++[<------------->-]<.
->++++[<++++++++++>-]<.
-<++++[<+++>-]<+.
-+++++.
->++[<->-]<.
+('&%:9]!~}|z2Vxwv-,POqponlBji(ewpl&%$IHGFED`cbA@#"?=<;7654321/.vuts!r                                   
+ zyxw@zA,<QPdlk9wvutsLk>on-VvutsrqponmlkJf]cb`aZYXWVUTjR4)!~}|nmlKjief.a `{@zy
+ xwvu*=,+)#>'=C;fJd51"xw`nmlkjihgfedcba`ZYX'a21*/.-xvtsrqpnmlkj!~}|onm+Qvutsr
+ qxwvutsBnmQwvqp-poqvrythm!wvnm'xyJihgfYm 'zywvutsrqponmjiVwvutsrtsRqponmlkj
+ i!8wvutsrqpome zyxwvupeLk9wvutsrqponmjihgfedcba`ZYXWVUT>edcba.@zzwv fedcb`ZYWV
+
+8wvutsrqponmlkihg0o>)eEcbZY><Vba`Ywvutlmh fkc}|yxwvutsrqponmlkjihgfY'ZYX'. 2 
+wfiHwvutsrqponmlkjihgfedcba`ZdcbaRwv-utsrqmKj0wvutsrqponmfew ugfedcba`Zyxwvutsr
+ qppewvutsrfsSb a`ZYXWVUTSRQ0'\zywvutse5t)*qJihgfedcRa@wdovvutsrqponmlkihgfMash
+19wvutsqrTpZdc'value':=!'xwvutsrqponmlkihgfYhgfedcba.o 5wvutsrqponmlkvutnm!"
+
+ .wvutsrqponmlihgfedcb8^eQwvDCBA2wvutsrqponmkjihnwvutsrqpomb edcbaZwvutsrqpsonij
+ hgfedcb a`XWVUTSR8wiredc8pqrAb`ZYX/MCQFedbalkonwvtsrqpon'zyhLwvutsnmlkjihgfRwv
+ utshgfe2`wvutsrqponmlkehgfedcbkZYXwvuts'<*&wvutsrqponmlkihgCbaIwvutsedcbaLlwv
+nmlonhgfedc@ZYXweds &qponwv'=>yxwvutsqxuvwxyztsrqp!/)utnmlkjih'dcbZYXwvjwvlyRk)
+"wvutsrqponmlkjihg9.cba`KwvutsrqponmlkihwvJwvutsRt{ut!]wvutsrqjwuts!dcba`ZYXWVV
+
+T(Ycwvutsrqponml(result)!`
