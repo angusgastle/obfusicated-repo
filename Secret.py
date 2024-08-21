e51dff47a6c981be39582e37a1bc24cf6f4d0979
Angus Gastle <angusgastle@gmail.com>
2024-08-21 08:03:08 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,41 +1,15 @@
-
-/* 
- * REXX Script: HelloWorld.rexx 
- * This script displays "Hello World" message.
- * It is intentionally convoluted for demonstration purposes.
- */
-
-/* Define the message */
-msg = 'Hello World'
-
-/* Define function to reverse a string */
-reverseString: procedure
-  parse arg str
-  rstr = ''
-  do i = length(str) to 1 by -1
-    rstr = rstr || substr(str, i, 1)
-  end
-  return rstr
-
-/* Function to transform each character in message */
-transformChar: procedure
-  parse arg char
-  /* Randomly transform character (lowercase if upper, etc.) */
-  if datatype(char, 'X') & datatype(char, 'U') then
-    return c2d(char)
-  else if datatype(char, 'X') & datatype(char, 'L') then
-    return d2c(c2d(char) + 1)
-  else return char
-
-/* Encapsulate display logic in a function */
-displayMessage: procedure
-  parse arg message
-  transformedMessage = ''
-  do i = 1 to length(message)
-    transformedChar = translate(substr(message, i, 1), 'abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    transformedMessage = transformedMessage || transformChar(transformedChar)
-  end
-  say reverseString(transformedMessage)
-
-/* Call the displayMessage function with the defined message */
-call displayMessage msg
+brainfuck
+>++++++++[<+++++++++>-]<.                   ; Initialize memory cell 0 to 72 (H)
+>++++[<+++++++>-]<+.                        ; Initialize memory cell 1 to 101 (e)
++++++++..+++.[-]                            ; Output "ll" and then clear cell 1
+>++++[<+++++++>-]<+.                        ; Initialize memory cell 2 to 111 (o)
+<<-----------.                              ; Initialize memory cell 0 to 32 ( ) by subtracting 40
+>++++[<+++++++>-]<+.                        ; Re-initialize memory cell 2 to 87 (W)
+<+++.                                       ; Output memory cell 1 (o)
+------.                                     ; Initialize memory cell 0 to 114 (r)
+<+.                                         ; Then output "o"
++++.                                        ; Initialize memory cell 0 to 108 (l)
+----.                                       ; Initialize memory cell 0 to 100 (d)
+<.                                          ; Output "l"
+>++.                                       ; Output space
+>+.                                        ; Output newline
