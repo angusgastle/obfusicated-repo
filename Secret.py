c91ee01b6c0dede1e24e433a334631ca0e5810ef
Angus Gastle <angusgastle@gmail.com>
2024-08-21 14:03:03 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,15 +1,61 @@
 brainfuck
->++++++++[<+++++++++>-]<.                   ; Initialize memory cell 0 to 72 (H)
->++++[<+++++++>-]<+.                        ; Initialize memory cell 1 to 101 (e)
-+++++++..+++.[-]                            ; Output "ll" and then clear cell 1
->++++[<+++++++>-]<+.                        ; Initialize memory cell 2 to 111 (o)
-<<-----------.                              ; Initialize memory cell 0 to 32 ( ) by subtracting 40
->++++[<+++++++>-]<+.                        ; Re-initialize memory cell 2 to 87 (W)
-<+++.                                       ; Output memory cell 1 (o)
-------.                                     ; Initialize memory cell 0 to 114 (r)
-<+.                                         ; Then output "o"
-+++.                                        ; Initialize memory cell 0 to 108 (l)
-----.                                       ; Initialize memory cell 0 to 100 (d)
-<.                                          ; Output "l"
->++.                                       ; Output space
->+.                                        ; Output newline
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.>-.<---.^.^.<<-.>>-----------.<<++++.---.+.>>.<.+++.------.--------.>>+.>++++.
+
+
+whitespace
+[S S S T S L S S T L S S T S L S S S S T L S S S S L S S S T S T T L S S S T T T L S L S S S T T T S L S S S S S S L S S S T S S T S S L S S S S T S T T T L S S T T L S S S S L S S S T T T S T T S T T T S L L S S T S L S S T S S S S T L S S S S L S S T S S T S L]
+
+[S S S T S L S S T L S S T S L S S S S T L S S S S L S S S T S T T L S S S T T T L S L S S S T T T T L S S T T L T T T S T S S T S L S S T T L S S S S T L S S S L S S S T S T T T L S L S S S T T T L S T S T S T L S S S T S S S L S S S T T T S]
+
+
+malbolge
+`j/!9~]?FLza3V4IrV%Ol6vvZ]_\T=7-Q!XsE0hj&:>,g]!=}WX]dDv.*9JCdl0uL4K?K#
+
+
+INTERCAL
+PLEASE DO .1 <- #13
+PLEASE DO .1 <- #2387
+DO (256) NEXT
+PLEASE GIVE UP
+
+
+Hexagony
+F'>-'.{(o~'l
+[jg)_cb]*#
+
+
+Piet
+[1][2][3]
+[4][5][6]
+[7][8][9]
+
+[1]: push
+[2]: 8 colors to indicate operations
+[3]: input integer
+[4]: output character
+[5]: end program
+[6]: 8 colors to indicate values
+[7]: reserved operation code
+[8]: reserved for future use
+[9]: reserved output code
+
+
+Shakespeare
+The.hello. world.
+
+ Act I: Scene I: [Enter Hamlet and Romeo]
+
+Hamlet:
+You. lying stupid lackey! You. are. as. mean. as. the. sum. of. yourself. and. a. cleaner.
+ Speak. your mind!
+ [Exit Hamlet]
+
+Romeo:
+Listen. to. your. heart! you. are. the. fairest. of. them. all.
+ Do. you. like. this. hell? away!
+
+
+ArnoldC
+IT'S SHOWTIME
+TALK TO THE HAND "HELLO WORLD"
+YOU HAVE BEEN TERMINATED
