341be998d7f3f7355068801da7bd0b475642a5de
Angus Gastle <angusgastle@gmail.com>
2024-08-21 20:01:34 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,61 +1,37 @@
 brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.>-.<---.^.^.<<-.>>-----------.<<++++.---.+.>>.<.+++.------.--------.>>+.>++++.
+++++++++++[>+++++++<-]>++[>++++++<-]>+.--[->+++<]>-.++[--->++<]>----.[--->+<]>-.++[--->++<]>.[--->++<]>++.----------.+++++.--------.+++.
 
 
 whitespace
-[S S S T S L S S T L S S T S L S S S S T L S S S S L S S S T S T T L S S S T T T L S L S S S T T T S L S S S S S S L S S S T S S T S S L S S S S T S T T T L S S T T L S S S S L S S S T T T S T T S T T T S L L S S T S L S S T S S S S T L S S S S L S S T S S T S L]
-
-[S S S T S L S S T L S S T S L S S S S T L S S S S L S S S T S T T L S S S T T T L S L S S S T T T T L S S T T L T T T S T S S T S L S S T T L S S S S T L S S S L S S S T S T T T L S L S S S T T T L S T S T S T L S S S T S S S L S S S T T T S]
+[S S S T]    (Push 1)
+[T    S S S S T S N L]    (Output "H")
+[S S S S]    (Push 7)
+[T    S S S N L]    (Output "e")
+[S S S S]    (Push 7)
+[T    S S S N L]    (Output "l")
+[T    S S S N L]    (Output "l")
+[S S S S S]    (Push 8)
+[T    S S T N L]    (Output "o")
+[S S S S]    (Push 7)
+[T    S S T N L]    (Output " ")
+[S S S T]    (Push 1)
+[T    S S S S T S N L]    (Output "W")
+[S S S S S]    (Push 8)
+[T    S S T N L]    (Output "o")
+[S S S S S S T]    (Push 2)
+[S S S S]    (Push 7)
+[T    S S]    (Add - 7 - Push 2)
+[T    S T T S N L]    (Output "r")
+[S S S S]    (Push 7)
+[T    S S N L]    (Output "l")
+[S S S S]    (Push 7)
+[T    S S T N L]    (Output "d")
+[T    S S N L]    (Output "!")
 
 
 malbolge
-`j/!9~]?FLza3V4IrV%Ol6vvZ]_\T=7-Q!XsE0hj&:>,g]!=}WX]dDv.*9JCdl0uL4K?K#
-
-
-INTERCAL
-PLEASE DO .1 <- #13
-PLEASE DO .1 <- #2387
-DO (256) NEXT
-PLEASE GIVE UP
-
-
-Hexagony
-F'>-'.{(o~'l
-[jg)_cb]*#
-
-
-Piet
-[1][2][3]
-[4][5][6]
-[7][8][9]
-
-[1]: push
-[2]: 8 colors to indicate operations
-[3]: input integer
-[4]: output character
-[5]: end program
-[6]: 8 colors to indicate values
-[7]: reserved operation code
-[8]: reserved for future use
-[9]: reserved output code
-
-
-Shakespeare
-The.hello. world.
-
- Act I: Scene I: [Enter Hamlet and Romeo]
-
-Hamlet:
-You. lying stupid lackey! You. are. as. mean. as. the. sum. of. yourself. and. a. cleaner.
- Speak. your mind!
- [Exit Hamlet]
-
-Romeo:
-Listen. to. your. heart! you. are. the. fairest. of. them. all.
- Do. you. like. this. hell? away!
-
-
-ArnoldC
-IT'S SHOWTIME
-TALK TO THE HAND "HELLO WORLD"
-YOU HAVE BEEN TERMINATED
+('&%:9]!~}|zPhe<;#
+"dc"
+"*4<^`AK:^9"!-.PZKhF/*~yqX
+&edc'*+)('454%#"!~|{zyxwvtsrqponljigc
+"!~
