12d6fc36cbacfbeb378bd421d66f142614b0c99d
Angus Gastle <angusgastle@gmail.com>
2024-08-26 14:02:55 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,19 @@
-brainfuck
-++++++++++[>+++++++<-]>.<++++[>----<-]>-.----.----.---.-[->+++++<]>.+.[->+++<]<.[->+++++<]>.-.
+Piet:
+
+
+            yellow                 yellow                       yellow                       yellow    red     red     red
+        light yellow           light yellow                 light yellow                 light yellow  green
+    red             white   light orange    light yellow    light yellow    kagome light yellow)  blue   magenta               white
+  red  green black  white   salmon          light orange   cli  light yellow blue lightning   radial  red     white   salmon       yellow
+ white salmon  yellow        yellow           light blue   light yellow)    blue   yellow pink light blue   radial water yellow        yellow       yellow 
+yellow     yellow             yellow            light yellow    light  yellow zigzag blue    magenta ford            yellow junction bright yellow light
+yellow  light orange            black    light yellow  pink white         brown light blue   maroon  radial  cyan  white azure  azure cyan white  cyan pink
+ white  light yellow          salmon white            light yellow choose salmon     blue    saul    yellow    white pink black winding  cyan white light
+    white  white mint red white  white           yellow   yellow  yellow transition red  yellow     white    brown      light  yellow gradational    salmon light blue
+ LRO_1                            yellow light yellow    cyan          blue      cyan  white         light blue jagged white    radial           salmon        pink      parabolic 
+winding                   yellow     LRO_2    light  yellow    blue derivative     light yellow   cyan    cyan Yukon light blue   white  pink  cyan winding cyan  blue
+
+
+
+
+Note: Piet programs are constructed as graphics, with different colored pixels representing different instructions. The above "Hello World" code is represented within the context and structure of the Piet language, thus appearing complex and abstract. This code needs to be copied to a Piet IDE or Interpreter to visualize it properly.
