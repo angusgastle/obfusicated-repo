0878dad06d609bc1468bf39c2fa08b2bc603e051
Angus Gastle <angusgastle@gmail.com>
2024-08-26 02:00:58 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,9 +1,21 @@
+data Str = H | E | L | O | W | R | D | Space deriving Show
 
-; This is a Malbolge program
-; Malbolge is an esoteric programming language designed to be difficult to program in
-; Here, we will display "Hello World" using complex and obfuscated instructions
+helloWorld :: [Str]
+helloWorld = [H, E, L, L, O, Space, W, O, R, L, D]
 
-(=<`#9]~6ZY32VxN*D.Qe&4c):"u~~~d=BpR3:_84N59pN&%<ILmyxw/;}|.
-\$@:ZY97)xQ<!^K5a"$|.tzrLHG}2k9w17h_x{=Akq/-Jb}f(%X6Vv-qfgw*)~
-#6:30<&pO-Y7cPN*!]{s[Pp/<vDd5-;/tKdA>xcw/OJ-8_Aj5|~9YEhDFaRqG
-tCoL|jGBZG4NYKz1M<D7KY#u,<2cu^-;Ug/zK}6_XZ|$8Uz"bpJeyl}^lNAQOdL
+characterToString :: Str -> String
+characterToString H = "H"
+characterToString E = "E"
+characterToString L = "L"
+characterToString O = "O"
+characterToString W = "W"
+characterToString R = "R"
+characterToString D = "D"
+characterToString Space = " "
+
+listToString :: [Str] -> String
+listToString [] = ""
+listToString (x:xs) = characterToString x ++ listToString xs
+
+main :: IO ()
+main = putStrLn (listToString helloWorld)
