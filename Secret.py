4f4317692a528486f812dd2c2e0dfa264bc24712
Angus Gastle <angusgastle@gmail.com>
2024-08-27 14:02:44 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,23 +1,12 @@
-# Brainfuck script to display "Hello World!"
 
-# Initialize cells to store ASCII values
-++++++++++  # Cell 0 (initial value 10)
-[
-  >+++++++ +++++       # Cell 1 (value 70)
-  >++++++++++ +       # Cell 2 (value 10)
-  >+++                # Cell 3 (value 3)
-  >+                  # Cell 4 (value 1)
-  <<<<-               # Move back and decrement loop
-]
+/*
+This script is written in Malbolge, an esoteric and obscure programming language.
+It is known for its complexity and being almost impossible to program in by hand.
+The following code will display "Hello World!".
+*/
 
-# Move to cell 1 and prepare the values for "Hello World!"
->++.                  # H (72)
->+.                   # e (101)
-+++++++..+++.         # l (108), l (108), o (111)
->++.                  # Space (32)
-<<++++++++++++++.     # W (87)
->.                    # o (111)
-+++.------.--------.  # r (114), l (108), d (100)
-<<+<+.                # ! (33) and move to start
+('&%:9]!~}|z&=B+3J;=xxfqqoimCXVsb1`]ick
 
-# Result: "Hello World!"
+~<:vvs,<:!!w??^_.]:#`Gq&/ro.lr`cxoypv.bottom$," coded /*_^)?^(_;&9!>.:=z#j[(`
+
+*/|etjszx\|aiori=^pN]..zsggooru-!!,Gqmw,=w9|(G^,\?$KxNPU^.[SNq?M-oere^^v!.
