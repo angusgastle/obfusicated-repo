dcc121bc160eb9ba347725011ca104e6c4c213db
Angus Gastle <angusgastle@gmail.com>
2024-08-27 20:01:30 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,12 +1,33 @@
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+>++>->>+++[<]<[>++++++++++++++.<]
 
-/*
-This script is written in Malbolge, an esoteric and obscure programming language.
-It is known for its complexity and being almost impossible to program in by hand.
-The following code will display "Hello World!".
-*/
 
-('&%:9]!~}|z&=B+3J;=xxfqqoimCXVsb1`]ick
+whitespace
+[S][S][S][S][L][T][T][S][T][T][S][T][T][L][S][T][L][T][L][T][S][S][L][T][S]
+[S][L][S][L][S][L][S][L][S][S][S][S][L][L][T][S][T][T][S][T][S][T][T][L][T]
+[T][S][S][T][L][S][S][S][L][L][T][S][T][T][S][T][T][T][S][S][L][T][S][T][T]
+[T][T][T][S][L][L][T][S][S][T][T][S][L][T][S][S][T][S][T][T][T][T][S][S][L]
+[NL][S][S][S][S][L][S][S][S][S][L][S][S][S][S][L][T][L][S][T][T][S][S][L]
+[T][S][S][L][T][T][T][T][S][T][S][T][L][T][S][S][NL]
 
-~<:vvs,<:!!w??^_.]:#`Gq&/ro.lr`cxoypv.bottom$," coded /*_^)?^(_;&9!>.:=z#j[(`
 
-*/|etjszx\|aiori=^pN]..zsggooru-!!,Gqmw,=w9|(G^,\?$KxNPU^.[SNq?M-oere^^v!.
+Piet
+  r grgb g  r bbbbbbbbb wbb rbb gbb rbb gbb r
+  r   b   b r g       g b r  b  r  g   gbbbb
+  r   rb  rg rg    rb  rg rg rb rg r rg r gbb
+  r w r r   r    rg   rgg rg bbb bbb bb g bbb
+  gggggggggggggggg  bbb  gggg  gggg  gg bbbbb
+      b    r          r r b     r  r
+     gg    r        r r rg      rg r r
+     gb    gb      g  g gb      g  g
+     gg    gggggggg ggggggggggggggggggg
+  ggggggggggggggggggggggggggggggggggggggggg g
+  g g g h  w  o  l  o w  r  d              g
+  g w  g    g  l d    g  g b    g           g
+ggg rggggg rgg rg rggg rgg ggg ggg gggggggggggg
+
+
+Malbolge
+('&%:9]!~}|zv}|zvl}?v|,{zy#v}?v~<}{zyx?!}>v}|:&:o+=zx03M1v}|:v}{zyxw>v}|:$:o-~yxwrl|
+>v}|:/+.(',/<?,.'cwxw<@>v}?v~<}{zyxw==#},yxlol}|:v|>@>:p789{zyxw?!$*7utl}|:v}|qrp'#2l+!3m{
+v}|:v}|:<$:"
