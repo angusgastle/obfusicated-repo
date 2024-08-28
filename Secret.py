455fde5e53e722176964f97437807fcc7a863369
Angus Gastle <angusgastle@gmail.com>
2024-08-28 02:00:55 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,33 +1,21 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+>++>->>+++[<]<[>++++++++++++++.<]
+/def hello-world :=
+/BEGIN
+  * Initialize variables
+  ~greeter %in String;
+  ~output %in String;
 
-
-whitespace
-[S][S][S][S][L][T][T][S][T][T][S][T][T][L][S][T][L][T][L][T][S][S][L][T][S]
-[S][L][S][L][S][L][S][L][S][S][S][S][L][L][T][S][T][T][S][T][S][T][T][L][T]
-[T][S][S][T][L][S][S][S][L][L][T][S][T][T][S][T][T][T][S][S][L][T][S][T][T]
-[T][T][T][S][L][L][T][S][S][T][T][S][L][T][S][S][T][S][T][T][T][T][S][S][L]
-[NL][S][S][S][S][L][S][S][S][S][L][S][S][S][S][L][T][L][S][T][T][S][S][L]
-[T][S][S][L][T][T][T][T][S][T][S][T][L][T][S][S][NL]
-
-
-Piet
-  r grgb g  r bbbbbbbbb wbb rbb gbb rbb gbb r
-  r   b   b r g       g b r  b  r  g   gbbbb
-  r   rb  rg rg    rb  rg rg rb rg r rg r gbb
-  r w r r   r    rg   rgg rg bbb bbb bb g bbb
-  gggggggggggggggg  bbb  gggg  gggg  gg bbbbb
-      b    r          r r b     r  r
-     gg    r        r r rg      rg r r
-     gb    gb      g  g gb      g  g
-     gg    gggggggg ggggggggggggggggggg
-  ggggggggggggggggggggggggggggggggggggggggg g
-  g g g h  w  o  l  o w  r  d              g
-  g w  g    g  l d    g  g b    g           g
-ggg rggggg rgg rg rggg rgg ggg ggg gggggggggggg
-
-
-Malbolge
-('&%:9]!~}|zv}|zvl}?v|,{zy#v}?v~<}{zyx?!}>v}|:&:o+=zx03M1v}|:v}{zyxw>v}|:$:o-~yxwrl|
->v}|:/+.(',/<?,.'cwxw<@>v}?v~<}{zyxw==#},yxlol}|:v|>@>:p789{zyxw?!$*7utl}|:v}|qrp'#2l+!3m{
-v}|:v}|:<$:"
+  * Set greetings
+  ~greeter := "Greetings, World!";
+  
+  * Start output
+  ?output := "";
+  
+  * Loop through each character of the greeting
+  |LOOP (i %in 1 to LENGTH(greeter))
+    * Append character to output
+    ?output := CONCAT(output, SUBSTRING(greeter, i-1, i));
+  |END LOOP
+  
+  * Print the output
+  >PRINT(output);
+END\
