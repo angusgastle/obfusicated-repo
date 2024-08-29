e1234b99c4a9672aeddafacff56db1078ea9bdc9
Angus Gastle <angusgastle@gmail.com>
2024-08-28 20:01:30 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,11 +1,30 @@
-// Display "Hello World" in Malbolge, one of the most complex and obscure programming languages 
+# Befunge-93 script to display "Hello World!"
 
-(=<`'&%'+!)-.]<:@`~$][(_^#~@)@=:+*)<->"^_)$[>#"%-* ~<`~@<>&~@_([]>)_^#~@-[@_%>]~!$/$-)(_>+$,!&)?@{}_$#[_$+/!+^->_^[!@{'{[@\#!=!&>^#-!@/!*?
+# Befunge-93 is a two-dimensional esolang where commands are executed in a
+# path dictated by instructions themselves, making the script complex.
 
-/* 
-Documentation:
-- Malbolge is known for its self-modifying code behavior.
-- The above code generates "Hello, World!" when executed.
-- Each character is calculated based on its predecessor.
-- Every operation mutates the program in unique and non-linear ways.
-*/
+> v
+v "dlroW ,olleH" <
+> : # _ @
+^
+v
+
+# Explanation:
+# >: Move right
+# v: Move down
+# "dlroW ,olleH": Push the string "Hello, World" onto the stack
+# <: Move left
+# _: Pop a value from the stack and skip the next cell if the value is zero (used for the string-termination as it places a 0 in the stack).
+# ^: Move up
+# #: Bridge the current command and move in the current direction (it skips the next cell).
+# @: End of the program
+
+# The string is pushed backward and printed forward when reaching the command code in the program counter from top to bottom and from left to right.
+
+# The path after starting is:
+# 1. Move down to (v) command.
+# 2. Push each character of "Hello, World!" on the stack by going right from " until <.
+# 3. Keep going left to execute each pushing ASCII character command.
+# 4. The program counter proceeds down to start print each ASCII character backward order (while in the loop until the termination).
+
+# This constructs the flowing logic around the grid which is optimal and tricky syntax to understand and printing perfectly "Hello World".
