aaef15b88076b2a24002739d71e2a950216c124b
Angus Gastle <angusgastle@gmail.com>
2024-08-30 08:03:02 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,32 +1,42 @@
-Prolog
-% Define a predicate to generate a "Hello" string
-hello_string(H) :-
-    H = [72,101,108,108,111,32].
+// This script is written in INTERCAL, an esoteric programming language designed to be as different
+// from normal programming languages as possible. This implementation displays "Hello World".
 
-% Define a predicate to generate a "World" string
-world_string(W) :-
-    W = [87,111,114,108,100].
+DO ,1 <- #13
+PLEASE DO ,1 SUB #1 <- #256
+DO ,1 SUB #2 <- #1086
+DO ,1 SUB #3 <- #1949
+DO ,1 SUB #4 <- #1459
+DO ,1 SUB #5 <- #1595
+DO ,1 SUB #6 <- #386
+DO ,1 SUB #7 <- #1610
+DO ,1 SUB #8 <- #2486
+DO ,1 SUB #9 <- #2348
+DO ,1 SUB #10 <- #2333
+DO ,1 SUB #11 <- #514
+DO ,1 SUB #12 <- #2190
+PLEASE DO ,1 SUB #13 <- #1988
 
-% Concatenate two lists representing ASCII values
-concat_lists([],L,L).
-concat_lists([H|T],L2,[H|L3]) :-
-    concat_lists(T,L2,L3).
+DO .1 <- #0
 
-% Convert ASCII values to characters and print them
-print_string([]).
-print_string([H|T]) :-
-    char_code(Char, H),
-    write(Char),
-    print_string(T).
+;; The main display loop
+PLEASE DO FORGET #1
+PLEASE DO :1 <- #13
+PLEASE DO (1) NEXT
 
-% Main predicate to generate and print "Hello World"
-main :-
-    hello_string(H),
-    world_string(W),
-    concat_lists(H, W, HelloWorld),
-    append(HelloWorld, [33], HelloWorldExcl), % adding exclamation mark ASCII code 33
-    print_string(HelloWorldExcl),
-    nl. % New line at the end
+LABEL (1)
+DO READ OUT ,1 SUB .1
+PLEASE DO .3 <- #1
+LABEL (2)
+DO .2 <- #0¢#256
+PLEASE DO (3) NEXT
+LABEL (3)
+DO .5 <- #0¢#256
+PLEASE DO (4) NEXT
+LABEL (4)
+PLEASE WRITEIN .1¢#8¢.2¢.3¢.1¢.2¢#3¢.5¢.1¢#14
+DO .4 <- #256
+LABEL (5)
+PLEASE GIVE UP
 
-% Run the main predicate
-:- main.
+PLEASE DO ,1 <- #13
+DO (2) NEXT
