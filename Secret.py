40f1068bebd3ffc82548932d85cc987a6b2b2dab
Angus Gastle <angusgastle@gmail.com>
2024-08-30 14:02:46 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,42 +1,10 @@
-// This script is written in INTERCAL, an esoteric programming language designed to be as different
-// from normal programming languages as possible. This implementation displays "Hello World".
 
-DO ,1 <- #13
-PLEASE DO ,1 SUB #1 <- #256
-DO ,1 SUB #2 <- #1086
-DO ,1 SUB #3 <- #1949
-DO ,1 SUB #4 <- #1459
-DO ,1 SUB #5 <- #1595
-DO ,1 SUB #6 <- #386
-DO ,1 SUB #7 <- #1610
-DO ,1 SUB #8 <- #2486
-DO ,1 SUB #9 <- #2348
-DO ,1 SUB #10 <- #2333
-DO ,1 SUB #11 <- #514
-DO ,1 SUB #12 <- #2190
-PLEASE DO ,1 SUB #13 <- #1988
+/*
+    Malbolge Program - "Hello, World!"
 
-DO .1 <- #0
+    Malbolge is an esoteric programming language known for its extreme difficulty and complexity.
+    This program will display the message "Hello, World!" on the screen.
+*/
 
-;; The main display loop
-PLEASE DO FORGET #1
-PLEASE DO :1 <- #13
-PLEASE DO (1) NEXT
-
-LABEL (1)
-DO READ OUT ,1 SUB .1
-PLEASE DO .3 <- #1
-LABEL (2)
-DO .2 <- #0¢#256
-PLEASE DO (3) NEXT
-LABEL (3)
-DO .5 <- #0¢#256
-PLEASE DO (4) NEXT
-LABEL (4)
-PLEASE WRITEIN .1¢#8¢.2¢.3¢.1¢.2¢#3¢.5¢.1¢#14
-DO .4 <- #256
-LABEL (5)
-PLEASE GIVE UP
-
-PLEASE DO ,1 <- #13
-DO (2) NEXT
+('&%:9]!~}|z2Vxwv-,POqponlKjihf\edcba`_^]\[ZYXWVtsrqp0/.m+*)J&%$#"!~}|zXwv-,PmlkKjihg\edcba`_^]\[ZYXWVUtsr_/.ponl,+*)('&%$#"!~}|z2ytwvkjihgONM@|nwoih\edcZXWVUTSROLw`_^]\[YXWV0tsrqponm,+*)('&%$#"!~}|zXwrujihg\edcba`_^[\MYXW0tsrqponml,+By or how led definition 776 always kijihtSRONT6 :: turns feels code +**)&%$ #"!~}|zXwv-.plSRONM@|nwoihgf\ecba`_^][\YXW0/tuonm,+$(('*%&$#"!~}|2wv-.ts3]kigf\ecZXWUSR0*32UWm,+{)+9!~}|Xwv-.senSRONT6\@onhilgfe &cba`_^[YXR0.strop0mlkjih+*)('&%$#"!~}|z2wv-.ts3]jihgf\edcba`_^][ rw43tSRONM@|nonihgf0+-C'*$#!}~{|wv-.seno4SRON&gf0ZYXW0/.unml,+B(('*C$#!~}|2wv-.ts3]kijhgf\edcZXWU0R.ONTMLKC5fg0ZYX'/.tmnl9 x$(*'%& & }|zXwv-.tsrpon423n@|nwoihgf`_^[\YXWVUT/ROPMLw*Z:zy+7e+nmljihgfe_^[ZXW9P/tSRONM@|non_S(w-43tsrq2WQONM@|6kig9(pkU*m5lRQ')$ $$"~! 5UONSRN~}|zXwv-,PQ_`YVUTSROLw&*\ZY+/nmlkjihgf \ecba`_^]\[YXWV0tsrqponm,+v*)(('Af$#"!|zXwv-,PQOMN_mlKjih &^]\\[YXWV&tuom+*)J'"%$!~}|zXwvvuQ4nLKjihgf\ecba`_^]\U3[XtSRONM@|nonihgf0/_ml,+$(('&C'$#!}|zXV/4t3]kijhf\ecWUSRON*32nU7m+l *)!$#%!))zXV/4Kjihgf\ecba`_^]\[YXWVtsrqponml,+ :9)&%$#!~}|2wv-.tsnSOLKC\bN~onf_2QX]=0tr0AL&lmBA)$"!~|zXwv-.senqK)y@{|gf~ZVUT.32m,ghtSRONT6 +w*(*&" $!"}|zNwv-.sQfgj3]{av65LBA(PONMK*)($$!)~}|z2wv-.pqu/]gOH^*[YXW0tsrponml,+ .v9&%$#"!~}|zXwhe
+*/                                                  
