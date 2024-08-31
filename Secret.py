b40cdd521b50d7953a83196abae94de1bdf1706d
Angus Gastle <angusgastle@gmail.com>
2024-08-30 20:01:13 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,10 +1,57 @@
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<++++++++++++++.>+.>>+.<<+++.------.--------.>>+.>++.
 
-/*
-    Malbolge Program - "Hello, World!"
 
-    Malbolge is an esoteric programming language known for its extreme difficulty and complexity.
-    This program will display the message "Hello, World!" on the screen.
-*/
-
-('&%:9]!~}|z2Vxwv-,POqponlKjihf\edcba`_^]\[ZYXWVtsrqp0/.m+*)J&%$#"!~}|zXwv-,PmlkKjihg\edcba`_^]\[ZYXWVUtsr_/.ponl,+*)('&%$#"!~}|z2ytwvkjihgONM@|nwoih\edcZXWVUTSROLw`_^]\[YXWV0tsrqponm,+*)('&%$#"!~}|zXwrujihg\edcba`_^[\MYXW0tsrqponml,+By or how led definition 776 always kijihtSRONT6 :: turns feels code +**)&%$ #"!~}|zXwv-.plSRONM@|nwoihgf\ecba`_^][\YXW0/tuonm,+$(('*%&$#"!~}|2wv-.ts3]kigf\ecZXWUSR0*32UWm,+{)+9!~}|Xwv-.senSRONT6\@onhilgfe &cba`_^[YXR0.strop0mlkjih+*)('&%$#"!~}|z2wv-.ts3]jihgf\edcba`_^][ rw43tSRONM@|nonihgf0+-C'*$#!}~{|wv-.seno4SRON&gf0ZYXW0/.unml,+B(('*C$#!~}|2wv-.ts3]kijhgf\edcZXWU0R.ONTMLKC5fg0ZYX'/.tmnl9 x$(*'%& & }|zXwv-.tsrpon423n@|nwoihgf`_^[\YXWVUT/ROPMLw*Z:zy+7e+nmljihgfe_^[ZXW9P/tSRONM@|non_S(w-43tsrq2WQONM@|6kig9(pkU*m5lRQ')$ $$"~! 5UONSRN~}|zXwv-,PQ_`YVUTSROLw&*\ZY+/nmlkjihgf \ecba`_^]\[YXWV0tsrqponm,+v*)(('Af$#"!|zXwv-,PQOMN_mlKjih &^]\\[YXWV&tuom+*)J'"%$!~}|zXwvvuQ4nLKjihgf\ecba`_^]\U3[XtSRONM@|nonihgf0/_ml,+$(('&C'$#!}|zXV/4t3]kijhf\ecWUSRON*32nU7m+l *)!$#%!))zXV/4Kjihgf\ecba`_^]\[YXWVtsrqponml,+ :9)&%$#!~}|2wv-.tsnSOLKC\bN~onf_2QX]=0tr0AL&lmBA)$"!~|zXwv-.senqK)y@{|gf~ZVUT.32m,ghtSRONT6 +w*(*&" $!"}|zNwv-.sQfgj3]{av65LBA(PONMK*)($$!)~}|z2wv-.pqu/]gOH^*[YXW0tsrponml,+ .v9&%$#"!~}|zXwhe
-*/                                                  
+whitespace
+[S S S S T N
+_Push_ ’H’_onto_stack]
+[S N
+S _Duplicate]
+[S S S T S N
+_Push_ ’e’_onto_stack]
+[S N
+S _Duplicate]
+[S S S T S T N
+_Push_ ’l’_onto_stack]
+[S N
+S _Duplicate]
+[S T S S T N
+_Add_top_two_numbers]
+[T N
+S S _Print_character_from_stack]
+[S S S T S T N 
+_Push_ ’l’_onto_stack_again]
+[T N
+S S _Print_character_from_stack]
+[S T S S S N
+_Push_ ’o’_onto_stack]
+[T N
+S S _Print_character_from_stack]
+[S N
+S _Duplicate]
+[S S S T S T N
+_Push_ Space_onto_stack]
+[T N
+S S _Print_character_from_stack]
+[S S S S T T T N
+_Push_ ’W’_onto_stack]
+[T N
+S S _Print_character_from_stack]
+[S S S T S T S N
+_Push_ ’o’_onto_stack]
+[T N
+S S _Print_character_from_stack]
+[S T S S S S N
+_Push_ ’r’_onto_stack]
+[T N
+S S _Print_character_from_stack]
+[S T S S T S N
+_Push_ ’l’_onto_stack]
+[T N
+S S _Print_character_from_stack]
+[S T S S S T N
+_Push_ ’d’_onto_stack]
+[T N
+S S _Print_character_from_stack]
+[T T N
+_Ends_the_program]
