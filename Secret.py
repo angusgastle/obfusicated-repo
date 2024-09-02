b5b768f7cb1147c7d518c12f0bdb5521b1fdd114
Angus Gastle <angusgastle@gmail.com>
2024-09-01 20:01:09 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,13 @@
 brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>-.>----.<--.>+.<<<+.
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.                    Documentation: 'H'
+>+.                                                                  Documentation: 'e'
++++++++..+++.                                              Documentation: 'l' (double)
+>>+.                                                                Documentation: 'o'
+<<+++++++++++++++.                                              Documentation: 'space'
+>.                                                         Documentation: 'W'
++++.------.--------.                            Documentation: 'o'
+>>+. <[-]                                           Documentation: 'r' (reset loop)
+<+.                                                            Documentation: 'l'
+------.--------.                                                 Documentation: 'd'
+>+.                                                        Documentation: '!'
+
