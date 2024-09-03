c44d0cc469070ccf55eacf8c1d59720af1786a11
Angus Gastle <angusgastle@gmail.com>
2024-09-03 14:02:53 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,19 +1,13 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<++++++++++++++.>+++++.----------------------.--.>+.>.
+Befunge-98 Code:
 
-# Brainfuck is an esoteric programming language created in 1993 by Urban Müller.
-# It is notable for its extreme minimalism, consisting of only eight commands.
-# Despite its simplicity, Brainfuck is Turing complete, able to express any computable function.
-# The complexity of writing programs in Brainfuck arises from its extremely low-level operations.
-# This "Hello World" program uses a loop to initialize memory cells with specific values
-# and then outputs each character of "Hello World!" by manipulating the memory.
 
-# Instructions:
-# +  : Increment the memory cell under the pointer
-# -  : Decrement the memory cell under the pointer
-# >  : Move the pointer to the right
-# <  : Move the pointer to the left
-# [  : Jump past the matching ] if the cell under the pointer is 0
-# ]  : Jump back to the matching [ if the cell under the pointer is non-zero
-# .  : Output the character at the memory cell under the pointer
-# ,  : Accept one byte of input, storing its value in the memory cell under the pointer
+>v>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+^ >1!*1-= v                                                                < ^*52/88/2+54/@3467-631
+0+60)''/-&/8.#!999999000000))00000000)&&&&&&../((<>*28;+1!*%&$#@-+=?.*()!p've>>>>:''''''''''''''' >:
+>:,, @."Hello World"                                                    v                      
+>v >:                                                                   ^                      
+>:: >: "defalut print message 1: Hello World!" ,,,,,,,,, :              ^                       >
+^ :,,@                                                                    ^  v                 
+                                      <> :''' ''''                             >: ^                     
+                                       > ^                                                                  ^
+                                                                              
