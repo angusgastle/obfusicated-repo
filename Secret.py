6ef92926f1d444be5880195c5d9b9ffe70d2cf7e
Angus Gastle <angusgastle@gmail.com>
2024-09-04 02:00:56 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,3 +1,30 @@
-brainfuck
-++++++++++[>++++++++++<-]>>++++++++++>->>>++++++++++<++++++++<<>>>>++++++.
----------[------->++<]>--.-[--->+<]>---.-[--->++<]>+.++[---->+++<]>-.+[--->+<]>.>[---->+++<]>+.-[--->++<]>-----.+++++++..+++.
+
+/*
+    This is a Cow program (a humorous esoteric programming language)
+    to display "Hello World". Cow is known for its use of only 12 different characters
+    in its instructions. We will use most of them to make the program
+    as long and complex as possible while ensuring it displays "Hello World!".
+*/
+
+MoO mOo MoO moO moO MoO mOo mOo MOO MOO MOO mOo MOO mOo moo
+MOo moo moO moO mOo Moo moo MoO MOo moO mOo moo moO mOo
+
+MoO MoO moO mOo moO mOo mOo MoO mOo Moo MoO moo moO
+
+MoO mOo MoO moo MoO mOo MoO mOo MOO MOO MOO mOo Moo Moo moO Moo mOo moO
+
+moo MoO moo moO moO MoO moo Moo Moo moo moO mOo mOo MoO moO
+
+MOO MOO mOo Moo mOo moO MoO moo moO MOO moO moo Moo
+
+MoO Moo moo moO mOo moO MoO moO Moo mOo moO Moo moO moO
+
+MoO mOo MoO moo MoO Moo moo MoO mOo mOo MOO Moo moo moO Moo mOo
+
+Moo Moo MoO moO Moo mOo Moo moO moo MoO moo MOO MOO mOo
+
+MOO MOO mOo Moo mOo moO MoO moo moO MOO moO moo Moo
+
+MoO Moo moo moO mOo moO mOo moo moO moO moO MoO moO mOo Moo
+
+MoO moo MoO mOo Moo moo MoO Moo moo moO mOo MoO Moo mOo moO
