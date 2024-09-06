77f7d34aa77fde4aa71247b22b1b3a776823e1aa
Angus Gastle <angusgastle@gmail.com>
2024-09-06 02:00:55 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,18 +1,11 @@
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
 
-/**
- * Malbolge program to print "Hello World"
- * Malbolge is known for its extreme obfuscation and difficulty.
- * The instructions are not given here as it is inherently difficult to understand.
- * This will just demonstrate the complexity.
- */
 
-('&%:9]!~}|z}z2Vxwv-,POqponmP+ed)DCBA@98<2zzywv/.-,xwvtsrB5pOnmlkKjih+hGFE.)
-    @BA9z};|wvu<~srqponmlkr+d`cb$^DCBA_~yt{e
-\#\!}|&]\,!(~)CXCX<wts><zyxwv<?/.9Xts>|26yt}r\fdc/:)6}?<?=!z$-9^%*3~# {
+malbolge
+(=<`:9876Z4321UT.-Q+):&%$#"!~}|{zyxwvu
 
-    $@#%!@+(&#!*234|-")_;'0#__)(&%@#,:"$*'")(''$
-}
 
-©The readable Malbolge example!
-.C\"\$#'**9+)\";'!&)($!&!%,',,''++/\(\$\#%*9C'\!*!{{$&\"#9' ,@Q)) } 
-:/#$#)%$##,@'&!@P+'$6#(*]{%@\"}|%}+=*);){!$!"
+Piet
+l
+rcffrffrcffrffrcrrcrffcfffrrcfcfrffrcffrffrffrffrfcfcfffrfrrrrfrrrrcfffrrrrcffffrrfcffffrfrfrffrrfrfrrfrffrrrrfffrrrffrccfcfcfrrrrfccffffcfcfacfrrfcrcrrffffrfrccrr
