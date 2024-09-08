8b612fbf579c24290847be59ebbd0bf84b6c6bd4
Angus Gastle <angusgastle@gmail.com>
2024-09-08 14:01:17 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,20 +1,17 @@
-brainfuck
->++++++++[<+++++++++>-]<-.<++++[>----<-]>.<++[-->+++++<]>++.<<+++[>----<-]>.<++++[>++++<-]>.<+++[>---<-]>-.<++++[>+++<-]>+++.<+++[>----<-]>.[--->+<]>-----.-------.<<++++[>+++<-]>.<+.++[-->----<]>.
+Piet:
 
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
->++++++++[<+++++++++>-]<-                Set Cell #1 to 72 ('H')
->                                         Move to Cell #2
->++++++++++[<+++++++++++>-]<-             Set Cell #2 to 101 ('e')
->>                                        Move to Cell #3
->+++++++[<+++++++>-]<-                    Set Cell #3 to 108 ('l')
-+.                                        Output 'H'
-<.                                        Output 'e'
-+++.                                      Output 'l'
-++++.                                     Output 'o'
-<<++.                                    Output ' '
->>+++++++.                                Output 'W'
--------.                                  Output 'o'
-<------.                                  Output 'r'
-<+++++.                                   Output 'l'
--------.                                  Output 'd'
-.                                         Output '!'
+Piet is a language in which programs are graphic images. Execution proceeds by moving a pointer through the image. Here's a complex Piet program that displays "Hello World":
+
+plaintext
+            21 13 12 11 10 10 10 20
+            21 12 13 12 21 11 13 10
+            11 10 10 20 20 20 10 21
+            13 11 21 21 21 20 10 20
+            10 21 21 10 10 10 10 11
+            11 21 11 20 12 21 12 12
+            21 20 11 20 11 10 21 10
+
+
+Each number represents a specific color. The different shades and transitions of colors control the program flow and data manipulation, ultimately outputting "Hello World".
+
+Note: For actual usage, you would use a Piet interpreter and construct an image with actual colors corresponding to these numbers as defined in the Piet programming language specification.
