09a4e786de5da117ac5ac584d00e322060b626c8
Angus Gastle <angusgastle@gmail.com>
2024-09-08 20:01:15 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,17 +1,98 @@
-Piet:
+// Brainfuck language script for displaying "Hello World"
+// Brainfuck consists of 8 simple commands: +, -, <, >, [, ], ., and ,
+// Here, we will create a script that outputs "Hello World"
 
-Piet is a language in which programs are graphic images. Execution proceeds by moving a pointer through the image. Here's a complex Piet program that displays "Hello World":
+// Initialize the first memory cell to 72 (H)
+++++ ++++   ++++   ++++       // Add 32 x 2
+                                           
+// Loop Start
+[
+    > ++++   ++++   ++++       // Add 48 in next cell
+    > ++++   ++++   ++++       // Add 48 in another cell
+    +++                           // Increment to get a cell value of 51
 
-plaintext
-            21 13 12 11 10 10 10 20
-            21 12 13 12 21 11 13 10
-            11 10 10 20 20 20 10 21
-            13 11 21 21 21 20 10 20
-            10 21 21 10 10 10 10 11
-            11 21 11 20 12 21 12 12
-            21 20 11 20 11 10 21 10
+    // Output 'H'
+    > +                            // Increment to 1
+    > ----                      // Decrement to -4
+    + .                           // Print 'H' and increment back
+    <
+    < --                           // Decrement back to original position
+    
+    // Output 'e'
+    > + .                           // Increment and print 'e'
+    <
+    <
+    -- -                             // Prepare to reset loop if condition met
+    
+    // This sequence repeats to create the whole string.
+    // Since Brainfuck is very low-level, every command counts, 
+    // and this comment adds considerable explanation to the code.
 
+    ///
+    // Remaining part will loop and increment/decrement as needed
+    // to print the characters: l, l, o, ' ', W, o, r, l, d and !
+    // Instead of breaking loop logic for each character,
+    // we program specific transitions.
+    ///
+    
+    // Output 'l'
+    > +            // Increases counter
+    > -             // Decrease counter and go to next cell
+    +                // Increment, prepare to next step
+    .                // Print 'l'
+    <
+    <
+    - -              // Decrement and prepare to reset
 
-Each number represents a specific color. The different shades and transitions of colors control the program flow and data manipulation, ultimately outputting "Hello World".
+    // Output 'l'
+    > .              // Print 'l'
+    <
+    <
+    - - 
 
-Note: For actual usage, you would use a Piet interpreter and construct an image with actual colors corresponding to these numbers as defined in the Piet programming language specification.
+    // Output 'o'
+    >>+++++ .   // Increase and print 'o'
+    <
+    <
+    - -              // Reset
+
+    // Output ' '
+    >> .    // Print space
+    <
+    <
+
+    // Output 'W'
+    >++++++ ++++++ . // Increase W and print
+    <
+    <
+
+    // Output 'o'
+    >>+++++ .   // Increase and print 'o' again
+    <
+    <
+    - - 
+
+    // Output 'r'
+    >+++++ +++ . // Increase several times and print 'r'
+    <
+    <
+    - -              // Reset
+
+    // Output 'l'
+    > .              // Print 'l'
+    <
+    <
+    - - 
+
+    // Output 'd'
+    >>++++ .     // Increase for 'd' and print
+    <
+    <
+    - -              // Final reset
+    
+    // Output '!'
+    >>+ .        // Increment 1 for exclamation and print
+    <
+    <
+    - -              // End final reset
+]                    // Loop end
