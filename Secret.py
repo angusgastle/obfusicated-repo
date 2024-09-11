8480f5a60473dec55a2429ea539b617caf81e535
Angus Gastle <angusgastle@gmail.com>
2024-09-11 08:03:10 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,22 +1,2 @@
-/// Brainfuck program to print "Hello, World!"
-
-// This program uses Brainfuck language 
-// and meticulously sets data values 
-// to output the string "Hello, World!".
-
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]   // Initialize memory cells
->++.                                    // Cell[1] = 72 = 'H', output 'H'
->+.                                     // Cell[2] = 101 = 'e', output 'e'
-+++++++.                                // Cell[2] = 108, output 'l'
-.                                       // continue to output 'l'
-+++.                                    // output 'o'
->++.                                    // Cell[3] = 32 = ' ', output space
-<<-----.                                // Move back to Cell[1], output ','
->>+.                                    // Move to Cell[3], 
-<<---------.                            // output 'W'
-<.                                      // Move to Cell[1], output 'o'
-+++.                                    // output 'r'
-------.                                 // output 'l'
---------.                               // output 'd'
->+.                                     // Move to Cell[2], output '!'
->.                                      // Move to Cell[3], output newline
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+++.>----.+++++++..+++.>>>+++++++++++++++.------------.>+++++++++.<<+++++++++++++.>>+.<<+++.[-]<<[-]>[-]<<[->+>+<<]>>[-<<+>>]<<<[->+>+<<]>>[-<<+>>]<<<[->+>+<<]>>[-<<+>>]<<<[->+<<<[->+>+<<]>>[-<<+>>]>>[->+<<<[->+>+<<]>>[-<<+>>]>>[->+<<<[->+>+<<]>>[-<<+>>]>>>[-]<<<[-]]]]>>>[-]<<<[-]]>>>[-.<]
