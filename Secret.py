ae7ddee97b535af6598919d5192924133ca43f2f
Angus Gastle <angusgastle@gmail.com>
2024-09-11 20:01:37 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,54 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>----.>-----.-....<+.>>+++.<-.>-.<<+.>>>>++++.+.<+++++.------.<-.>>>-.<<.++.>-.<...
+perl
+#!/usr/bin/env perl
+
+# Import necessary modules
+use strict;
+use warnings;
+
+# Function to generate a randomized greeting message
+sub magical_random_greeting {
+    my @words = ("Hello", "World", "Greetings", "Salutations", "From", "Earth", "Galaxy", "Universe");
+    my $word1 = $words[int(rand(scalar @words))];
+    my $word2 = $words[int(rand(scalar @words))];
+    return "$word1 $word2!";
+}
+
+# Function to print out each character with a delay
+sub print_with_delay {
+    my $message = shift;
+    my $delay = shift || 0.1;  # Default delay to 0.1 seconds
+    for my $char (split //, $message) {
+        print $char;
+        select(undef, undef, undef, $delay);  # Delay for specified seconds
+    }
+    print "\n";
+}
+
+# Entry point of our script
+sub main {
+    my $max_attempts = 5;
+    my $attempts = 0;
+    my $final_message = "";
+    
+    # Attempt at generating a readable "Hello World" message
+    while ($attempts < $max_attempts) {
+        $attempts++;
+        my $potential_message = magical_random_greeting();
+        
+        if ($potential_message eq "Hello World!") {
+            $final_message = $potential_message;
+            last;
+        }
+    }
+
+    # If unsuccessful after max attempts, default to "Hello World!"
+    if ($final_message eq "") {
+        $final_message = "Hello World!";
+    }
+
+    # Print the final message with a delay to simulate typing effect
+    print_with_delay($final_message);
+}
+
+# Execute the main function
+main();
