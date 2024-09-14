ebb4d79a9ca0740adbeefa1688a022ec8b6abdcd
Angus Gastle <angusgastle@gmail.com>
2024-09-14 08:01:51 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,11 +1,28 @@
-/// Malbolge program to print "Hello, World!"
+sed
+# SED script to display "Hello, World!"
+# SED is a stream editor for filtering and transforming text
 
-D=<':9DZ=VXXyxz2Vxpa$$m5J]8ZJUV8-2iP=Wnsb!!(!(!%)	L&&/!!!!!!!!!!!!'`_&+\=WS~yV=<VXYdz.xyr:dv+>lVs!2N,dV+Tm-xws:R=dfv~!&IA};SH<eT*W$,"$)>>&CwB2elseif(#DM<?cCarAB"DsjSBvqf{rMxI)%(!'#M!3,jjHGjXtJoEf<IKOBQ?PADGGNiJFXCBInrUFWmCNOqCQN3QAPGTK{Jtt}>RGjC/PN[WXVi1SRA9JgaiHg<"]))UXlI1Fkc
-~ExG0-efQWa8VGXx#P$xZpJqFJ6+]qxWWW8}"B~eGJY9Kxu~o6M5QE"8o'C=>M$e?)<%'">UöpS!I1fk-ZRTcJ'[XwNdaN%D.j{CKOQZHeHbEbxEybCDngI'J*ROiP}A1(aX))F:iPh.N7>B*o3]IT!2-lzDA/Sk5x^44izLB5eydp0d25^(+)vFdV=:_AnRRzs=#Yf5.<JdTHen(iSe0YTqh=!K>,6Yu"&%
-AOo0)Y>iqaGI[!JkNIV(!**!>Q^0>`Fb1LSQm(]*7"md!B9)tV&l.Pitia<!`,>ZOq]vByATYk(SE)|@pxn\IY7PfCPs}0-BP!*CzD^%9T|ouH2wM[S28p[HY}Ya]j3B/S9CS5PymSYUk[B"1VVwyB-MqH=QAI(Q"Q"#r`G..<W8[!}O@NODE@="3RnmgT=OJdLBUW*W{NcU]R(<lg9VHu}EwCIt5/uQ>");
-4x)Psvi'y"BG/y7Tv?A!REc(^]nEWE>Vih'c1#HDv.vAf6Wn)kUkmP&&+,/!!!!HELl!!LO W1wOOR?Ld!j<@I<*/}
+# Clear any existing buffer
+d
 
-99<pdgj0)%)"W)V"!%!dX3AGrC5'J(KWMCVE"Soajdd8YI%!))!%!er7bFPaS>_[+=$gw.L~<VoV(.kV6VXVx~`F[(F$Rm\GटीB,s- कोणारे ?,41, w0Y#N(vc
-sh+#D>FiQx6lmCp
-H<!>#=% &&dOB[!FG!0Z#%)Xp0s301B^/NjxM,lNRzPXW4fte:]%6[Pk^nD)%)X6piABsxes59AEZmnBEJ{3D}k'[:TX(*}%$<=GINq[FXVDxz>FRu>}=>>FOQOAVa<]4v"') =<<alboj+-*/DNE\O~uErTtiR]%S]QD2yKs^8JjbEl0dGN*aBHaP5iv0]"$/2D>>_anK-K4(ivedesolC)>
-WC#4Ym(HBe)yT%)));G$SVFAAIT%.4,3IM@Node(input :&> File.read(-(FHMVS%]{Z<=@@C~ magaz58T!;MW)y)!*:Q""-!PLADIW(juspK>>codaed)}0VtpvSXH)%A!MP#D-=t))%)#491)!@FG,(*Q!WS}|^)*-'=@>>EOF
+# Append the string to the pattern space
+a\
+H\
+e\
+l\
+l\
+o\
+,\
+ \ 
+W\
+o\
+r\
+l\
+d\
+!
+
+# Print the pattern space, which now contains "Hello, World!"
+p
+
+# End of script
+q
