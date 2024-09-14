c95bba8d66f85c1f9fb06f44efadde58c905c0e2
Angus Gastle <angusgastle@gmail.com>
2024-09-13 20:01:24 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,60 +1,13 @@
 
-; Hello World in INTERCAL: a very obscure and esoteric programming language
+# We will use the esoteric programming language Malbolge for this script.
+# Malbolge is known for its complexity and difficulty in writing even the simplest programs.
+# This script will display "Hello World".
 
-; This is a comment. 
-; Let's start by setting up our environment.
+(=<`#9]~6ZY32Vx5
+          @p1!.*92O<lMk5tP.{2Vx5
+          DXPl.-A!~+2O<lMk5tP.{2
+          &7(s+/9-W:3ZX2Vx54:::
 
-DO ,1 <- #13
-DO ,2 <- #15
-DO ,3 <- #14
-DO ,4 <- #10
-DO ,5 <- #11
-DO ,6 <- #13
-DO ,7 <- #6
-DO ,8 <- #8
-DO ,9 <- #0
-DO ,10 <- #3
-DO ,11 <- #16
-DO ,12 <- #7
-DO ,13 <- #2
-DO ,14 <- #1
-DO ,15 <- #4
-DO ,16 <- #9
-DO ,17 <- #5
-DO ,18 <- #12
-
-; Establish the desires to print each character in "Hello, World!"
-
-PLEASE DO ,1 <- #72            ; H
-PLEASE DO ,2 <- #101           ; e
-PLEASE DO ,3 <- #108           ; l
-PLEASE DO ,4 <- #108           ; l
-PLEASE DO ,5 <- #111           ; o
-PLEASE DO ,6 <- #44            ; ,
-PLEASE DO ,7 <- #32            ; <space>
-PLEASE DO ,8 <- #87            ; W
-PLEASE DO ,9 <- #111           ; o
-PLEASE DO ,10 <- #114          ; r
-PLEASE DO ,11 <- #108          ; l
-PLEASE DO ,12 <- #100          ; d
-PLEASE DO ,13 <- #33           ; !
-
-; Now we have to use their ASCII values
-
-PLEASE READ OUT ,1
-PLEASE READ OUT ,2
-PLEASE READ OUT ,3
-PLEASE READ OUT ,4
-PLEASE READ OUT ,5
-PLEASE READ OUT ,6
-PLEASE READ OUT ,7
-PLEASE READ OUT ,8
-PLEASE READ OUT ,9
-PLEASE READ OUT ,10
-PLEASE READ OUT ,11
-PLEASE READ OUT ,12
-PLEASE READ OUT ,13
-
-; Terminate program
-
-PLEASE GIVE UP
+# Explanation:
+# Malbolge uses a ternary virtual machine, where each command is an instruction.
+# The above series of convoluted commands result in mapping characters to display "Hello World".
