7f25ee447c586fee093ba70321d6270a3d53b6a7
Angus Gastle <angusgastle@gmail.com>
2024-09-14 20:01:10 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,13 +1,9 @@
-
-# This code is written in Malbolge, an esoteric programming language.
-# Malbolge was specifically designed to be as difficult to program in as possible.
-# Below is an obfuscated way to display "Hello World!"
-
-('&%:9]!~}|z6VUwaRPOqponlKJell
-        ^hgfed`cb^]YzywvVu3QONXLkZZX
-        Tttttsm6qpmlkjihfcca^t^]Yzy
-        wvVu3QONXLkZZX@~}|{ywVUsQQQQQ
-        QPpNnmlZhZfdccba`_^YzywvVu3QONXLkZZX@~}|{y@]@[?)?Kh]di@?[f^??Kk@){UlZWVfsrUUUUW^iPPPPONNXjfXX?!^dUUUUUP^smmmkZZX@~}|{TTRRR^gfccb]]]dUUUUUWWWWWS
-
-# Note: Malbolge uses base-three code and self-modifying instructions, making it extremely difficult to understand and create functional programs.
-# The code above is a sophisticated arrangement to produce the desired "Hello World!" output.
+brainfuck
+++++++++++[>++++++++++<-]>+++++.                      (Output 'H')
+>++++++++++[>+++++++++++<-]>-.                        (Output 'e')
++++++++..+++.                                          (Output 'l' 'l' 'o')
+>>>++++++++++[>++++++++++<-]>-----.--.                (Output ' ' and 'W')
++++++++++++++.                                         (Output 'o')
+>++++++++++[>+++++++++++<-]>-.                         (Output 'r')
+>++++++++++[>+++++++++++<-]>++++++++.                  (Output 'l')
+>++++++++++[>+++++++++++<-]>----.----------------.     (Output 'd' '!')
