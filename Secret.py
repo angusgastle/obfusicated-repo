ec3ed899a7625c83ad513198b602ba96a8b77025
Angus Gastle <angusgastle@gmail.com>
2024-09-15 02:00:59 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,9 +1,21 @@
-brainfuck
-++++++++++[>++++++++++<-]>+++++.                      (Output 'H')
->++++++++++[>+++++++++++<-]>-.                        (Output 'e')
-+++++++..+++.                                          (Output 'l' 'l' 'o')
->>>++++++++++[>++++++++++<-]>-----.--.                (Output ' ' and 'W')
-+++++++++++++.                                         (Output 'o')
->++++++++++[>+++++++++++<-]>-.                         (Output 'r')
->++++++++++[>+++++++++++<-]>++++++++.                  (Output 'l')
->++++++++++[>+++++++++++<-]>----.----------------.     (Output 'd' '!')
+perl
+# Befunge Code for Printing "Hello World"
+# Befunge is an esoteric programming language which uses a 2D playfield with instructions.
+
+# Befunge 93 interpreter typically processes a program in this 2D space, moving
+# The instruction pointer in various directions based on the commands it encounters.
+
+>              v
+# Initialize arrow pointing down to start vertical motion
+v 12345 <
+# Loop set up: Program enters here, preps Hello World
+>987654321 v
+             ^
+# This is our String: Bottom row of instructions, using assembler style Daa & ASCII equivalents
+",olleH ">!dlroW-OlleH@ "^
+# Additional settings routing ASCII codes into the printing directive.
+
+# GOTO
+,.>
+# R : Pop top of the stack and interpret.
+.@ #
