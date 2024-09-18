0f3e0f6f8cbbe30adb9e0c4874f7f28d943e5755
Angus Gastle <angusgastle@gmail.com>
2024-09-17 20:01:40 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,32 +1,22 @@
-
-# Befunge-93 code to display "Hello World" program
-# "Befunge" is a stack-based, reflective, esoteric programming language
-
->>v
- v"dlroW olleH"<
- >,@
-
-# Explanation of each command:
-#
-# >>v
-# '>' moves the instruction pointer (IP) to the right.
-# '' is no operation.
-# 'v' changes the direction of the IP to move downward.
-#
-#  v"dlroW olleH"<
-# 'v' changes direction of IP to move downwards.
-# '"' starts a string mode, which pushes each ASCII value of subsequent characters to the stack.
-# Characters "dlroW olleH" are pushed to the stack.
-# '"' ends the string mode.
-# '<' changes the direction of the IP to move left.
-#
-#  >,@
-# '>' changes the direction of the IP to move right again.
-# ',' is the output command and prints the top value from stack as a character.
-# '@' is the end of program command.
-#
-# Here is the translation where IP >, <>
-# | v"dlroW olleH"< moves down to load stack:
-# H: 72, e: 101, l: 108, l: 108, o: 111, W: 87, o: 111, r: 114, l: 108, d: 100
-# ',' outputs values from stack in reverse order since stacks are LIFO by nature.
-# '@' ends the program execution.
+brainfuck
++++++ +++++             initialize counter (cell #0) to 10
+[                       use loop to set the next four cells to 70/100/30/10
+    > +++++ ++              add  7 to cell #1
+    > +++++ +++++           add 10 to cell #2 
+    > +++                   add  3 to cell #3
+    > +                     add  1 to cell #4
+    <<<< -                  decrement counter (cell #0)
+]
+> ++ .                  print 'H'
+> +                       increment cell #1 from 70 to 72
+.                        print 'e'
++++++ ++                 increment cell #1 from 72 to 79
+.                        print 'l'
+.                        print 'l'
++++ .                   increment cell #1 to 82 and print 'o'
+> .                       print space
+--- .                    change value in cell #2 to 87 and print 'W'
+< +++ .                 increment value in cell #1 to 79 and print 'o'
+------ .               change value in cell #2 to 76 and print 'r'
+-------- .            change value in cell #2 to 68 and print 'l'
+< +++ .                 change value in cell #2 from 62 to 65 and print 'd'
