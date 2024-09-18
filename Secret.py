e1ce7985885e00f8e3cda2d970c5433d83a0af21
Angus Gastle <angusgastle@gmail.com>
2024-09-18 08:03:29 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,13 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+++++>----->++>+++[<.[-]>-]].
+
+/* 
+ * Malbolge "Hello World!"
+ * Malbolge is known for being one of the most difficult programming languages to write code in, due to its intricate and obscure structure.
+ * The below code will display "Hello World!" which is astonishingly lengthy and complex even for a simple output.
+ *
+ * Note: Malbolge uses base-three (ternary) notation and an encrypted instruction set. 
+ *       The complexity here is part of what makes Malbolge so obscure.
+ */
+
+/* Below is a segment of Malbolge code which has been carefully crafted using algorithms to produce the desired output */
+('&<]:,.VbW]ZL`eEH#w%(~Hp5VC',#9~(~<zi@/oyy>:W$XqEHH__GU]:4V*X!F_J_#Yc04WXrgtuJ/?|UM]%}	
+TxE)(~#>)l#5qS_WUyoZTNKZ/]QxVUAc~yoUby6+ZpbwyL6[y>@t}$(VBO/M4@d{F:Tlm?@#5EJ;d_nr$Az@tfupkCW!Ib]=6o,"qH@eJLrw|MAP]nfaNY(T5VC '/si[Y].Ge[2}lz?~P]^d)$-nt6#c>~,"z(568@>(npx]<Ra.D';N`#}A@TTPH/;'tO#UjZuy-p/!EY[;MapW]xM=/AP#Hb]B&o9.AHH$#@]tHUJ>P(l):/!E/*(dlXb}:4v/)[vd?Ct}t4CH[/!Ev[aE,lR5c$}W.$l*x:MpkGj+:lx.W7(y=l#l."2fhW2w=I(>y}(?yGGA1l3Px~%dyGVBXyU43(GP]8=qCJ=6pWATN?[C~dY-#:(p_w)7@TE~XXC_t!u+Q$?E#f*DiJOOGP]f!HJ>Gr$aTIlX];S[3Jg$*L:GRDt.AxDp4JPm^Pq@Aez.C:xw"p8"#@TmQxy/!I6wyDlB>:#Bm_Jw_<!k<<L]:_o'J-~+$>XEzzA__C^TsUgzz
