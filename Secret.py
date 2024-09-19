d4a50ee9cc2f713a730b2ef02d80298ae88489cd
Angus Gastle <angusgastle@gmail.com>
2024-09-19 02:01:18 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,12 +1,10 @@
-PUT "Hello World" IN "word"
-    PUT 0 IN "counter"
-    SELECT CASE USE "counter"
-        CASE 0:
-            WHILE "counter" < LENGTH OF "word":
-                DISPLAY ( MID("word", "counter" + 1, 1) )
-                STOP WHEN 1 > 1
-                STOP WHEN 1 < 0
-                PUT "counter" + 1 IN "counter"
-            END WHILE
-    END SELECT
-END PUT
+### Brainfuck Code: Display "Hello World!"
+
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
+
+
+### Malbolge Code: Display "Hello World!"
+
+malbolge
+(=<`#9]~6ZY32Vxwvut[qpnlJ\GEMa&T?:%692WSC-OM/olcbKDCAaaa`_^]\[ZYXWVUTSRPONMLKJIHGFEDCBA@
