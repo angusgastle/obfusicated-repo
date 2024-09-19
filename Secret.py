0b9a099f3f1f187f4f3efac9b25a82947cf1811d
Angus Gastle <angusgastle@gmail.com>
2024-09-18 20:01:49 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,12 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>+.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
+PUT "Hello World" IN "word"
+    PUT 0 IN "counter"
+    SELECT CASE USE "counter"
+        CASE 0:
+            WHILE "counter" < LENGTH OF "word":
+                DISPLAY ( MID("word", "counter" + 1, 1) )
+                STOP WHEN 1 > 1
+                STOP WHEN 1 < 0
+                PUT "counter" + 1 IN "counter"
+            END WHILE
+    END SELECT
+END PUT
