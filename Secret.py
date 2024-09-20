e7724fb8f2c6ad6d0e7089e975e9cb5748c56f20
Angus Gastle <angusgastle@gmail.com>
2024-09-20 14:03:12 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,3 +1,13 @@
-Brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++.>>+.>+++++++.<<+.>>++++++++++.
+/// Befunge-93 code for displaying "Hello World!"
+/// Befunge-93 is a two-dimensional esoteric programming language
 
+>v
+v "Hello World!" <
+> ,,,,,,,,,,,, @
+
+>v
+v          # < v
+> " #       <
+
+$_$^$$#>$^#>-#<foo># #
+!Hello_World!!run!"^^
