18921b0437a35fa6bfd544d1a67f4bf448279f3e
Angus Gastle <angusgastle@gmail.com>
2024-09-21 14:01:51 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,24 +1,68 @@
+befunge
+>25*"!dlroW ,olleH":v
+                v:,_@
+                ^  <
+
+
 brainfuck
-++++++++++[>+++++++<-]>+++++.                   # Initialize memory cell to hold ASCII value of 'H'
->++.                                             # Initialize memory cell to hold ASCII value of 'e'
->>++++++++++[>+++<-]>+.                          # Initialize memory cell to hold ASCII value of 'l'
-<.                                               # Display 'H'
->----.                                           # Display 'e'
-+++++++++++.                                     # Display 'l'
->.                                               # Display another 'l'
-+++.                                             # Initialize memory cell to hold ASCII value of 'o'
-<-.                                              # Display 'o'
->++++++++++.                                     # Display ' '
->++++++++++[>++<-]>+.                            # Initialize memory cell to hold ASCII value of 'W'
->.                                               # Display 'W'
-------.                                          # Initialize memory cell to hold ASCII value of 'o'
-<++++++++++++++.                                 # Initialize memory cell to hold ASCII value of 'r'
->.                                               # Display 'o'
-<.                                               # Display 'l'
->>>>+++++++++.                                   # Initialize memory cell to hold ASCII value of 'd'
-<<<<.                                            # Display 'r'
-<++.                                             # Display 'l'
->.                                               # Display 'd'
->>-.                                             
------------.                                     # Move the instruction pointer to zero memory cell
-+++++++++++++++.                                 # Move the instruction pointer to next command
++[----->+++<]>.---.+++++++..+++.[--->+<]>-----.---[->++++<]>-.
+
+
+whitespace
+[S S S T    S S S N
+_Push_0]
+[S S S T    S S T N
+_Push_1]
+[S S S T    S T S N
+_Push_2]
+[S S S T    S T T S T N
+_Push_3]
+[S S S T    S T T S S N
+_Push_4]
+[S S S T    S T T T T S N
+_Push_5]
+[S S S T    S T T T T T S T N
+_Push_6]
+[S S S T    S T T T T T T S T T N
+_Push_7]
+[S S S T    S T T T T T T S T T T S N
+_Push_8]
+[S S S T    S T T T T T T T T S T T S T S N
+_Push_9]
+[S S S T    S T T T T T T T T S T T S T S S N
+_Push_10]
+[S T S T   S T S T    N
+_Copy_1]
+[S N
+_Copy_1]
+[S S S S   N
+_Duplication]
+[T N
+_Print]
+[S S S S   N
+_Pop]
+[S S S S   N
+_Pop]
+[S S S S   N
+_Pop]
+[T    S N
+_Halt]
+
+
+malbolge
+(=/<`z{'%;<vvl#=~!!!!~<^\3~^;=!:@~)==~/x?zv~;=#_=z};>`q<^Nr_xz>;r`=tv:r/vi@<=rr~~~
+
+
+INTERCAL
+DO ,1 <- #13
+DO ,1 SUB #1 <- #91
+DO ,1 SUB #2 <- #53
+DO ,1 SUB #3 <- #106
+DO ,1 SUB #4 <- #15
+DO ,1 SUB #5 <- #49
+DO ,1 SUB #6 <- #108
+DO ,1 SUB #7 <- #82
+DO ,1 SUB #8 <- #99
+DO ,1 SUB #9 <- #91
+DO ,1 SUB #10 <- #20
+PLEASE WRITE OUT ,1
