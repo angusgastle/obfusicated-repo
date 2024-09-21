3b33fe620065125a7593b182ecebd0ccaf557304
Angus Gastle <angusgastle@gmail.com>
2024-09-21 08:01:59 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,51 +1,24 @@
-
-# Whitespace programming language
-# This program prints "Hello World"
-# Whitespace only uses spaces, tabs, and linefeeds, no visible characters
-
-# [SP] – Space
-# [TAB] – Tab
-# [LF] – Linefeed (Enter)
-
-# Push character codes onto the stack (Hello World in ASCII)
-[SP][SP][SP][SP][SP][SP][LF]   # Push 10 (newline)
-[SP][TAB][SP][TAB][LF]         # Push 33 ('!')
-[SP][SP][SP][SP][SP][SP][SP][SP][TAB][LF] # Push 100 ('d')
-[SP][TAB][SP][TAB][LF]         # Push 33 ('!')
-[SP][TAB][TAB][SP][LF]         # Push 32 (' ')
-[SP][TAB][SP][SP][TAB][LF]     # Push 87 ('W')
-[SP][TAB][TAB][TAB][LF]        # Push 111 ('o')
-[SP][TAB][SP][SP][SP][SP][LF]  # Push 114 ('r')
-[SP][TAB][SP][SP][TAB][LF]     # Push 87 ('W')
-[SP][TAB][SP][SP][SP][LF]      # Push 108 ('l')
-[SP][TAB][SP][SP][SP][TAB][LF] # Push 100 ('l')
-[SP][TAB][SP][TAB][LF]         # Push 33 ('!')
-[SP][TAB][TAB][SP][LF]         # Push 32 (' ')
-[SP][SP][SP][SP][SP][SP][LF]   # Push 72 ('H')
-[SP][TAB][SP][SP][SP][LF]      # Push 108 ('l')
-[SP][TAB][TAB][TAB][LF]        # Push 111 ('o')
-[SP][TAB][SP][SP][TAB][LF]     # Push 87 ('W')
-
-# Print characters in order
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-[TAB][LF][SP][SP][LF]
-
-# Version: 1.0.0
-# Author: OpenAI's Assistant
-# Date: 2023
-
+brainfuck
+++++++++++[>+++++++<-]>+++++.                   # Initialize memory cell to hold ASCII value of 'H'
+>++.                                             # Initialize memory cell to hold ASCII value of 'e'
+>>++++++++++[>+++<-]>+.                          # Initialize memory cell to hold ASCII value of 'l'
+<.                                               # Display 'H'
+>----.                                           # Display 'e'
++++++++++++.                                     # Display 'l'
+>.                                               # Display another 'l'
++++.                                             # Initialize memory cell to hold ASCII value of 'o'
+<-.                                              # Display 'o'
+>++++++++++.                                     # Display ' '
+>++++++++++[>++<-]>+.                            # Initialize memory cell to hold ASCII value of 'W'
+>.                                               # Display 'W'
+------.                                          # Initialize memory cell to hold ASCII value of 'o'
+<++++++++++++++.                                 # Initialize memory cell to hold ASCII value of 'r'
+>.                                               # Display 'o'
+<.                                               # Display 'l'
+>>>>+++++++++.                                   # Initialize memory cell to hold ASCII value of 'd'
+<<<<.                                            # Display 'r'
+<++.                                             # Display 'l'
+>.                                               # Display 'd'
+>>-.                                             
+-----------.                                     # Move the instruction pointer to zero memory cell
++++++++++++++++.                                 # Move the instruction pointer to next command
