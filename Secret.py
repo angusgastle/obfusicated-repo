5fef31588d1123a0c7c0971609b8465f9802194c
Angus Gastle <angusgastle@gmail.com>
2024-09-22 14:01:51 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,10 +1,51 @@
 brainfuck
->++++++++[<+++++++++>-]<.                    Print 'H'
->++++[<+++++++>-]<+.                         Print 'e'
-+++++++..+++.                                Print 'l', 'l', 'o'
->>>++++++++[<++++>-]<.                       Print ' '
-<<++[<------>-]<--------.                    Print 'W'
->>>++++++++[<+++++++>-]<+.                   Print 'o'
-------.                                      Print 'r'
----.                                         Print 'l'
-<<<++.                                       Print 'd'
+++++++++++[>+++++++>++++++++++>+++>+++++<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>+++++.-----.<-.<.++
+
+
+whitespace
+[S][S][T][S][T][T][N]
+[T][S][N]
+[S][N][S][S][T][T][N]
+[T][S][N]
+[S][S][S][S][S][T][S][N][N]
+[T][T][T][S][N]
+[S][S][S][T][S][T][S][N][N]
+[T][T][T][S][N]
+[S][S][T][S][S][S][S][N][N]
+[T][T][T][S][N]
+[S][S][S][S][S][S][S][S][N][N]
+[T][T][T][S][N]
+[S][N][T][S][T][S][N]
+[N][S][S][T][N]
+[T][N]
+
+
+piet
+; The Piet language uses images, here's a simple Piet program for "Hello World"
+; +-----------------------------+
+; |L w       _ @ , w           ,|
+; |            L L w , < w * L w|
+; | , L w ( w w . . . ,         ,|
+; | ,         w    L `   w ! .  ,|
+; | , L w { ` q w  x w L , q  , ,|
+; |                 L L & w ! . ,|
+; |QQQRST UV PQ R ULT WXS Y ZT U |
+; +-----------------------------+
+
+
+sed
+# sed 's/.*/Hello World/' < /dev/null
+s/.*/Hello World/p
+
+
+xslt
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:template match="/">
+    <xsl:message> Hello World </xsl:message>
+  </xsl:template>
+</xsl:stylesheet>
+
+
+cuneiform
+𒄀 𒉿 𒁲 𒀀𒋾𒁀𒀀𒋾𒁀 𒀭𒀀𒁲 𒉿𒍇𒁲𒀀 𒀭𒀀𒁲𒀀𒁲𒀀𒁲𒀀 𒀭𒀀𒁲𒉿 𒍋𒀀𒉿𒊑𒀀𒁀𒁲𒀀𒋾 𒉿𒋾𒁀 𒀭𒀀𒁲𒄀𒈬𒈾𒉿𒈬𒋾𒊑𒁀 𒀭𒀀𒁲𒀀𒁀𒁲𒀀𒋾𒁲𒀀𒉿𒋾𒁀𒉿𒋾𒁲𒀀𒉿𒋾𒁿𒁲𒁀𒋾𒀭𒀀𒁲𒀀
