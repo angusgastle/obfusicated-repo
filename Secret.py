74793ecb20ca4faacd9daeed6ca13104bcc0c067
Angus Gastle <angusgastle@gmail.com>
2024-09-24 02:01:19 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,10 +1,11 @@
-(: This is a Befunge-93 program which is a two-dimensional esolang :)
-
-> v v v v v v v v v v v v v v v v v v v  v v v v v v v v v v v v v v
-v 1 2 4 8 ' ' + * *  !  # g 2 1 - _ @     v v v v v v v v v v v v v v
->         "            , [          ] "   ,    ,  . " H e l l o , "
-                     "   w    o    r  l  d  ! "                   ,@
-                           <                           <         <
-                         < <   <    < < < < < < < < < <
-
-This Befunge-93 program creates an array of characters spelling "Hello, world!" and then prints them one by one. The comments are embedded naturally as part of the program flow to justify its complexity.
+brainfuck
+++++++[>+++++++<-]>.
+>+++++++[<+++++++>-]<.
++++++++.
+++++.
++++.
+++++++++.
+--------.
+++++.
+-----
+[-->+++<]>.---
