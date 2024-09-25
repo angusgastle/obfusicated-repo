9a764625841711ae04a9c241ae7c7d0c173bbad1
Angus Gastle <angusgastle@gmail.com>
2024-09-25 02:01:18 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,2 +1,31 @@
-brainfuck
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++.>+++++++.-------.<++++++++.--------.>+.>.<<.
+bc
+/*  
+  This is a simple script in the programming language 'bc' 
+  to display "Hello World" using the 'print' and ASCII codes.
+*/
+
+/* Define ASCII codes for 'H', 'e', 'l', 'o', ' ', 'W', 'r', 'd' with variables */
+H = 72
+e = 101
+l = 108
+o = 111
+space = 32
+W = 87
+r = 114
+d = 100
+
+/* Print each character by converting ASCII codes to their corresponding characters */
+print "\072"  /* H */
+print "\101"  /* e */
+print "\108"  /* l */
+print "\108"  /* l */
+print "\111"  /* o */
+print "\032"  /* Space */
+print "\087"  /* W */
+print "\111"  /* o */
+print "\114"  /* r */
+print "\108"  /* l */
+print "\100"  /* d */
+
+/* Print a newline to end the output cleanly */
+print "\n"
