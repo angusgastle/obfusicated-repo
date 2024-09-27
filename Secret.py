4c42885b0a55c3d790c4486895f4d9b7ebd2768b
Angus Gastle <angusgastle@gmail.com>
2024-09-27 14:03:41 -0600

    A New Update

--- Secret.py
+++ Secret.py
@@ -1,14 +1,38 @@
-perl
-# OBfuscated Perl Code for "Hello World"
-# This code snippet is intentionally complex and unconventional
-$;=$]==$:=$[=!$^F;$]=pop||($*[pop]||pop)->pop;@;="$]".$$;x::y:::q{print+x+eval('`"'.("@;"/=((map{ord-9+@;--$[,map/./g&&eval$]+$*.($_-3)/$_,split(//,@;+chr--$#_)}ref("")).hex).oct.cap||($+=.01)((do{$,>15x 0})))'}).++$[++$;-=$*],$:+++($---($,-$))+--$$::-+--$|+q\xCFcd+i>>$_&-++$*--;
+brainfuck
+++++++++++[>+++++++>++++++++++>+++>++++++++++++<<<<<-]>++++.>-.+++++++.---------.+++++.+++++++++.<+++++++.>.---------.>+++..+++++++++++++.
 
-BEGIN{
-    # Encoded message
-    @="2222222 333 \334\321J==";$*?{print++$:=$]=~y,CELv99::'Jy\\f&,${}=$@}}]$[[q}0xx+'%|:"*X%"=,=($===)/xB-\x22>?a[221].q:aX'\J~{^|@=240;
-}
-for(@){($,-=((sort{length$+@(=())})/(qx[->(@@,\+`))(($_)=>><]).(/.=`'"\x5ET?.<etc,/.,.*),);($):++1x"23pgj\160"+"1d+M234";$
+# Breakdown:
+# Initialize memory cells, let's say initially at memory[0]
+# ++++++++++        -> Set memory[0] to 10
+# [                 -> Start loop (will run 10 times)
+#   >+++++++        -> Increment memory[1] by 7 (7 * 10 = 70 in total)
+#   >++++++++++     -> Increment memory[2] by 10 (10 * 10 = 100 in total)
+#   >+++            -> Increment memory[3] by 3 (3 * 10 = 30 in total)
+#   >++++++++++++   -> Increment memory[4] by 12 (12 * 10 = 120 in total)
+#   <<<<<-          -> Move back to memory[0] and decrement it by 1
+# ]
+# Now memory[1] = 70, memory[2] = 100, memory[3] = 30, memory[4] = 120
 
-# Execute encoded message
-(tr/222333422233344223334//)x(25)}
-qx !! @ };
+# Moving to memory[1] (Hello World characters computation)
+# >++++            -> Go to memory[1], increment (70+4=74, ASCII for 'H')
+# .                -> Output 'H'
+# >-.              -> Move to memory[2], decrement (100-1=99, ASCII for 'e')
+# .                -> Output 'e'
+# ++++++++.        -> Increment to get 'l' (99 + 7 = 106, ASCII for 'l')
+# +++++++.         -> 'l' again
+# ---------.       -> Decrement to get 'o' (106 - 8 = 98, ASCII for 'o')
+# +++++.           -> Increment to get ' ' (32 + 5 = 37)
+# +++++++++.       -> (37 + 8 = 45)
+# Move back for 'W'
+# <+++++++.        -> Go back to memory[3] (30+7=37)
+# >.               -> Output ' '
+# >---------.      -> Decrement (120 - 7= 113, ASCII for 'r')
+# >+++             -> Go memory[4], increment (113 + 3 = 116, ASCII for 'W')
+# .                -> Output 'W'
+# +++++++++        -> Increment (116 + 9 = 125)
+# +++++++++.       -> Increment to get 'o' again (125 + 4 = 129 to prevent overflow)
+# +.               -> Increment once more to get 'r' (129 + 1 = 130 avoids overflows)
+# <.               -> Go back to memory[3], output ' ' 
+# >----.           -> Now back to memory[4], decrement (130 - 6 = 'l') 
+# >+++.            -> Output 'd' (103 + 3 = 106)
+# +++++++++++++.]  -> Adjust loop to end properly
